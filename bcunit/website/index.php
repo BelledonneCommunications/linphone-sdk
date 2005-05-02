0a521f922a1343015c2c728f95d408aaf292db86
jds2 <jds2@f00e6729-6848-4c17-8bfb-678c97c00071>
2005-05-02 10:28:12 +0000

    Initial import of version 1 CUnit website files.
    git-svn-id: http://svn.code.sf.net/p/cunit/code/trunk@71 f00e6729-6848-4c17-8bfb-678c97c00071

---
+++ bcunit/website/index.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+<meta http-equiv="Content-Type"
+content="text/html; charset=iso-8859-1">
+<meta name="GENERATOR" content="Microsoft FrontPage Express 2.0">
+<title>CUnit</title>
+</head>
+
+<body bgcolor="#D3D3D3" text="#56272C" style="{ width: 500px; text-align: center;">
+
+<p align="center"><font color="#0000FF" size="4"
+face="Comic Sans MS">CUnit</font></p>
+<div align="left">
+
+<table border="0" width="732" height="52">
+    <tr>
+        <td width="131" height="30">&nbsp;</td>
+        <td width="593" height="50"><p align="center"><em><strong>CUnit
+        is Unit Testing Framework for 'C' language.</strong></em></p>
+        </td>
+    </tr>
+    <tr>
+        <td width="131" height="21"><div align="center"><center><table
+        border="0" width="100%">
+            <tr>
+                <td><a href="http://cunit.sourceforge.net/">Home</a></td>
+            </tr>
+            <tr>
+                <td><a
+                href="http://sourceforge.net/projects/cunit/">Project</a></td>
+            </tr>
+            <tr>
+                <td><a
+                href="http://cunit.sourceforge.net/documentation/">Documentation</a></td>
+            </tr>
+            <tr>
+                <td>CVS</td>
+            </tr>
+            <tr>
+                <td><a
+                href="http://cunit.sourceforge.net/screenshots/">Screenshots</a></td>
+            </tr>
+            <tr>
+                <td><a
+                href="http://cunit.sourceforge.net/contact/">Contact</a></td>
+            </tr>
+        </table>
+        </center></div></td>
+        <td width="593" height="21">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        As of now we don't have any light weight unit testing
+        framework for C as it is for C++ (CppUnit) or Java
+        (JUnit). This intiative is taken to provide such a
+        platform independent/version independent core framework
+        and the OS/Environment specific wrappers around them. The
+        wrappers will be finally provided for the following
+        environments,<blockquote>
+            <blockquote>
+                <ul>
+                    <li>Console (Using standard C library)</li>
+                    <li>Curses (On Unix using ncurses library)</li>
+                    <li>Windows Based (Dialog based on
+                        Win9x/NT/2000/XWindows)</li>
+                </ul>
+            </blockquote>
+        </blockquote>
+        </td>
+    </tr>
+    <tr>
+        <td width="131" height="21">&nbsp;</td>
+        <td width="593" height="21">&nbsp;</td>
+    </tr>
+</table>
+</div>
+</body>
+</html>
