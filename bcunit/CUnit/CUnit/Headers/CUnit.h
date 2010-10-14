09ae9b48d83594cb397f8985d0609683dcd6e324
tlh2000 <tlh2000@f00e6729-6848-4c17-8bfb-678c97c00071>
2010-10-14 09:11:14 +0000

    * version bump to 2.1.1 * added pkg-config file
    git-svn-id: http://svn.code.sf.net/p/cunit/code/trunk@116 f00e6729-6848-4c17-8bfb-678c97c00071

--- bcunit/CUnit/CUnit/Headers/CUnit.h
+++ bcunit/CUnit/CUnit/Headers/CUnit.h
@@ -54,7 +54,7 @@
 #include <math.h>
 
 /** CUnit version number. */
-#define CU_VERSION "2.1-0"
+#define CU_VERSION "2.1-1"
 
 /*  Max string lengths for names (includes terminating NULL. */
 /** Maximum length of a test name string. */
