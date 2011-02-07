b5e71b133bb7ea591dd828004e589e1f88dcbb61
Simon Morlat <simon.morlat@linphone.org>
2011-02-07 15:00:20 +0100

    mingw build updated

--- msamr/autogen.sh
+++ msamr/autogen.sh
@@ -20,6 +20,11 @@ if test -d /opt/local/share/aclocal ; then
 	ACLOCAL_ARGS="-I /opt/local/share/aclocal"
 fi
 
+
+if test -d /share/aclocal ; then
+        ACLOCAL_ARGS="-I /share/aclocal"
+fi
+
 echo "Generating build scripts in msamr..."
 set -x
 $LIBTOOLIZE --copy --force
