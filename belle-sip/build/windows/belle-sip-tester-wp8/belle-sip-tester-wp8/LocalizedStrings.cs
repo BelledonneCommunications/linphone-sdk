5c79234f0d9a8ef1b68faf0abf8746bf12d31773
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-02-13 14:17:13 +0100

    belle-sip-tester application for Windows Phone 8.

---
+++ belle-sip/build/windows/belle-sip-tester-wp8/belle-sip-tester-wp8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using belle_sip_tester_wp8.Resources;
+
+namespace belle_sip_tester_wp8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
