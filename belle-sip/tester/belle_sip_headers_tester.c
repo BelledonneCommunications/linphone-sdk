c3ddccc26f73f4f41ad3beb0543c37b237881459
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-02-19 14:34:07 +0100

    Fix compilation with Visual Studio.

--- belle-sip/tester/belle_sip_headers_tester.c
+++ belle-sip/tester/belle_sip_headers_tester.c
@@ -608,10 +608,11 @@ void test_header_replaces(void) {
 void test_header_replaces_escaped(void) {
 	belle_sip_header_replaces_t* L_replaces;
 	char* escaped_to_string;
+	char* l_raw_header;
 	belle_sip_header_replaces_t* L_tmp = belle_sip_header_replaces_create("12345@192.168.118.3","5FFE-3994","12345");
 	escaped_to_string=belle_sip_header_replaces_value_to_escaped_string(L_tmp);
 	L_replaces=belle_sip_header_replaces_create2(escaped_to_string);
-	char* l_raw_header = belle_sip_object_to_string(BELLE_SIP_OBJECT(L_replaces));
+	l_raw_header = belle_sip_object_to_string(BELLE_SIP_OBJECT(L_replaces));
 	belle_sip_object_unref(BELLE_SIP_OBJECT(L_replaces));
 	L_tmp= belle_sip_header_replaces_parse(l_raw_header);
 	L_replaces = BELLE_SIP_HEADER_REPLACES(belle_sip_object_clone(BELLE_SIP_OBJECT(L_tmp)));
