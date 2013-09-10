941ad6e6e4f28d315382a6a48acd22a3d42c654a
Sylvain Berfini <sylvain.berfini@linphone.org>
2013-09-10 11:11:56 +0200

    Added build step for windows to set the correct package version

---
+++ belle-sip/build/windows/belle-sip/version.bat
@@ -0,0 +1,10 @@
+REM define the tools
+SET grep_exe=grep.exe
+SET sed_exe=sed.exe
+
+REM get the version
+SET grep_cmd=%grep_exe% "" ../../../VERSION
+FOR /F %%i IN (' %grep_cmd% ') DO SET version=%%i
+
+REM set the version in the props file
+%sed_exe% -i.bak "s/BELLESIP_PACKAGE_VERSION>[0-9.]*</BELLESIP_PACKAGE_VERSION>%version%</g" belle-sip.props
