6869b8b7474d8bfb31d79aea331a25da73d13506
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2013-10-24 16:44:58 +0200

    Added an Android.mk file to enable Android builds. See linphone-android repo.

---
+++ mswebrtc/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libmsisac
+
+ISAC_SRC_DIR    = $(LOCAL_PATH)/../externals/webrtc
+
+LOCAL_SRC_FILES := isac_enc.c isac_dec.c
+
+LOCAL_C_INCLUDES += $(ISAC_SRC_DIR) \
+    $(ISAC_SRC_DIR)/modules/audio_coding/codecs/isac/fix/source     \
+    $(ISAC_SRC_DIR)/modules/audio_coding/codecs/isac/fix/util 		\
+    $(ISAC_SRC_DIR)/modules/audio_coding/codecs/isac/fix/interface 	\
+	$(ISAC_SRC_DIR)/common_audio/signal_processing/include          \
+	$(LOCAL_PATH)/../linphone/oRTP/include                          \
+	$(LOCAL_PATH)/../linphone/mediastreamer2/include
+
+include $(BUILD_STATIC_LIBRARY)
+
+
