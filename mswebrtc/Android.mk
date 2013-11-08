4ce3a022250c3c9c30fc26feec12e8f951ce8ee5
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2013-11-08 14:54:21 +0100

    Added library dependencies in Android.mk

--- mswebrtc/Android.mk
+++ mswebrtc/Android.mk
@@ -7,6 +7,9 @@ ISAC_SRC_DIR    = $(LOCAL_PATH)/../externals/webrtc
 
 LOCAL_SRC_FILES := isac_enc.c isac_dec.c
 
+
+LOCAL_STATIC_LIBRARIES += libwebrtc_isacfix libwebrtc_isacfix_neon libwebrtc_spl libwebrtc_spl_neon 
+
 LOCAL_C_INCLUDES += $(ISAC_SRC_DIR) \
     $(ISAC_SRC_DIR)/modules/audio_coding/codecs/isac/fix/source     \
     $(ISAC_SRC_DIR)/modules/audio_coding/codecs/isac/fix/util 		\
