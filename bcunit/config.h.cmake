2f0570e3b0a9960c80f9488a28bb9f0652fa591f
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-02-06 18:09:13 +0100

    Enable compilation of CUnit using CMake.

---
+++ bcunit/config.h.cmake
@@ -0,0 +1,46 @@
+/***************************************************************************
+* antlr3config.h.cmake
+* Copyright (C) 2014  Belledonne Communications, Grenoble France
+*
+****************************************************************************
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+****************************************************************************/
+
+#cmakedefine PACKAGE "@PACKAGE@"
+#cmakedefine PACKAGE_NAME "@PACKAGE_NAME@"
+#cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
+#cmakedefine PACKAGE_STRING "@PACKAGE_STRING@"
+#cmakedefine PACKAGE_TARNAME "@PACKAGE_TARNAME@"
+#cmakedefine PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
+#cmakedefine PACKAGE_URL "@PACKAGE_URL@"
+#cmakedefine VERSION "@VERSION@"
+
+#cmakedefine HAVE_ASSERT_H
+#cmakedefine HAVE_CTYPE_H
+#cmakedefine HAVE_DLFCN_H
+#cmakedefine HAVE_FILE_H
+#cmakedefine HAVE_INTTYPES_H
+#cmakedefine HAVE_MATH_H
+#cmakedefine HAVE_MEMORY_H
+#cmakedefine HAVE_STDINT_H
+#cmakedefine HAVE_STDIO_H
+#cmakedefine HAVE_STDLIB_H
+#cmakedefine HAVE_STRINGS_H
+#cmakedefine HAVE_STRING_H
+#cmakedefine HAVE_SYS_STAT_H
+#cmakedefine HAVE_SYS_TYPES_H
+#cmakedefine HAVE_UNISTD_H
