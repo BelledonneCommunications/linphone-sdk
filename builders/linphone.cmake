b8e3d20967ef671e41242efd926428530cade88b
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-02-20 11:41:00 +0100

    Update linphone.

--- builders/linphone.cmake
+++ builders/linphone.cmake
@@ -21,7 +21,7 @@
 ############################################################################
 
 set(EP_linphone_GIT_REPOSITORY "git://git.linphone.org/linphone.git")
-set(EP_linphone_GIT_TAG "e31a4256ac52904d036d231e84e5e1dcea0f1c68") # Branch 'master'
+set(EP_linphone_GIT_TAG "1372da9628fd6a04a7ff1b74fca4db12d4bc5268") # Branch 'master'
 set(EP_linphone_AUTOTOOLS "yes")
 set(EP_linphone_CONFIGURE_OPTIONS "--disable-strict --enable-bellesip --enable-external-ortp --enable-external-mediastreamer")
 set(EP_linphone_DEPENDENCIES EP_bellesip EP_ortp EP_ms2 EP_xml2)
