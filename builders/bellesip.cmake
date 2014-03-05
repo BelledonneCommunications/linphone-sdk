ab2cb03d4604c1e660d43d09944894c76f21e67c
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-03-05 15:15:21 +0100

    Update belle-sip.

--- builders/bellesip.cmake
+++ builders/bellesip.cmake
@@ -21,7 +21,7 @@
 ############################################################################
 
 set(EP_bellesip_GIT_REPOSITORY "git://git.linphone.org/belle-sip.git")
-set(EP_bellesip_GIT_TAG "b3765e87e7ce902cfc57126bd6159450fc07b6a7") # Branch 'master'
+set(EP_bellesip_GIT_TAG "7eda9195ade3fb814f7969dc96543ecf50da5c00") # Branch 'master'
 set(EP_bellesip_CMAKE_OPTIONS )
 set(EP_bellesip_LINKING_TYPE "-DENABLE_STATIC=0")
 set(EP_bellesip_DEPENDENCIES EP_antlr3c EP_polarssl)
