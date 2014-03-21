8977aff1d0626d8a49d298e7415b02905461bffb
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-03-21 14:07:49 +0100

    Use tarball instead of git to get vpx.

--- builders/vpx.cmake
+++ builders/vpx.cmake
@@ -20,8 +20,7 @@
 #
 ############################################################################
 
-set(EP_vpx_GIT_REPOSITORY "https://chromium.googlesource.com/webm/libvpx")
-set(EP_vpx_GIT_TAG "v1.3.0")
+set(EP_vpx_URL "http://webm.googlecode.com/files/libvpx-v1.3.0.tar.bz2")
 set(EP_vpx_USE_AUTOTOOLS "yes")
 set(EP_vpx_CONFIG_H_FILE vpx_config.h)
 set(EP_vpx_CONFIGURE_OPTIONS
