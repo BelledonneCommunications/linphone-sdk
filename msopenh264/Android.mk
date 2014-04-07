0b56d4f121196b393fa9998725c6f8a994e533a6
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-04-07 12:45:05 +0200

    Fix Android.mk.

--- msopenh264/Android.mk
+++ msopenh264/Android.mk
@@ -4,13 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libmsopenh264
 
 
-LOCAL_SRC_FILES = src/msopenh264.c
+LOCAL_SRC_FILES = src/msopenh264.cpp src/msopenh264dec.cpp src/msopenh264enc.cpp
 
 
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../linphone/oRTP/include \
 	$(LOCAL_PATH)/../linphone/mediastreamer2/include \
-	$(LOCAL_PATH)/../externals/openh264
+	$(LOCAL_PATH)/../externals/build/openh264/include
 
 LOCAL_CFLAGS += -DVERSION=\"android\"
 
