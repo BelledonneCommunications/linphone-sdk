27ebedb0eeecfaf066927abf641a66f59ad70a50
Simon Morlat <simon.morlat@linphone.org>
2014-04-16 15:27:21 +0200

    change android include path

--- msopenh264/Android.mk
+++ msopenh264/Android.mk
@@ -10,7 +10,7 @@ LOCAL_SRC_FILES = src/msopenh264.cpp src/msopenh264dec.cpp src/msopenh264enc.cpp
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../linphone/oRTP/include \
 	$(LOCAL_PATH)/../linphone/mediastreamer2/include \
-	$(LOCAL_PATH)/../externals/build/openh264/include
+	$(LOCAL_PATH)/../externals/openh264/include
 
 LOCAL_CFLAGS += -DVERSION=\"android\"
 
