40bdfd391e5744847293c8c614380bb66f287764
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-11-03 11:56:55 +0100

    Build with the correct version on Windows Phone 8.

--- belle-sip/build/wp8/belle-sip/version.bat
+++ belle-sip/build/wp8/belle-sip/version.bat
@@ -1,9 +1,7 @@
-REM define the tools
-SET grep_exe=grep.exe
-SET sed_exe=sed.exe
+@ECHO off
 
-REM get the version
-FOR /F %%i IN (' %grep_exe% "AC_INIT(" ../../../configure.ac ^| %sed_exe% -e "s/.*belle-sip\]//" ^| %sed_exe% -e "s/].*//" ^| %sed_exe% -e "s/.*\[//" ') DO SET version=%%i
-
-REM set the version in the props file
-REM %sed_exe% -i.bak "s/BELLESIP_PACKAGE_VERSION>\"[0-9.]*\"</BELLESIP_PACKAGE_VERSION>\"%version%\"</g" belle-sip.props
+FOR /F "delims=" %%a IN ('findstr /B AC_INIT ..\..\..\configure.ac') DO (
+	FOR /F "tokens=1,2,3 delims=[,]" %%1 IN ("%%a") DO (
+		ECHO #define PACKAGE_VERSION "%%3" > config.h
+	)
+)
