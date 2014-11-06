a115fb91b505f4c0f56a37b474bddc125c78c76e
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2014-11-06 14:48:22 +0100

    Update myodbc and unixodbc references to fix documentation location

--- builders/unixodbc.cmake
+++ builders/unixodbc.cmake
@@ -21,7 +21,7 @@
 ############################################################################
 
 set(EP_unixodbc_GIT_REPOSITORY "git://git.linphone.org/unixODBC.git")
-set(EP_unixodbc_GIT_TAG "35a82939ed0d28d0d777124ea55406a947ed5173")
+set(EP_unixodbc_GIT_TAG "095a70fea9e4f6b486813032868f69f44973260a")
 
 
 set(EP_unixodbc_DEPENDENCIES )
