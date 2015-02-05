6d045b4389e654e56064b16cfe3fd5daabe600fa
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2015-02-05 10:40:16 +0100

    Update ortp

--- builders/ortp.cmake
+++ builders/ortp.cmake
@@ -22,7 +22,7 @@
 
 set(EP_ortp_GIT_REPOSITORY "git://git.linphone.org/ortp.git")
 set(EP_ortp_GIT_TAG_LATEST "master")
-set(EP_ortp_GIT_TAG "8cac6a399755001f5df7a933bc26c7b9629981fc")
+set(EP_ortp_GIT_TAG "d7b7f8af70c7a40c55f6b026833b21e177c88eea")
 
 if("${EP_ortp_FORCE_AUTOTOOLS}" STREQUAL "yes")
 	set(EP_ortp_LINKING_TYPE "--enable-static")
