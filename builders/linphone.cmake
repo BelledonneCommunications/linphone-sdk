6a25971158767f777846cfc7e8471d827dda58e5
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2015-02-10 16:09:53 +0100

    Update linphone git revision.

--- builders/linphone.cmake
+++ builders/linphone.cmake
@@ -22,7 +22,7 @@
 
 set(EP_linphone_GIT_REPOSITORY "git://git.linphone.org/linphone.git")
 set(EP_linphone_GIT_TAG_LATEST "master")
-set(EP_linphone_GIT_TAG "b97cb51e58a18a62d87629296ebe02d3abad7d18")
+set(EP_linphone_GIT_TAG "b305c8861301675d82d3e92743e20e1a0e102fb7")
 
 set(EP_linphone_CMAKE_OPTIONS )
 set(EP_linphone_LINKING_TYPE "-DENABLE_STATIC=NO")
