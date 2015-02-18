f50da9f209f63b353e00cef3f72e00cf557ae008
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2015-02-18 11:36:51 +0100

    Update ms2 git revision.

--- builders/ms2.cmake
+++ builders/ms2.cmake
@@ -22,7 +22,7 @@
 
 set(EP_ms2_GIT_REPOSITORY "git://git.linphone.org/mediastreamer2.git")
 set(EP_ms2_GIT_TAG_LATEST "master")
-set(EP_ms2_GIT_TAG "d1684034e44781ca71a107d7d2b6db626a3ab993")
+set(EP_ms2_GIT_TAG "2af2ca2e4b2b027329b49212b1d161828efc1c0c")
 
 set(EP_ms2_CMAKE_OPTIONS )
 set(EP_ms2_LINKING_TYPE "-DENABLE_STATIC=NO")
