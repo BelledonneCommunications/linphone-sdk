09576a6bde288e6746fa6ab4fb0cc421f5710622
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2015-02-24 23:01:07 +0100

    Updated ms2 git revision

--- builders/ms2.cmake
+++ builders/ms2.cmake
@@ -22,7 +22,7 @@
 
 set(EP_ms2_GIT_REPOSITORY "git://git.linphone.org/mediastreamer2.git")
 set(EP_ms2_GIT_TAG_LATEST "master")
-set(EP_ms2_GIT_TAG "bd145603878f063f4c28406219e96381217fb3c7")
+set(EP_ms2_GIT_TAG "9d600a34ebf0a7c7a94cac36898f4fbca0dcd09a")
 
 set(EP_ms2_CMAKE_OPTIONS )
 set(EP_ms2_LINKING_TYPE "-DENABLE_STATIC=NO")
