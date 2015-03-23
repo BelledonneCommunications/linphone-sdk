32903610a08f76f6353178c63692175b525dab1a
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2015-03-23 16:29:42 +0100

    Fix build of sqlite3 and xml2 for Windows Phone.

---
+++ builders/xml2/windowsphone_port.h
@@ -0,0 +1,19 @@
+
+#ifndef WINDOWSPHONE_PORT_H
+#define WINDOWSPHONE_PORT_H
+
+#define CreateMutex(a, b, c)	CreateMutexExW(a, c, ((b) ? CREATE_MUTEX_INITIAL_OWNER : 0), 0)
+
+#define GetVersionEx(osvi)		(((osvi)->dwPlatformId = 0) != 0)
+
+#define InitializeCriticalSection(cs)	InitializeCriticalSectionEx(cs, 0, 0)
+
+#define WaitForSingleObject(hHandle, dwMilliseconds)	WaitForSingleObjectEx(hHandle, dwMilliseconds, 0)
+
+#define Sleep(ms)	{ \
+	HANDLE sleepEvent = CreateEventEx(NULL, NULL, CREATE_EVENT_MANUAL_RESET, EVENT_ALL_ACCESS); \
+	if (!sleepEvent) return; \
+	WaitForSingleObjectEx(sleepEvent, ms, FALSE); \
+}
+
+#endif /* WINDOWSPHONE_PORT_H */
