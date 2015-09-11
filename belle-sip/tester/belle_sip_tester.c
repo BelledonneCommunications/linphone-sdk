df2f731e4ec4e262d9eb835fd7ddfb2794f5379e
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-09-11 15:22:26 +0200

    tester.c: fix (again...) resource directory path

--- belle-sip/tester/belle_sip_tester.c
+++ belle-sip/tester/belle_sip_tester.c
@@ -131,7 +131,9 @@ int main (int argc, char *argv[]) {
 
 	// this allows to launch liblinphone_tester from outside of tester directory
 	if (strstr(argv[0], ".libs")) {
-		char* prefix = belle_sip_strdup_printf("%s%s", argv[0][0]=='/'?"":"./", strstr(argv[0], ".libs"));
+		int prefix_length = strstr(argv[0], ".libs") - argv[0] + 1;
+		char *prefix = belle_sip_strdup_printf("%s%.*s", argv[0][0] == '/' ? "" : "./", prefix_length, argv[0]);
+		belle_sip_warning("Resource prefix set to %s", prefix);
 		bc_tester_set_resource_dir_prefix(prefix);
 		bc_tester_set_writable_dir_prefix(prefix);
 		belle_sip_free(prefix);
