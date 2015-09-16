47ee22073168e17e833a8516cc36644c523fe419
François Grisez <francois.grisez@belledonne-communications.com>
2015-09-16 14:52:34 +0200

    Add an option to CMake Builder to enable/disable NLS

---
+++ options/linphone.cmake
@@ -0,0 +1,2 @@
+option(ENABLE_NLS "Enable internationalization of Linphone and Liblinphone." ON)
+add_feature_info("NLS" ENABLE_NLS "Enable internationalization of Linphone and Liblinphone. (Only for the desktop target)")
