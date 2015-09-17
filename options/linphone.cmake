c498d65cf74afd8cce7ad50d195d1ae445cdd118
François Grisez <francois.grisez@belledonne-communications.com>
2015-09-17 09:29:41 +0200

    Add the ENABLE_RELATIVE_PREFIX to CMakeBuilder

--- options/linphone.cmake
+++ options/linphone.cmake
@@ -1,2 +1,5 @@
 option(ENABLE_NLS "Enable internationalization of Linphone and Liblinphone." ON)
 add_feature_info("NLS" ENABLE_NLS "Enable internationalization of Linphone and Liblinphone. (Only for the desktop target)")
+
+option(ENABLE_RELATIVE_PREFIX "Makes liblinphone and mediastreamer look for their ressources relatively to their location. (MacOSX only)" OFF)
+add_feature_info("Relative prefix" ENABLE_RELATIVE_PREFIX "Makes liblinphone and mediastreamer look for their ressources relatively to their location. (MacOSXonly)")
