ac4d43dc911de8f38da0cca2af78819d54c7db46
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-09-22 15:01:35 +0200

    linphone.cmake: ENABLE_RELATIVE_PREFIX is specific to macosx, it should never be shown otherwise

--- options/linphone.cmake
+++ options/linphone.cmake
@@ -1,5 +1,7 @@
 option(ENABLE_NLS "Enable internationalization of Linphone and Liblinphone." ON)
 add_feature_info("NLS" ENABLE_NLS "Enable internationalization of Linphone and Liblinphone. (Only for the desktop target)")
 
-option(ENABLE_RELATIVE_PREFIX "Makes liblinphone and mediastreamer look for their ressources relatively to their location. (MacOSX only)" OFF)
-add_feature_info("Relative prefix" ENABLE_RELATIVE_PREFIX "Makes liblinphone and mediastreamer look for their ressources relatively to their location. (MacOSXonly)")
+if (APPLE AND NOT IOS)
+	option(ENABLE_RELATIVE_PREFIX "liblinphone and mediastreamer will look for their respective ressources relatively to their location." OFF)
+	add_feature_info("Relative prefix" ENABLE_RELATIVE_PREFIX "liblinphone and mediastreamer will look for their respective ressources relatively to their location.")
+endif()
