20861b06847109d3c9489eef6998eceaba85bd9a
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2015-12-16 11:13:18 +0100

    Added license at top of each file

--- belcard/tests/belcard-parser.cpp
+++ belcard/tests/belcard-parser.cpp
@@ -1,3 +1,21 @@
+/*
+	belcard-parser.cpp
+	Copyright (C) 2015  Belledonne Communications SARL
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "belcard/belcard_parser.hpp"
 
 #include <iostream>
@@ -17,17 +35,8 @@ int main(int argc, char *argv[]) {
 	}
 	file = argv[1];
 	
-	ifstream istr(file);
-	if (!istr.is_open()) {
-		return -1;
-	}
-	
-	stringstream vcardStream;
-	vcardStream << istr.rdbuf();
-	string vcard = vcardStream.str();
-	
 	BelCardParser *parser = new BelCardParser();
-	shared_ptr<BelCardList> belCards = parser->parse(vcard);
+	shared_ptr<BelCardList> belCards = parser->parseFile(file);
 	cout << *belCards << endl;
 	
 	delete parser;
