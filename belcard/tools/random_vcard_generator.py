fd27d7785fa02a158696fd8f971550787772f12b
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2015-12-17 13:57:56 +0100

    Generated vcards are now more random

--- belcard/tools/random_vcard_generator.py
+++ belcard/tools/random_vcard_generator.py
@@ -5,6 +5,7 @@ from barnum import gen_data
 import codecs
 import frogress
 import random
+from random import shuffle
 
 gender_vcard_list = ['M', 'F', 'O', 'N', 'U']
 
@@ -19,19 +20,33 @@ def generate_vCard():
 	(first_name, last_name) = gen_data.create_name(gender=gender)
 	adr = gen_data.create_street()
 	zip, city, state = gen_data.create_city_state_zip()
+
+	properties = []
+	properties.append('FN:{} {}\r\n'.format(first_name, last_name))
+	if random.randint(0, 1):
+		properties.append('N:{};{};;;\r\n'.format(last_name, first_name))
+	if random.randint(0, 1):
+		properties.append('TEL:tel:{}\r\n'.format(gen_data.create_phone()))
+	if random.randint(0, 1):
+		properties.append('GENDER:{}\r\n'.format(gender_initial))
+	if random.randint(0, 1):
+		properties.append('EMAIL:{}\r\n'.format(gen_data.create_email(name=(first_name, last_name)).lower()))
+	if random.randint(0, 1):
+		properties.append('IMPP:sip:{}@{}\r\n'.format(first_name.lower(), 'sip.linphone.org'))
+	if random.randint(0, 1):
+		properties.append('ADR:;;{};{};{};{};\r\n'.format(adr, city, state, zip))
+	if random.randint(0, 1):
+		properties.append('NOTE:{}\r\n'.format(gen_data.create_sentence()))
+	if random.randint(0, 1):
+		properties.append('ORG:{}\r\n'.format(gen_data.create_company_name()))
+	if random.randint(0, 1):
+		properties.append('BDAY:{0:%Y%m%d}\r\n'.format(gen_data.create_birthday()))
 	
+	shuffle(properties)
 	vCard = 'BEGIN:VCARD\r\n'
 	vCard += 'VERSION:4.0\r\n'
-	vCard += 'FN:{} {}\r\n'.format(first_name, last_name)
-	vCard += 'N:{};{};;;\r\n'.format(last_name, first_name)
-	vCard += 'TEL:tel:{}\r\n'.format(gen_data.create_phone())
-	vCard += 'GENDER:{}\r\n'.format(gender_initial)
-	vCard += 'EMAIL:{}\r\n'.format(gen_data.create_email(name=(first_name, last_name)).lower())
-	vCard += 'IMPP:sip:{}@{}\r\n'.format(first_name.lower(), 'sip.linphone.org')
-	vCard += 'ADR:;;{};{};{};{};\r\n'.format(adr, city, state, zip)
-	vCard += 'NOTE:{}\r\n'.format(gen_data.create_sentence())
-	vCard += 'ORG:{}\r\n'.format(gen_data.create_company_name())
-	vCard += 'BDAY:{0:%Y%m%d}\r\n'.format(gen_data.create_birthday())
+	for property in properties:
+		vCard += property
 	vCard += 'END:VCARD\r\n'
 	return vCard
 
