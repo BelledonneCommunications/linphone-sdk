4a4bb7863910483c06a27c0f4e95013400aeb487
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2016-01-15 15:52:54 +0100

    Fix Visual Studio compil

--- belcard/src/belcard_general.cpp
+++ belcard/src/belcard_general.cpp
@@ -16,7 +16,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "belcard/belcard_general.hpp"
+#include "belcard/belcard.hpp"
 
 using namespace::std;
 using namespace::belr;
--- belcard/src/belcard_rfc6474.cpp
+++ belcard/src/belcard_rfc6474.cpp
@@ -16,7 +16,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "belcard/belcard_rfc6474.hpp"
+#include "belcard/belcard.hpp"
 
 using namespace::std;
 using namespace::belr;
