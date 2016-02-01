c5670d15a33d91a6d01084b8e2d8f22d2d20161f
Sandrine Avakian <sandrine.avakian@belledonne-communications.com>
2016-02-01 16:02:42 +0100

    Renaming update : ms_snd_card_factory_get to ms_snd_card_get_factory.

--- mswasapi/mswasapi.cpp
+++ mswasapi/mswasapi.cpp
@@ -621,7 +621,7 @@ static void ms_wasapi_snd_card_detect(MSSndCardManager *m) {
 }
 
 static MSFilter *ms_wasapi_snd_card_create_reader(MSSndCard *card) {
-	MSFilter *f = ms_factory_create_filter_from_desc(ms_snd_card_factory_get(card), &ms_wasapi_read_desc);
+	MSFilter *f = ms_factory_create_filter_from_desc(ms_snd_card_get_factory(card), &ms_wasapi_read_desc);
 	WasapiSndCard *wasapicard = static_cast<WasapiSndCard *>(card->data);
 	MSWASAPIReaderType reader = MSWASAPI_READER(f->data);
 	reader->init(wasapicard->id);
@@ -629,7 +629,7 @@ static MSFilter *ms_wasapi_snd_card_create_reader(MSSndCard *card) {
 }
 
 static MSFilter *ms_wasapi_snd_card_create_writer(MSSndCard *card) {
-	MSFilter *f = ms_factory_create_filter_from_desc(ms_snd_card_factory_get(card), &ms_wasapi_write_desc);
+	MSFilter *f = ms_factory_create_filter_from_desc(ms_snd_card_get_factory(card), &ms_wasapi_write_desc);
 	WasapiSndCard *wasapicard = static_cast<WasapiSndCard *>(card->data);
 	MSWASAPIWriterType writer = MSWASAPI_WRITER(f->data);
 	writer->init(wasapicard->id);
