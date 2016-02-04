b5c0464f749e87e01b20ed75067e6ba8691be1c6
Johan Pascal <johan.pascal@belledonne-communications.com>
2016-02-04 15:09:13 +0100

    improve CMakeList to have a config.h

---
+++ bctoolbox/config.h.cmake
@@ -0,0 +1,32 @@
+/***************************************************************************
+* config.h.cmake
+* Copyright (C) 2014  Belledonne Communications, Grenoble France
+*
+****************************************************************************
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+****************************************************************************/
+
+#cmakedefine PACKAGE "@PACKAGE@"
+#cmakedefine PACKAGE_NAME "@PACKAGE_NAME@"
+#cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
+#cmakedefine PACKAGE_STRING "@PACKAGE_STRING@"
+#cmakedefine PACKAGE_TARNAME "@PACKAGE_TARNAME@"
+#cmakedefine PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
+#cmakedefine PACKAGE_URL "@PACKAGE_URL@"
+#cmakedefine VERSION "@VERSION@"
+
+#cmakedefine HAVE_DTLS_SRTP
