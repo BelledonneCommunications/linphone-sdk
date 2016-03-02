d1cd0df7bf8af21586e5e10722f2a1a4e01a338b
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2016-03-02 14:43:10 +0100

    config.h.cmake: remove unused defines

--- belle-sip/config.h.cmake
+++ belle-sip/config.h.cmake
@@ -37,10 +37,6 @@
 #cmakedefine HAVE_ANTLR3_H
 #cmakedefine HAVE_ANTLR_STRING_STREAM_NEW
 
-#cmakedefine HAVE_CUNIT_CUNIT_H
-#cmakedefine HAVE_CU_CURSES
-#cmakedefine HAVE_CU_GET_SUITE
-
 #cmakedefine HAVE_TUNNEL
 #cmakedefine HAVE_ZLIB
 
