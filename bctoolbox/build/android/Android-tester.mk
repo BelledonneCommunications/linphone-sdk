3f3d977d00634feeb6472237c2501f75493a52b3
Simon Morlat <simon.morlat@linphone.org>
2016-03-08 14:56:03 +0100

    fix android support

--- bctoolbox/build/android/Android-tester.mk
+++ bctoolbox/build/android/Android-tester.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE:= libbctoolbox_tester
 LOCAL_SRC_FILES := \
 	tester/utils.c
 
-LOCAL_CFLAGS += -Wno-maybe-uninitialized -DIN_CUNIT_SOURCES
+LOCAL_CFLAGS += -Wno-maybe-uninitialized -DIN_CUNIT_SOURCES -DHAVE_CU_SET_TRACE_HANDLER=1 -DHAVE_CU_GET_SUITE=1
 
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/ \
