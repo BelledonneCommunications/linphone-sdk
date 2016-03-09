0886932d9cb2422cfc0f2103d0deaec8cd197e6e
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2016-03-09 16:23:31 +0100

    win32: use _WIN32 not WIN32

--- mswebrtc/aec.c
+++ mswebrtc/aec.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <echo_control_mobile.h>
 #include "ortp/b64.h"
 
-#ifdef WIN32
+#ifdef _WIN32
 #include <malloc.h> /* for alloca */
 #endif
 
