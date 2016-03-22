66cde8d67fa0b0055ef96863e69e6a7abc71c251
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2016-03-22 13:31:04 +0100

    belle_sip_refresher_tester.c: fix compilation

--- belle-sip/tester/belle_sip_refresher_tester.c
+++ belle-sip/tester/belle_sip_refresher_tester.c
@@ -444,8 +444,8 @@ static void refresher_base_with_body(endpoint_t* client
 	begin = belle_sip_time_ms();
 	BC_ASSERT_TRUE(wait_for(server->stack,client->stack,&client->stat.refreshOk,client->register_count+(client->early_refresher?1:0),client->register_count*1000 + 1000));
 	end = belle_sip_time_ms();
-	BC_ASSERT_GREATER(end-begin,client->register_count*1000*.9,uint64_t,"%llu"); /*because refresh is at 90% of expire*/
-	BC_ASSERT_LOWER_STRICT(end-begin,(client->register_count*1000 + 2000),uint64_t,"%llu");
+	BC_ASSERT_GREATER(end-begin,client->register_count*1000*.9,long,"%ld"); /*because refresh is at 90% of expire*/
+	BC_ASSERT_LOWER_STRICT(end-begin,(client->register_count*1000 + 2000),long,"%ld");
 	/*unregister twice to make sure refresh operation can be safely cascaded*/
 	belle_sip_refresher_refresh(refresher,0);
 	belle_sip_refresher_refresh(refresher,0);
@@ -576,8 +576,8 @@ static void subscribe_base(int with_resource_lists) {
 	begin = belle_sip_time_ms();
 	BC_ASSERT_TRUE(wait_for(server->stack,client->stack,&client->stat.refreshOk,3,4000));
 	end = belle_sip_time_ms();
-	BC_ASSERT_GREATER(end-begin,3000*.9,uint64_t,"%llu");
-	BC_ASSERT_LOWER_STRICT(end-begin,5000,uint64_t,"%llu");
+	BC_ASSERT_GREATER(end-begin,3000*.9,long,"%ld");
+	BC_ASSERT_LOWER_STRICT(end-begin,5000,long,"%ld");
 
 	belle_sip_message("simulating dialog error and recovery");
 	belle_sip_stack_set_send_error(client->stack, 1500);
