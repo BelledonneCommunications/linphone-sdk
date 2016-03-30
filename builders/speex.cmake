38c55f449d4ce8afcbfeb05e1f5066056727c904
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2016-03-30 12:43:22 +0200

    Always build libspeexdsp but keep choice of building the speex codec.

--- builders/speex.cmake
+++ builders/speex.cmake
@@ -22,10 +22,11 @@
 
 set(EP_speex_GIT_REPOSITORY "git://git.linphone.org/speex.git" CACHE STRING "speex repository URL")
 set(EP_speex_GIT_TAG_LATEST "linphone" CACHE STRING "speex tag to use when compiling latest version")
-set(EP_speex_GIT_TAG "caeb73052b1171758acb565e7174a49fbd1427d2" CACHE STRING "speex tag to use")
+set(EP_speex_GIT_TAG "fc1dd43c3c9d244bca1c300e408ce0373dbd5ed8" CACHE STRING "speex tag to use")
 set(EP_speex_EXTERNAL_SOURCE_PATHS "speex" "externals/speex")
 
 set(EP_speex_LINKING_TYPE ${DEFAULT_VALUE_CMAKE_LINKING_TYPE})
+set(EP_speex_CMAKE_OPTIONS "-DENABLE_SPEEX_DSP=YES" "-DENABLE_SPEEX_CODEC=${ENABLE_SPEEX}")
 if(MSVC)
 	set(EP_speex_EXTRA_LDFLAGS "/SAFESEH:NO")
 endif()
