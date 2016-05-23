179df26359e81706267137692a42b90912995c35
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2016-05-23 14:17:51 +0200

    Prevent CMake from using -isystem instead of -I for include directories of targets imported by find_package().

--- prepare.py
+++ prepare.py
@@ -65,6 +65,7 @@ class Target:
             cmd += ['-A', self.platform_name]
         cmd += ['-DCMAKE_BUILD_TYPE=' + build_type]
         cmd += ['-DCMAKE_PREFIX_PATH=' + self.output_dir(), '-DCMAKE_INSTALL_PREFIX=' + self.output_dir()]
+        cmd += ['-DCMAKE_NO_SYSTEM_FROM_IMPORTED=YES']
         cmd += ['-DLINPHONE_BUILDER_WORK_DIR=' + self.abs_work_dir]
         if self.toolchain_file is not None:
             cmd += ['-DCMAKE_TOOLCHAIN_FILE=' + self.toolchain_file]
