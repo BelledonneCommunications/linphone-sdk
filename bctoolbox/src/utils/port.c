cfec33aabb7d30dd48c1031cb7894178b75cfb7a
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2016-07-22 10:52:47 +0200

    Fix build on Mac OS X.

--- bctoolbox/src/utils/port.c
+++ bctoolbox/src/utils/port.c
@@ -1238,6 +1238,8 @@ void bctbx_sockaddr_remove_nat64_mapping(const struct sockaddr *v6, struct socka
 #ifdef _MSC_VER
 		if ((((in6->sin6_addr.u.Word[0] << 16) & in6->sin6_addr.u.Word[1]) == htonl(0x0064))
 			&& (((in6->sin6_addr.u.Word[2] << 16) & in6->sin6_addr.u.Word[3]) == htonl(0xff9b))) {
+#elif __APPLE__
+		if ((in6->sin6_addr.__u6_addr.__u6_addr32[0] == htonl(0x0064)) && (in6->sin6_addr.__u6_addr.__u6_addr32[1] == htonl(0xff9b))) {
 #else
 		if ((in6->sin6_addr.s6_addr32[0] == htonl(0x0064)) && (in6->sin6_addr.s6_addr32[1] == htonl(0xff9b))) {
 #endif
