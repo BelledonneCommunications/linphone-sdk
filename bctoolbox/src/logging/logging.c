1d2ffc0fa6f8856284d19c7f625be5ea74abb607
Benjamin Reis <benjamin.reis@belledonne-communications.com>
2017-03-30 17:14:07 +0200

    change include

--- bctoolbox/src/logging/logging.c
+++ bctoolbox/src/logging/logging.c
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "bctoolbox/logging.h"
 #include <time.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
