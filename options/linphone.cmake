270a5b24f22ba9c715c391d25ac53d181c299597
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2017-10-11 15:32:35 +0200

    Added Java wrapper option

--- options/linphone.cmake
+++ options/linphone.cmake
@@ -25,6 +25,7 @@
 lcb_add_option("GTK UI" "Enable the GTK user interface of Linphone." "${DEFAULT_VALUE_ENABLE_GTK_UI}")
 lcb_add_option("CXX_WRAPPER" "Build the C++ wrapper for Liblinphone." "${DEFAULT_VALUE_ENABLE_CXX_WRAPPER}")
 lcb_add_option("CSHARP_WRAPPER" "Build the C# wrapper from Liblinphone." "${DEFAULT_VALUE_ENABLE_CSHARP_WRAPPER}")
+lcb_add_option("JAVA_WRAPPER" "Build the Java wrapper from Liblinphone." "${DEFAULT_VALUE_ENABLE_JAVA_WRAPPER}")
 lcb_add_option("LIME" "Enable Linphone IM Encryption support in  Liblinphone." "${DEFAULT_VALUE_ENABLE_LIME}")
 lcb_add_option("NLS" "Enable internationalization of Linphone and Liblinphone." "${DEFAULT_VALUE_ENABLE_NLS}")
 lcb_add_option("Update Check" "Enable update check." "${DEFAULT_VALUE_ENABLE_UPDATE_CHECK}")
