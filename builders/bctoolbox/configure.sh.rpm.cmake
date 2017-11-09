696e736232353db5bb057bc00a08695d760734e3
Jehan Monnier <jehan.monnier@linphone.org>
2017-11-09 11:26:00 +0100

    fix bctbx rpm build for flexisip

--- builders/bctoolbox/configure.sh.rpm.cmake
+++ builders/bctoolbox/configure.sh.rpm.cmake
@@ -1,29 +1,5 @@
 #!/bin/sh
 
-if [ -n "@AUTOTOOLS_AS_COMPILER@" ]
-then
-	export AS="@AUTOTOOLS_AS_COMPILER@"
-fi
-export CC="@AUTOTOOLS_C_COMPILER@"
-export CXX="@AUTOTOOLS_CXX_COMPILER@"
-export OBJC="@AUTOTOOLS_OBJC_COMPILER@"
-export LD="@AUTOTOOLS_LINKER@"
-export AR="@AUTOTOOLS_AR@"
-export RANLIB="@AUTOTOOLS_RANLIB@"
-export STRIP="@AUTOTOOLS_STRIP@"
-export NM="@AUTOTOOLS_NM@"
-
-export ASFLAGS="@ep_asflags@"
-export CPPFLAGS="@ep_cppflags@"
-export CFLAGS="@ep_cflags@"
-export CXXFLAGS="@ep_cxxflags@"
-export OBJCFLAGS="@ep_objcflags@"
-export LDFLAGS="@ep_ldflags@"
-
-export PKG_CONFIG="@LINPHONE_BUILDER_PKG_CONFIG@"
-export PKG_CONFIG_PATH="@LINPHONE_BUILDER_PKG_CONFIG_PATH@"
-export PKG_CONFIG_LIBDIR="@LINPHONE_BUILDER_PKG_CONFIG_LIBDIR@"
-
 export RPM_TOPDIR="@LINPHONE_BUILDER_WORK_DIR@/rpmbuild"
 
 
