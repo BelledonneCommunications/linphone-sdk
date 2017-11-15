cc58ec55f47e094c187097d55b77829d665dc4d3
Simon Morlat <simon.morlat@linphone.org>
2017-11-15 14:20:08 +0100

    add x86_64 toolchain file

---
+++ toolchains/toolchain-android-x86_64.cmake
@@ -0,0 +1,26 @@
+############################################################################
+# toolchain-android-x86.cmake
+# Copyright (C) 2016  Belledonne Communications, Grenoble France
+#
+############################################################################
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+############################################################################
+
+set(CMAKE_ANDROID_API 21)
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION "clang")
+include("${CMAKE_CURRENT_LIST_DIR}/android/toolchain-android.cmake")
