d8b3bbd844c900219e4f052dae5c78d08266d0a3
Johan Pascal <johan.pascal@belledonne-communications.con>
2017-12-12 08:53:40 +0700

    Fix test server package +add copyright message on it

--- lime/tester/server/x3dh.js
+++ lime/tester/server/x3dh.js
@@ -1,3 +1,22 @@
+/*
+	x3dh node server
+	author Johan Pascal
+	copyright 	Copyright (C) 2017  Belledonne Communications SARL
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // parse arguments to find port to listen and path to DB
 // db path is mandatory
 // port default to 25519
@@ -56,7 +75,7 @@ const yargs = require('yargs')
 	.argv;
 
 const https = require('https');
-var auth = require('http-auth');
+const auth = require('http-auth');
 var digest = auth.digest({
 	realm: "limeTester",
 	file: yargs.resource_dir+"/"+yargs.passwords
