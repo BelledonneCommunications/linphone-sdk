13ef108c9e14364872ccefcfcdca0d906449da6d
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-03-08 12:04:32 +0100

    Add minizip builder.

---
+++ builders/minizip.cmake
@@ -0,0 +1,33 @@
+############################################################################
+# minizip.cmake
+# Copyright (C) 2018  Belledonne Communications, Grenoble France
+#
+############################################################################
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+############################################################################
+
+lcb_git_repository("git://git.linphone.org/minizip.git")
+lcb_git_tag_latest("master")
+lcb_git_tag("d65cd2ea9d740f62884e0beaf8ab86740620c783")
+lcb_external_source_paths("externals/minizip")
+
+lcb_dependencies("zlib")
+
+lcb_cmake_options(
+	"-DUSE_AES=NO"
+)
+
