decf0a95b8bb6da15b1ad82faeeeaf440ae90a41
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-03-12 12:05:13 +0100

    Handle RPM packaging.

---
+++ mswebrtc/build/rpm/mswebrtc.spec.cmake
@@ -0,0 +1,73 @@
+# -*- rpm-spec -*-
+
+%define _prefix    @CMAKE_INSTALL_PREFIX@
+%define pkg_prefix @BC_PACKAGE_NAME_PREFIX@
+
+# re-define some directories for older RPMBuild versions which don't. This messes up the doc/ dir
+# taken from https://fedoraproject.org/wiki/Packaging:RPMMacros?rd=Packaging/RPMMacros
+%define _datarootdir       %{_prefix}/share
+%define _datadir           %{_datarootdir}
+%define _docdir            %{_datadir}/doc
+
+%define build_number @PROJECT_VERSION_BUILD@
+%if %{build_number}
+%define build_number_ext -%{build_number}
+%endif
+
+
+
+Name:           @CPACK_PACKAGE_NAME@
+Version:        @PROJECT_VERSION@
+Release:        %{build_number}%{?dist}
+Summary:        mediastreamer2 plugin to include features from WebRTC (iSAC codec, AECM...).
+
+Group:          Applications/Communications
+License:        GPL
+URL:            http://www.linphone.org
+Source0:        %{name}-%{version}%{?build_number_ext}.tar.gz
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-buildroot
+
+Requires:	%{pkg_prefix}bctoolbox
+
+%description
+
+mediastreamer2 (linphone) plugin to include features from WebRTC (iSAC codec, AECM...)
+
+%if 0%{?rhel} && 0%{?rhel} <= 7
+%global cmake_name cmake3
+%define ctest_name ctest3
+%else
+%global cmake_name cmake
+%define ctest_name ctest
+%endif
+
+# This is for debian builds where debug_package has to be manually specified, whereas in centos it does not
+%define custom_debug_package %{!?_enable_debug_packages:%debug_package}%{?_enable_debug_package:%{nil}}
+%custom_debug_package
+
+%prep
+%setup -n %{name}-%{version}%{?build_number_ext}
+
+%build
+%{expand:%%%cmake_name} . -DCMAKE_BUILD_TYPE=@CMAKE_BUILD_TYPE@ -DCMAKE_INSTALL_LIBDIR:PATH=%{_libdir} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} @RPM_ALL_CMAKE_OPTIONS@
+make %{?_smp_mflags}
+
+%install
+make install DESTDIR=%{buildroot}
+
+%check
+%{ctest_name} -V %{?_smp_mflags}
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%post -p /sbin/ldconfig
+
+%postun -p /sbin/ldconfig
+
+
+%files
+%defattr(-,root,root)
+%doc AUTHORS ChangeLog COPYING NEWS README
+%{_libdir}/mediastreamer/plugins/*.so
+
