41e089d1c847d20b8a541acb6d8c39924bd7b30c
Erwan Croze <erwan.croze@belledonne-communications.com>
2018-07-03 16:40:14 +0200

    Migrate mswasapi repo

--- builders/mswasapi.cmake
+++ builders/mswasapi.cmake
@@ -20,7 +20,7 @@
 #
 ############################################################################
 
-lcb_git_repository("git://git.linphone.org/mswasapi.git")
+lcb_git_repository("https://gitlab.linphone.org/BC/public/mswasapi.git")
 lcb_git_tag_latest("master")
 lcb_git_tag("1.1.0")
 lcb_external_source_paths("mswasapi")
