d63af212b0452ea319e4848b6c39d474623f7a02
Erwan Croze <erwan.croze@belledonne-communications.com>
2018-10-02 09:19:20 +0200

    Fix bctbx exception

--- bctoolbox/include/bctoolbox/exception.hh
+++ bctoolbox/include/bctoolbox/exception.hh
@@ -1,17 +1,17 @@
 /*
 	bctoolbox
 	Copyright (C) 2016  Belledonne Communications SARL.
- 
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as
 	published by the Free Software Foundation, either version 3 of the
 	License, or (at your option) any later version.
- 
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
- 
+
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -50,7 +50,8 @@ public:
 	const char *what() const noexcept override;
 
 	std::string str() const {
-		return mOs.str();
+		mMessage = mOs.str();
+		return mMessage;
 	}
 
 	/* same as osstringstream, but as osstream does not have cp contructor, BctbxException can't inherit from
@@ -64,6 +65,7 @@ private:
 	void *mArray[20];
 	size_t mSize;
 	std::ostringstream mOs;
+	mutable std::string mMessage;
 };
 std::ostream &operator<<(std::ostream &__os, const BctbxException &e);
 
