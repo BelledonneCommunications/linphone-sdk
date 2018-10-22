00a7a94f4496015409c7eeb90892633424ef36fc
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-10-22 10:44:47 +0200

    Split desktop config for linphone-sdk integration.

---
+++ configs/options-desktop.cmake
@@ -0,0 +1,50 @@
+############################################################################
+# options-desktop.cmake
+# Copyright (C) 2010-2018 Belledonne Communications, Grenoble France
+#
+############################################################################
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+############################################################################
+
+# Define default values for the linphone builder options
+set(DEFAULT_VALUE_ENABLE_BV16 ON)
+set(DEFAULT_VALUE_ENABLE_CXX_WRAPPER ON)
+set(DEFAULT_VALUE_ENABLE_FFMPEG ON)
+set(DEFAULT_VALUE_ENABLE_G729 ${DEFAULT_VALUE_ENABLE_GPL_THIRD_PARTIES})
+set(DEFAULT_VALUE_ENABLE_G729B_CNG OFF)
+set(DEFAULT_VALUE_ENABLE_GPL_THIRD_PARTIES ON)
+set(DEFAULT_VALUE_ENABLE_GSM ON)
+set(DEFAULT_VALUE_ENABLE_GTK_UI OFF)
+set(DEFAULT_VALUE_ENABLE_JPEG ON)
+set(DEFAULT_VALUE_ENABLE_LIME OFF)
+set(DEFAULT_VALUE_ENABLE_LIME_X3DH OFF)
+set(DEFAULT_VALUE_ENABLE_MBEDTLS ON)
+set(DEFAULT_VALUE_ENABLE_MKV ON)
+set(DEFAULT_VALUE_ENABLE_NLS OFF)
+set(DEFAULT_VALUE_ENABLE_OPUS ON)
+set(DEFAULT_VALUE_ENABLE_SPEEX ON)
+set(DEFAULT_VALUE_ENABLE_SRTP ON)
+set(DEFAULT_VALUE_ENABLE_TOOLS OFF)
+if(NOT LINPHONE_BUILDER_ENABLE_RPM_PACKAGING)
+	set(DEFAULT_VALUE_ENABLE_UNIT_TESTS ON)
+endif()
+set(DEFAULT_VALUE_ENABLE_VCARD ON)
+set(DEFAULT_VALUE_ENABLE_VIDEO ON)
+set(DEFAULT_VALUE_ENABLE_VPX ON)
+set(DEFAULT_VALUE_ENABLE_WASAPI ON)
+set(DEFAULT_VALUE_ENABLE_WEBRTC_AEC ON)
+set(DEFAULT_VALUE_ENABLE_ZRTP ON)
