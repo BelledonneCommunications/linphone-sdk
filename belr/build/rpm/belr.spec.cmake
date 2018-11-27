1b98058f869b7e26a63aa96e7d0ae3bec9684f72
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2018-11-27 13:23:10 +0100

    chore(build): do not set CMAKE_INSTALL_LIBDIR and never with _libdir!

--- belr/build/rpm/belr.spec.cmake
+++ belr/build/rpm/belr.spec.cmake
@@ -62,7 +62,7 @@ Libraries and headers required to develop software with belr
 %setup -n %{name}-%{version}%{?build_number_ext}
 
 %build
-%{expand:%%%cmake_name} . -DCMAKE_BUILD_TYPE=@CMAKE_BUILD_TYPE@ -DCMAKE_INSTALL_LIBDIR:PATH=%{_libdir} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} @RPM_ALL_CMAKE_OPTIONS@
+%{expand:%%%cmake_name} . -DCMAKE_BUILD_TYPE=@CMAKE_BUILD_TYPE@ -DCMAKE_PREFIX_PATH:PATH=%{_prefix} @RPM_ALL_CMAKE_OPTIONS@
 make %{?_smp_mflags}
 
 %install
@@ -100,5 +100,9 @@ rm -rf $RPM_BUILD_ROOT
 %endif
 
 %changelog
+
+* Tue Nov 27 2018 ronan.abhamon <ronan.abhamon@belledonne-communications.com>
+- Do not set CMAKE_INSTALL_LIBDIR and never with _libdir!
+
 * Wed Jul 19 2017 jehan.monnier <jehan.monnier@linphone.org>
 - Initial RPM release.
