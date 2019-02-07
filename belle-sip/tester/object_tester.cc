c551f01b58d364ccf3ad09126a49380bf77cfd2e
Simon Morlat <simon.morlat@linphone.org>
2019-02-07 10:04:54 +0100

    Fix.

--- belle-sip/tester/object_tester.cc
+++ belle-sip/tester/object_tester.cc
@@ -15,7 +15,7 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-                                                                                            */
+
 
 #include "belle-sip/belle-sip.h"
 #include "belle_sip_tester.h"
