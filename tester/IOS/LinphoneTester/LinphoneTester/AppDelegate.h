f3930a702d8d4478364374d40467d6b2ec410829
Danmei Chen <danmei.chen@belledonne-communications.com>
2019-02-13 13:51:20 +0100

    add ios tester

---
+++ tester/IOS/LinphoneTester/LinphoneTester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinphoneTester
+//
+//  Created by Danmei Chen on 13/02/2019.
+//  Copyright © 2019 belledonne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tester/IOS/LinphoneTester/LinphoneTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LinphoneTester
+//
+//  Created by Danmei Chen on 13/02/2019.
+//  Copyright © 2019 belledonne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
