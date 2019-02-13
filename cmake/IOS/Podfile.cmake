32d1abdeff27fdc018b5ac351b16ac502984c1f5
Danmei Chen <danmei.chen@belledonne-communications.com>
2019-02-13 14:14:03 +0100

    add Podfile for ios tester

---
+++ cmake/IOS/Podfile.cmake
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'LinphoneTester' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LinphoneTester
+  pod 'linphone-sdk', :path => "@LINPHONESDK_BUILD_DIR@/"
+
+  target 'LinphoneTesterTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
