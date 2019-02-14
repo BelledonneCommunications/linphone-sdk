f994a97edcd0273e5c470bc31d23ac674fb6fe3d
Danmei Chen <danmei.chen@belledonne-communications.com>
2019-02-14 11:52:09 +0100

    add files to unitest

--- tester/IOS/LinphoneTester/LinphoneTester/ViewController.m
+++ tester/IOS/LinphoneTester/LinphoneTester/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "Log.h"
+#include "TargetConditionals.h"
+#include "linphonetester/liblinphone_tester.h"
+
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
@@ -17,6 +20,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    liblinphone_tester_init(NULL);
+    [Log enableLogs:ORTP_DEBUG];
+    liblinphone_tester_keep_accounts(TRUE);
+    
+    NSString *bundlePath = [[NSBundle mainBundle] bundlePath];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *writablePath = [paths objectAtIndex:0];
+    
+    bc_tester_set_resource_dir_prefix([bundlePath UTF8String]);
+    bc_tester_set_writable_dir_prefix([writablePath UTF8String]);
+    
+   // LOGI(@"Bundle path: %@", bundlePath);
+   // LOGI(@"Writable path: %@", writablePath);
+    
 }
 
 
