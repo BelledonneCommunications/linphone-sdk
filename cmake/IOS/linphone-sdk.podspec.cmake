4740f4eff2f2f6f9f2ad2b2433b294ba0baec75e
Danmei Chen <danmei.chen@belledonne-communications.com>
2019-09-04 14:46:11 +0200

    fix resources

--- cmake/IOS/linphone-sdk.podspec.cmake
+++ cmake/IOS/linphone-sdk.podspec.cmake
@@ -14,8 +14,8 @@ Pod::Spec.new do |s|
   s.source       = { :http => "@LINPHONESDK_IOS_BASE_URL@/linphone-sdk-ios-@LINPHONESDK_VERSION@.zip" }
   s.vendored_frameworks = "linphone-sdk/apple-darwin/Frameworks/**"
   s.pod_target_xcconfig = { 'VALID_ARCHS' => "@VALID_ARCHS@" }
+  s.resource = "linphone-sdk/apple-darwin/Resources/**"
   s.subspec '@LINPHONE_SUBSPEC_NAME@' do |basic|
     basic.vendored_frameworks = "linphone-sdk/apple-darwin/Frameworks/{@LINPHONE_SUBSPEC_FRAMEWORKS@}"
   end
-
 end
