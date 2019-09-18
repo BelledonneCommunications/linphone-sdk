2fc22480fb7f7a8fac6c85434c45dec765e5e652
Simon Morlat <simon.morlat@belledonne-communications.com>
2019-09-18 15:15:59 +0200

    Change license for a few files.

--- cmake/scripts/gas-preprocessor.pl
+++ cmake/scripts/gas-preprocessor.pl
@@ -1,8 +1,22 @@
 #!/usr/bin/env perl
-# by David Conrad
-# This code is licensed under GPLv2 or later; go to gnu.org to read it
-#  (not that it much matters for an asm preprocessor)
-# usage: set your assembler to be something like "perl gas-preprocessor.pl gcc"
+##
+## Copyright (c) 2012-2019 Belledonne Communications SARL.
+##
+## This file is part of linphone-sdk.
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
 use strict;
 
 # Apple's gas is ancient and doesn't support modern preprocessing features like
