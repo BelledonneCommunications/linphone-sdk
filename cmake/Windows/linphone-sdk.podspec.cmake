c0ba7868d82e25800c161201d115c88fec5dac52
unknown <julien.wadel@belledonne-communications.com>
2020-01-27 20:38:57 +0100

    Build package and upload

---
+++ cmake/Windows/linphone-sdk.podspec.cmake
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+
+  s.name         = "linphone-sdk"
+  s.version      = "@LINPHONESDK_VERSION@"
+  s.summary      = "This is the linphone SDK, a free (GPL) video voip library based on the SIP protocol"
+  s.description  = <<-PODSPEC_DESC
+@LINPHONESDK_ENABLED_FEATURES@PODSPEC_DESC
+  s.homepage     = "https://github.com/BelledonneCommunications/linphone-desktop"
+  s.license      = { :type => "GNU GPL 2", :text => <<-LICENSE
+@LINPHONESDK_LICENSE@LICENSE
+    }
+  s.author       = "employees@belledonne-communications.com"
+  s.platform     = :windows, "10"
+  s.source       = { :http => "@LINPHONESDK_WINDOWS_BASE_URL@/linphone-sdk-@LINPHONESDK_VERSION@.zip" }  
+
+end
