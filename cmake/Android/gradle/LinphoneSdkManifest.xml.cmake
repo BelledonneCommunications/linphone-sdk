5176e90b8ba167180ffa20b3dba86eecf150454d
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2020-03-02 14:32:56 +0100

    Updated liblinphone with Android service, iterate schedule & push related code
    Fixed Android Manifest & build with legacy java wrapper

--- cmake/Android/gradle/LinphoneSdkManifest.xml.cmake
+++ cmake/Android/gradle/LinphoneSdkManifest.xml.cmake
@@ -1,5 +1,19 @@
 <?xml version="1.0" encoding="utf-8"?>
 <manifest package="org.linphone.core"
-	xmlns:android="http://schemas.android.com/apk/res/android"
-	android:installLocation="auto">
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:installLocation="auto">
+
+    <!-- SDK required permissions -->
+    <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.RECORD_AUDIO" />
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+	<uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
+    <uses-permission android:name="android.permission.WAKE_LOCK" />
+    <!-- Needed to be able to use WifiManager.MulticastLock -->
+    <uses-permission android:name="android.permission.CHANGE_WIFI_MULTICAST_STATE" />
+    <uses-permission android:name="android.permission.CAMERA" />
+    <!-- Needed to allow Linphone to install on tablets, since android.permission.CAMERA implies android.hardware.camera and android.hardware.camera.autofocus are required -->
+    <uses-feature android:name="android.hardware.camera" android:required="false" />
+    <!-- End of SDK required permissions -->
+	
 </manifest>
