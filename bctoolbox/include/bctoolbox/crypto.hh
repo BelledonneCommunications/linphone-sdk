761c6afc594cb0a019006a51e6629efd276904ea
Johan Pascal <johan.pascal@belledonne-communications.com>
2020-06-19 18:29:39 +0200

    Improve c++ crypto API Add HMAC and KDF

--- bctoolbox/include/bctoolbox/crypto.hh
+++ bctoolbox/include/bctoolbox/crypto.hh
@@ -44,6 +44,13 @@ class RNG {
 		void randomize(uint8_t *buffer, const size_t size);
 
 		/**
+		 * return a random vector of given size
+		 * @param[in]		size	size in bytes of the random generated
+		 **/
+		std::vector<uint8_t> randomize(const size_t size);
+
+
+		/**
 		 * generates a 32 bits random unsigned number
 		 **/
 		uint32_t randomize();
@@ -70,6 +77,142 @@ class RNG {
 		std::unique_ptr<Impl> pImpl;
 		static std::unique_ptr<Impl> pImplClass;
 }; //class RNG
+
+
+/*****************************************************************************/
+/***                      Hash related function                            ***/
+/*****************************************************************************/
+/**
+ * @brief SHA256 buffer size definition
+ */
+struct SHA256 {
+	/// maximum output size for SHA256 is 32 bytes
+	static constexpr size_t ssize() {return 32;}
+};
+
+/**
+ * @brief SHA384 buffer size definition
+ */
+struct SHA384 {
+	/// maximum output size for SHA384 is 48 bytes
+	static constexpr size_t ssize() {return 48;}
+};
+
+/**
+ * @brief SHA512 buffer size definition
+ */
+struct SHA512 {
+	/// maximum output size for SHA512 is 64 bytes
+	static constexpr size_t ssize() {return 64;}
+};
+
+
+/**
+ * @brief templated HMAC
+ *
+ * @tparam	hashAlgo	the hash algorithm used (only SHA512 available for now)
+ *
+ * @param[in]	key		HMAC key
+ * @param[in]	input		HMAC input
+ *
+ * @return an array of size matching the selected hash algorithm output size
+ *
+ */
+template <typename hashAlgo>
+std::array<uint8_t, hashAlgo::ssize()> HMAC(const std::vector<uint8_t> &key, const std::vector<uint8_t> &input);
+/* declare template specialisations */
+template <> std::array<uint8_t, SHA256::ssize()>  HMAC<SHA256>(const std::vector<uint8_t> &key, const std::vector<uint8_t> &input);
+template <> std::array<uint8_t, SHA384::ssize()>  HMAC<SHA384>(const std::vector<uint8_t> &key, const std::vector<uint8_t> &input);
+template <> std::array<uint8_t, SHA512::ssize()>  HMAC<SHA512>(const std::vector<uint8_t> &key, const std::vector<uint8_t> &input);
+
+/**
+ * @brief HKDF as described in RFC5869
+ *	@par Compute:
+ *	@code{.unparsed}
+ *		PRK = HMAC-Hash(salt, IKM)
+ *
+ *		N = ceil(L/HashLen)
+ *		T = T(1) | T(2) | T(3) | ... | T(N)
+ *		OKM = first L octets of T
+ *
+ *		where:
+ *		T(0) = empty string (zero length)
+ *		T(1) = HMAC-Hash(PRK, T(0) | info | 0x01)
+ *		T(2) = HMAC-Hash(PRK, T(1) | info | 0x02)
+ *		T(3) = HMAC-Hash(PRK, T(2) | info | 0x03)
+ *		...
+ *	@endcode
+ *
+ * @tparam	hashAlgo	the hash algorithm to use
+ * @tparam	infoType	the info parameter type : can be passed as a string or a std::vector<uint8_t>
+ *
+ * @param[in]	salt 		salt
+ * @param[in]	ikm		input key material
+ * @param[in]	info		a info string or buffer
+ * @param[in]	okmSize		requested amount of data. (L in the RFC doc)
+ *
+ * @return the output key material
+ *
+ */
+template <typename hashAlgo>
+std::vector<uint8_t> HKDF(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::vector<uint8_t> &info, size_t okmSize);
+template <typename hashAlgo>
+std::vector<uint8_t> HKDF(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::string &info, size_t okmSize);
+/* declare template specialisations */
+template <> std::vector<uint8_t> HKDF<SHA256>(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::vector<uint8_t> &info, size_t outputSize);
+template <> std::vector<uint8_t> HKDF<SHA256>(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::string &info, size_t outputSize);
+template <> std::vector<uint8_t> HKDF<SHA512>(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::vector<uint8_t> &info, size_t outputSize);
+template <> std::vector<uint8_t> HKDF<SHA512>(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::string &info, size_t outputSize);
+
+
+/************************ AEAD interface *************************************/
+// AEAD function defines
+/**
+ * @brief AES256GCM buffers size definition
+ */
+struct AES256GCM128 {
+	/// key size is 32 bytes
+	static constexpr size_t keySize(void) {return 32;};
+	/// tag size is 16 bytes
+	static constexpr size_t tagSize(void) {return 16;};
+};
+
+/**
+ * @brief Encrypt and tag using scheme given as template parameter
+ *
+ * @param[in]	key		Encryption key
+ * @param[in]	IV		Initialisation vector
+ * @param[in]	plain		Plain text
+ * @param[in]	AD		Additional data used in tag computation
+ * @param[out]	tag		Generated authentication tag
+ * @return	the cipher text
+ */
+template <typename AEADAlgo>
+std::vector<uint8_t> AEAD_encrypt(const std::array<uint8_t, AEADAlgo::keySize()> &key, const std::vector<uint8_t> IV, const std::vector<uint8_t> &plain, const std::vector<uint8_t> &AD,
+		std::array<uint8_t, AEADAlgo::tagSize()> &tag);
+
+/**
+ * @brief Authenticate and Decrypt using scheme given as template parameter
+ *
+ * @param[in]	key		Encryption key
+ * @param[in]	IV		Initialisation vector
+ * @param[in]	cipher		Cipher text
+ * @param[in]	AD		Additional data used in tag computation
+ * @param[in]	tag		Authentication tag
+ * @param[out]	plain		A vector to store the plain text
+ *
+ * @return true if authentication tag match and decryption was successful
+ */
+template <typename AEADAlgo>
+bool AEAD_decrypt(const std::array<uint8_t, AEADAlgo::keySize()> &key, const std::vector<uint8_t> &IV, const std::vector<uint8_t> &cipher, const std::vector<uint8_t> &AD,
+		const std::array<uint8_t, AEADAlgo::tagSize()> &tag, std::vector<uint8_t> &plain);
+
+/* declare AEAD template specialisations : AES256-GCM with 128 bits auth tag*/
+template <> std::vector<uint8_t> AEAD_encrypt<AES256GCM128>(const std::array<uint8_t, AES256GCM128::keySize()> &key, const std::vector<uint8_t> IV, const std::vector<uint8_t> &plain, const std::vector<uint8_t> &AD,
+		std::array<uint8_t, AES256GCM128::tagSize()> &tag);
+
+template <> bool AEAD_decrypt<AES256GCM128>(const std::array<uint8_t, AES256GCM128::keySize()> &key, const std::vector<uint8_t> &IV, const std::vector<uint8_t> &cipher, const std::vector<uint8_t> &AD,
+		const std::array<uint8_t, AES256GCM128::tagSize()> &tag, std::vector<uint8_t> &plain);
 
 } // namespace bctoolbox
 #endif // BCTBX_CRYPTO_HH
--- bctoolbox/src/crypto/mbedtls.cc
+++ bctoolbox/src/crypto/mbedtls.cc
@@ -24,13 +24,19 @@
 #include <mbedtls/version.h>
 #include <mbedtls/entropy.h>
 #include <mbedtls/ctr_drbg.h>
+#include <mbedtls/md.h>
+#include <mbedtls/sha256.h>
+#include <mbedtls/sha512.h>
+#include <mbedtls/hkdf.h>
+#include <mbedtls/gcm.h>
+
 
 #include "bctoolbox/crypto.hh"
 #include "bctoolbox/exception.hh"
 #include "bctoolbox/logging.h"
 
 
-using namespace bctoolbox;
+namespace bctoolbox {
 
 /*****************************************************************************/
 /***                      Random Number Generation                         ***/
@@ -84,6 +90,15 @@ void RNG::randomize(uint8_t *buffer, size_t size) {
 	}
 }
 
+std::vector<uint8_t> RNG::randomize(const size_t size) {
+	std::vector<uint8_t> buffer(size);
+	int ret = mbedtls_ctr_drbg_random(&(pImpl->ctr_drbg), buffer.data(), size);
+	if ( ret != 0) {
+		throw BCTBX_EXCEPTION << ((ret == MBEDTLS_ERR_CTR_DRBG_REQUEST_TOO_BIG)?"RNG failure: Request too big":"RNG failure: entropy source failure");
+	}
+	return buffer;
+}
+
 uint32_t RNG::randomize() {
 	uint8_t buffer[4];
 	randomize(buffer, 4);
@@ -106,3 +121,154 @@ uint32_t RNG::c_randomize() {
 	c_randomize(buffer, 4);
 	return (buffer[0]<<24) | (buffer[1]<<16) | (buffer[2]<<8) | buffer[3];
 }
+
+/*****************************************************************************/
+/***                      Hash related function                            ***/
+/*****************************************************************************/
+/* HMAC templates */
+/* HMAC must use a specialized template */
+template <typename hashAlgo>
+std::array<uint8_t, hashAlgo::ssize()> HMAC(const std::vector<uint8_t> &key, const std::vector<uint8_t> &input) {
+	/* if this template is instanciated the static_assert will fail but will give us an error message */
+	static_assert(sizeof(hashAlgo) != sizeof(hashAlgo), "You must specialize HMAC function template");
+}
+
+/* HMAC specialized template for SHA256 */
+template <> std::array<uint8_t, SHA256::ssize()> HMAC<SHA256>(const std::vector<uint8_t> &key, const std::vector<uint8_t> &input) {
+	std::array<uint8_t, SHA256::ssize()> hmacOutput;
+	mbedtls_md_hmac(mbedtls_md_info_from_type(MBEDTLS_MD_SHA256), key.data(), key.size(), input.data(), input.size(), hmacOutput.data());
+	return  hmacOutput;
+}
+
+/* HMAC specialized template for SHA384 */
+template <> std::array<uint8_t, SHA384::ssize()> HMAC<SHA384>(const std::vector<uint8_t> &key, const std::vector<uint8_t> &input) {
+	std::array<uint8_t, SHA384::ssize()> hmacOutput;
+	mbedtls_md_hmac(mbedtls_md_info_from_type(MBEDTLS_MD_SHA384), key.data(), key.size(), input.data(), input.size(), hmacOutput.data());
+	return  hmacOutput;
+}
+
+/* HMAC specialized template for SHA512 */
+template <> std::array<uint8_t, SHA512::ssize()> HMAC<SHA512>(const std::vector<uint8_t> &key, const std::vector<uint8_t> &input) {
+	std::array<uint8_t, SHA512::ssize()> hmacOutput;
+	mbedtls_md_hmac(mbedtls_md_info_from_type(MBEDTLS_MD_SHA512), key.data(), key.size(), input.data(), input.size(), hmacOutput.data());
+	return  hmacOutput;
+}
+
+/* HKDF templates */
+/* HKDF must use a specialized template */
+template <typename hashAlgo>
+std::vector<uint8_t> HKDF(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::vector<uint8_t> &info, size_t okmSize) {
+	/* if this template is instanciated the static_assert will fail but will give us an error message */
+	static_assert(sizeof(hashAlgo) != sizeof(hashAlgo), "You must specialize HKDF function template");
+
+	return std::vector<uint8_t>(0);
+}
+template <typename hashAlgo>
+std::vector<uint8_t> HKDF(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::string &info, size_t okmSize) {
+	/* if this template is instanciated the static_assert will fail but will give us an error message */
+	static_assert(sizeof(hashAlgo) != sizeof(hashAlgo), "You must specialize HKDF function template");
+
+	return std::vector<uint8_t>(0);
+}
+
+/* HKDF specialized template for SHA256 */
+template <> std::vector<uint8_t> HKDF<SHA256>(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::vector<uint8_t> &info, size_t outputSize) {
+	std::vector<uint8_t> okm(outputSize);
+	if (mbedtls_hkdf(mbedtls_md_info_from_type(MBEDTLS_MD_SHA256), salt.data(), salt.size(), ikm.data(), ikm.size(), info.data(), info.size(), okm.data(), outputSize) != 0) {
+		throw BCTBX_EXCEPTION<<"HKDF-SHA256 error";
+	}
+	return okm;
+};
+template <> std::vector<uint8_t> HKDF<SHA256>(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::string &info, size_t outputSize) {
+	std::vector<uint8_t> okm(outputSize);
+	if (mbedtls_hkdf(mbedtls_md_info_from_type(MBEDTLS_MD_SHA256), salt.data(), salt.size(), ikm.data(), ikm.size(), reinterpret_cast<const unsigned char*>(info.data()), info.size(), okm.data(), outputSize) != 0) {
+		throw BCTBX_EXCEPTION<<"HKDF-SHA256 error";
+	}
+	return okm;
+};
+
+/* HKDF specialized template for SHA512 */
+template <> std::vector<uint8_t> HKDF<SHA512>(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::vector<uint8_t> &info, size_t outputSize) {
+	std::vector<uint8_t> okm(outputSize);
+	if (mbedtls_hkdf(mbedtls_md_info_from_type(MBEDTLS_MD_SHA512), salt.data(), salt.size(), ikm.data(), ikm.size(), info.data(), info.size(), okm.data(), outputSize) != 0) {
+		throw BCTBX_EXCEPTION<<"HKDF-SHA512 error";
+	}
+	return okm;
+};
+template <> std::vector<uint8_t> HKDF<SHA512>(const std::vector<uint8_t> &salt, const std::vector<uint8_t> &ikm, const std::string &info, size_t outputSize) {
+	std::vector<uint8_t> okm(outputSize);
+	if (mbedtls_hkdf(mbedtls_md_info_from_type(MBEDTLS_MD_SHA512), salt.data(), salt.size(), ikm.data(), ikm.size(), reinterpret_cast<const unsigned char*>(info.data()), info.size(), okm.data(), outputSize) != 0) {
+		throw BCTBX_EXCEPTION<<"HKDF-SHA512 error";
+	}
+	return okm;
+};
+
+/*****************************************************************************/
+/***                      Authenticated Encryption                         ***/
+/*****************************************************************************/
+/* AEAD template must be specialized */
+template <typename AEADAlgo>
+std::vector<uint8_t> AEAD_encrypt(const std::array<uint8_t, AEADAlgo::keySize()> &key, const std::vector<uint8_t> IV, const std::vector<uint8_t> &plain, const std::vector<uint8_t> &AD,
+		std::array<uint8_t, AEADAlgo::tagSize()> &tag) {
+	/* if this template is instanciated the static_assert will fail but will give us an error message with faulty type */
+	static_assert(sizeof(AEADAlgo) != sizeof(AEADAlgo), "You must specialize AEAD_encrypt function template");
+	return std::vector<uint8_t>(0);
+}
+
+template <typename AEADAlgo>
+bool AEAD_decrypt(const std::array<uint8_t, AEADAlgo::keySize()> &key, const std::vector<uint8_t> &IV, const std::vector<uint8_t> &cipher, const std::vector<uint8_t> &AD,
+		const std::array<uint8_t, AEADAlgo::tagSize()> &tag, std::vector<uint8_t> &plain) {
+	/* if this template is instanciated the static_assert will fail but will give us an error message with faulty type */
+	static_assert(sizeof(AEADAlgo) != sizeof(AEADAlgo), "You must specialize AEAD_encrypt function template");
+	return false;
+}
+
+/* declare AEAD template specialisations : AES256-GCM with 128 bits auth tag*/
+template <> std::vector<uint8_t> AEAD_encrypt<AES256GCM128>(const std::array<uint8_t, AES256GCM128::keySize()> &key, const std::vector<uint8_t> IV, const std::vector<uint8_t> &plain, const std::vector<uint8_t> &AD,
+		std::array<uint8_t, AES256GCM128::tagSize()> &tag) {
+	mbedtls_gcm_context gcmContext;
+	mbedtls_gcm_init(&gcmContext);
+
+	auto ret = mbedtls_gcm_setkey(&gcmContext, MBEDTLS_CIPHER_ID_AES, key.data(), key.size()*8); // key size in bits
+	if (ret != 0) {
+		mbedtls_gcm_free(&gcmContext);
+		throw BCTBX_EXCEPTION<<"Unable to set key in AES_GCM context : return value "<<ret;
+	}
+
+	std::vector<uint8_t> cipher(plain.size()); // cipher size is the same than plain
+	ret = mbedtls_gcm_crypt_and_tag(&gcmContext, MBEDTLS_GCM_ENCRYPT, plain.size(), IV.data(), IV.size(), AD.data(), AD.size(), plain.data(), cipher.data(), tag.size(), tag.data());
+	mbedtls_gcm_free(&gcmContext);
+
+	if (ret != 0) {
+		throw BCTBX_EXCEPTION<<"Error during AES_GCM encryption : return value "<<ret;
+	}
+	return cipher;
+}
+
+template <> bool AEAD_decrypt<AES256GCM128>(const std::array<uint8_t, AES256GCM128::keySize()> &key, const std::vector<uint8_t> &IV, const std::vector<uint8_t> &cipher, const std::vector<uint8_t> &AD,
+		const std::array<uint8_t, AES256GCM128::tagSize()> &tag, std::vector<uint8_t> &plain) {
+	mbedtls_gcm_context gcmContext;
+	mbedtls_gcm_init(&gcmContext);
+
+	auto ret = mbedtls_gcm_setkey(&gcmContext, MBEDTLS_CIPHER_ID_AES, key.data(), key.size()*8); // key size in bits
+	if (ret != 0) {
+		mbedtls_gcm_free(&gcmContext);
+		throw BCTBX_EXCEPTION<<"Unable to set key in AES_GCM context : return value "<<ret;
+	}
+
+	plain.resize(cipher.size()); // plain is the same size than cipher
+	ret = mbedtls_gcm_auth_decrypt(&gcmContext, cipher.size(), IV.data(), IV.size(), AD.data(), AD.size(), tag.data(), tag.size(), cipher.data(), plain.data());
+	mbedtls_gcm_free(&gcmContext);
+
+	if (ret == 0) {
+		return true;
+	}
+	if (ret == MBEDTLS_ERR_GCM_AUTH_FAILED) {
+		return false;
+	}
+
+	throw BCTBX_EXCEPTION<<"Error during AES_GCM decryption : return value "<<ret;
+}
+
+
+} // namespace bctoolbox
