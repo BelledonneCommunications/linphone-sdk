74f7c32b36448eeae0296033e220415a0dfbc109
Julien Wadel <julien.wadel@belledonne-communications.com>
2020-06-29 13:22:45 +0200

    Update podspec for licensing to GPL 3, homepage to liphone-sdk and author to Belledonne Communications

--- cmake/macos/linphone-sdk.podspec.cmake
+++ cmake/macos/linphone-sdk.podspec.cmake
@@ -5,11 +5,11 @@ Pod::Spec.new do |s|
   s.summary      = "This is the linphone SDK, a free (GPL) video voip library based on the SIP protocol"
   s.description  = <<-PODSPEC_DESC
 @LINPHONESDK_ENABLED_FEATURES@PODSPEC_DESC
-  s.homepage     = "https://github.com/BelledonneCommunications/linphone-desktop"
-  s.license      = { :type => "GNU GPL 2", :text => <<-LICENSE
+  s.homepage     = "https://github.com/BelledonneCommunications/linphone-sdk"
+  s.license      = { :type => "GNU GPL 3", :text => <<-LICENSE
 @LINPHONESDK_LICENSE@LICENSE
     }
-  s.author       = "employees@belledonne-communications.com"
+  s.author       =  { 'Belledonne Communications SARL' => 'linphone-sdk@belledonne-communications.com' }
   s.platform     = :osx, "10.9"
   s.source       = { :http => "@LINPHONESDK_MACOS_BASE_URL@/linphone-sdk-@LINPHONESDK_VERSION@.zip" }
   s.vendored_frameworks = "linphone-sdk/desktop/Frameworks/**"
