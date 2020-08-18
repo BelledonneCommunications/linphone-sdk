BelledonneCommunications/liblinphone

Added new chat message callback to fix issue with send return value not used in multi listener

coreapi/private_functions.h
@@ -344,6 +344,7 @@ void _linphone_chat_message_notify_msg_state_changed(LinphoneChatMessage* msg, L
 void _linphone_chat_message_notify_participant_imdn_state_changed(LinphoneChatMessage* msg, const LinphoneParticipantImdnState *state);
 void _linphone_chat_message_notify_file_transfer_recv(LinphoneChatMessage *msg, LinphoneContent* content, const LinphoneBuffer *buffer);
 void _linphone_chat_message_notify_file_transfer_send(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t size);
+void _linphone_chat_message_notify_file_transfer_send_2(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t size, LinphoneBuffer *buffer);
 void _linphone_chat_message_notify_file_transfer_progress_indication(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t total);
 void _linphone_chat_message_notify_ephemeral_message_timer_started(LinphoneChatMessage* msg);
 void _linphone_chat_message_notify_ephemeral_message_deleted(LinphoneChatMessage* msg);

include/linphone/api/c-chat-message-cbs.h
@@ -95,6 +95,7 @@ LINPHONE_PUBLIC void linphone_chat_message_cbs_set_file_transfer_recv (LinphoneC
   * Get the file transfer send callback.
   * @param cbs LinphoneChatMessageCbs object. @notnil
   * @return The current file transfer send callback.
+  * @deprecated 17/08/2020 Use #linphone_chat_message_cbs_get_file_transfer_send_2 instead.
   */
 LINPHONE_PUBLIC LinphoneChatMessageCbsFileTransferSendCb linphone_chat_message_cbs_get_file_transfer_send (const LinphoneChatMessageCbs *cbs);
 
@@ -102,8 +103,23 @@ LINPHONE_PUBLIC LinphoneChatMessageCbsFileTransferSendCb linphone_chat_message_c
  * Set the file transfer send callback.
  * @param cbs LinphoneChatMessageCbs object. @notnil
  * @param cb The file transfer send callback to be used.
+  * @deprecated 17/08/2020 Use #linphone_chat_message_cbs_set_file_transfer_send_2 instead.
  */
 LINPHONE_PUBLIC void linphone_chat_message_cbs_set_file_transfer_send (LinphoneChatMessageCbs *cbs, LinphoneChatMessageCbsFileTransferSendCb cb);
+
+/**
+  * Get the file transfer send callback.
+  * @param cbs LinphoneChatMessageCbs object. @notnil
+  * @return The current file transfer send callback.
+  */
+LINPHONE_PUBLIC LinphoneChatMessageCbsFileTransferSend2Cb linphone_chat_message_cbs_get_file_transfer_send_2 (const LinphoneChatMessageCbs *cbs);
+
+/**
+ * Set the file transfer send callback.
+ * @param cbs LinphoneChatMessageCbs object. @notnil
+ * @param cb The file transfer send callback to be used.
+ */
+LINPHONE_PUBLIC void linphone_chat_message_cbs_set_file_transfer_send_2 (LinphoneChatMessageCbs *cbs, LinphoneChatMessageCbsFileTransferSend2Cb cb);
 
 /**
  * Get the file transfer progress indication callback.

src/c-wrapper/api/c-chat-message.cpp
@@ -167,6 +167,10 @@ void _linphone_chat_message_notify_file_transfer_send(LinphoneChatMessage *msg, 
 	NOTIFY_IF_EXIST(FileTransferSend, file_transfer_send, msg, content, offset, size)
 }
 
+void _linphone_chat_message_notify_file_transfer_send_2(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t size, LinphoneBuffer *buffer) {
+	NOTIFY_IF_EXIST(FileTransferSend2, file_transfer_send_2, msg, content, offset, size, buffer)
+}
+
 void _linphone_chat_message_notify_file_transfer_progress_indication(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t total) {
 	NOTIFY_IF_EXIST(FileTransferProgressIndication, file_transfer_progress_indication, msg, content, offset, total)
 }

tester/flexisip_tester.c
@@ -1001,7 +1001,7 @@ static void file_transfer_message_rcs_to_external_body_client(void) {
 		linphone_content_set_name(content,"nowebcamCIF.jpg");
 		linphone_content_set_user_data(content,file_to_send);
 		message = linphone_chat_room_create_file_transfer_message(chat_room, content);
-		cbs = linphone_chat_message_get_callbacks(message);
+		cbs = linphone_factory_create_chat_message_cbs(linphone_factory_get());
 		{
 			int dummy=0;
 			wait_for_until(marie->lc,pauline->lc,&dummy,1,100); /*just to have time to purge message stored in the server*/
@@ -1009,7 +1009,9 @@ static void file_transfer_message_rcs_to_external_body_client(void) {
 			reset_counters(&pauline->stat);
 		}
 		linphone_chat_message_cbs_set_msg_state_changed(cbs,liblinphone_tester_chat_message_msg_state_changed);
-		linphone_chat_message_cbs_set_file_transfer_send(cbs, tester_file_transfer_send);
+		linphone_chat_message_cbs_set_file_transfer_send_2(cbs, tester_file_transfer_send_2);
+		linphone_chat_message_add_callbacks(message, cbs);
+		linphone_chat_message_cbs_unref(cbs);
 		linphone_chat_message_send(message);
 		BC_ASSERT_TRUE(wait_for(pauline->lc,marie->lc,&marie->stat.number_of_LinphoneMessageReceivedWithFile,1));
 

tester/group_chat_tester.c
@@ -300,10 +300,12 @@ void _send_file_plus_text(LinphoneChatRoom* cr, const char *sendFilepath, const 
 		linphone_content_unref(content2);
 	}
 
-	cbs = linphone_chat_message_get_callbacks(msg);
-	linphone_chat_message_cbs_set_file_transfer_send(cbs, tester_file_transfer_send);
+	cbs = linphone_factory_create_chat_message_cbs(linphone_factory_get());
+	linphone_chat_message_cbs_set_file_transfer_send_2(cbs, tester_file_transfer_send_2);
 	linphone_chat_message_cbs_set_msg_state_changed(cbs, liblinphone_tester_chat_message_msg_state_changed);
 	linphone_chat_message_cbs_set_file_transfer_progress_indication(cbs, file_transfer_progress_indication);
+	linphone_chat_message_add_callbacks(msg, cbs);
+	linphone_chat_message_cbs_unref(cbs);
 	linphone_chat_message_send(msg);
 	linphone_chat_message_unref(msg);
 }

BelledonneCommunications/linphone-sdk

liblinphone/coreapi/private_functions.h
@@ -344,6 +344,7 @@ void _linphone_chat_message_notify_msg_state_changed(LinphoneChatMessage* msg, L
 void _linphone_chat_message_notify_participant_imdn_state_changed(LinphoneChatMessage* msg, const LinphoneParticipantImdnState *state);
 void _linphone_chat_message_notify_file_transfer_recv(LinphoneChatMessage *msg, LinphoneContent* content, const LinphoneBuffer *buffer);
 void _linphone_chat_message_notify_file_transfer_send(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t size);
+void _linphone_chat_message_notify_file_transfer_send_chunk(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t size, LinphoneBuffer *buffer);
 void _linphone_chat_message_notify_file_transfer_progress_indication(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t total);
 void _linphone_chat_message_notify_ephemeral_message_timer_started(LinphoneChatMessage* msg);
 void _linphone_chat_message_notify_ephemeral_message_deleted(LinphoneChatMessage* msg);

liblinphone/include/linphone/api/c-chat-message-cbs.h
@@ -95,6 +95,7 @@ LINPHONE_PUBLIC void linphone_chat_message_cbs_set_file_transfer_recv (LinphoneC
   * Get the file transfer send callback.
   * @param cbs LinphoneChatMessageCbs object. @notnil
   * @return The current file transfer send callback.
+  * @deprecated 17/08/2020 Use #linphone_chat_message_cbs_get_file_transfer_send_chunk instead.
   */
 LINPHONE_PUBLIC LinphoneChatMessageCbsFileTransferSendCb linphone_chat_message_cbs_get_file_transfer_send (const LinphoneChatMessageCbs *cbs);
 
@@ -102,8 +103,23 @@ LINPHONE_PUBLIC LinphoneChatMessageCbsFileTransferSendCb linphone_chat_message_c
  * Set the file transfer send callback.
  * @param cbs LinphoneChatMessageCbs object. @notnil
  * @param cb The file transfer send callback to be used.
+  * @deprecated 17/08/2020 Use #linphone_chat_message_cbs_set_file_transfer_send_chunk instead.
  */
 LINPHONE_PUBLIC void linphone_chat_message_cbs_set_file_transfer_send (LinphoneChatMessageCbs *cbs, LinphoneChatMessageCbsFileTransferSendCb cb);
+
+/**
+  * Get the file transfer send callback.
+  * @param cbs LinphoneChatMessageCbs object. @notnil
+  * @return The current file transfer send callback.
+  */
+LINPHONE_PUBLIC LinphoneChatMessageCbsFileTransferSendChunkCb linphone_chat_message_cbs_get_file_transfer_send_chunk (const LinphoneChatMessageCbs *cbs);
+
+/**
+ * Set the file transfer send callback.
+ * @param cbs LinphoneChatMessageCbs object. @notnil
+ * @param cb The file transfer send callback to be used.
+ */
+LINPHONE_PUBLIC void linphone_chat_message_cbs_set_file_transfer_send_chunk (LinphoneChatMessageCbs *cbs, LinphoneChatMessageCbsFileTransferSendChunkCb cb);
 
 /**
  * Get the file transfer progress indication callback.

liblinphone/src/c-wrapper/api/c-chat-message.cpp
@@ -167,6 +167,10 @@ void _linphone_chat_message_notify_file_transfer_send(LinphoneChatMessage *msg, 
 	NOTIFY_IF_EXIST(FileTransferSend, file_transfer_send, msg, content, offset, size)
 }
 
+void _linphone_chat_message_notify_file_transfer_send_chunk(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t size, LinphoneBuffer *buffer) {
+	NOTIFY_IF_EXIST(FileTransferSendChunk, file_transfer_send_chunk, msg, content, offset, size, buffer)
+}
+
 void _linphone_chat_message_notify_file_transfer_progress_indication(LinphoneChatMessage *msg, LinphoneContent* content, size_t offset, size_t total) {
 	NOTIFY_IF_EXIST(FileTransferProgressIndication, file_transfer_progress_indication, msg, content, offset, total)
 }

liblinphone/tester/flexisip_tester.c
@@ -1001,7 +1001,7 @@ static void file_transfer_message_rcs_to_external_body_client(void) {
 		linphone_content_set_name(content,"nowebcamCIF.jpg");
 		linphone_content_set_user_data(content,file_to_send);
 		message = linphone_chat_room_create_file_transfer_message(chat_room, content);
-		cbs = linphone_chat_message_get_callbacks(message);
+		cbs = linphone_factory_create_chat_message_cbs(linphone_factory_get());
 		{
 			int dummy=0;
 			wait_for_until(marie->lc,pauline->lc,&dummy,1,100); /*just to have time to purge message stored in the server*/
@@ -1009,7 +1009,9 @@ static void file_transfer_message_rcs_to_external_body_client(void) {
 			reset_counters(&pauline->stat);
 		}
 		linphone_chat_message_cbs_set_msg_state_changed(cbs,liblinphone_tester_chat_message_msg_state_changed);
-		linphone_chat_message_cbs_set_file_transfer_send(cbs, tester_file_transfer_send);
+		linphone_chat_message_cbs_set_file_transfer_send_chunk(cbs, tester_file_transfer_send_2);
+		linphone_chat_message_add_callbacks(message, cbs);
+		linphone_chat_message_cbs_unref(cbs);
 		linphone_chat_message_send(message);
 		BC_ASSERT_TRUE(wait_for(pauline->lc,marie->lc,&marie->stat.number_of_LinphoneMessageReceivedWithFile,1));
 

liblinphone/tester/group_chat_tester.c
@@ -300,10 +300,12 @@ void _send_file_plus_text(LinphoneChatRoom* cr, const char *sendFilepath, const 
 		linphone_content_unref(content2);
 	}
 
-	cbs = linphone_chat_message_get_callbacks(msg);
-	linphone_chat_message_cbs_set_file_transfer_send(cbs, tester_file_transfer_send);
+	cbs = linphone_factory_create_chat_message_cbs(linphone_factory_get());
+	linphone_chat_message_cbs_set_file_transfer_send_chunk(cbs, tester_file_transfer_send_2);
 	linphone_chat_message_cbs_set_msg_state_changed(cbs, liblinphone_tester_chat_message_msg_state_changed);
 	linphone_chat_message_cbs_set_file_transfer_progress_indication(cbs, file_transfer_progress_indication);
+	linphone_chat_message_add_callbacks(msg, cbs);
+	linphone_chat_message_cbs_unref(cbs);
 	linphone_chat_message_send(msg);
 	linphone_chat_message_unref(msg);
 }