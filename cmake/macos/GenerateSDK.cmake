3f9c34053946b6e8948533c23a88ac6495fd5fda
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-05-11 00:35:20 +0200

    Apple ARM support : vpx, opus, mswebrtc. Merge architectures with lipo

--- cmake/macos/GenerateSDK.cmake
+++ cmake/macos/GenerateSDK.cmake
@@ -1,24 +1,21 @@
-############################################################################
-# GenerateSDK.cmake
-# Copyright (C) 2010-2018 Belledonne Communications, Grenoble France
+################################################################################
 #
-############################################################################
+#  Copyright (c) 2010-2021 Belledonne Communications SARL.
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-############################################################################
+################################################################################
 
 
 list(APPEND CMAKE_MODULE_PATH "${LINPHONESDK_DIR}/cmake")
@@ -26,12 +23,19 @@ include(LinphoneSdkUtils)
 
 
 # Create the zip file of the SDK
+if (LINPHONESDK_PLATFORM STREQUAL "Desktop")
+	set(INSTALL_FOLDER "desktop")
+else()
+	set(INSTALL_FOLDER "macos")
+endif()
 execute_process(
-	COMMAND "zip" "-r" "--symlinks" "linphone-sdk-${LINPHONESDK_VERSION}.zip" "linphone-sdk/desktop"
+	COMMAND "zip" "-r" "--symlinks" "linphone-sdk-macos-${LINPHONESDK_VERSION}.zip" "linphone-sdk/apple-${INSTALL_FOLDER}"
 	WORKING_DIRECTORY "${LINPHONESDK_BUILD_DIR}"
 )
 
 # Generate podspec file
 file(READ "${LINPHONESDK_DIR}/LICENSE.txt" LINPHONESDK_LICENSE)
 file(READ "${LINPHONESDK_ENABLED_FEATURES_FILENAME}" LINPHONESDK_ENABLED_FEATURES)
+linphone_sdk_convert_comma_separated_list_to_cmake_list("${LINPHONESDK_MACOS_ARCHS}" VALID_ARCHS)
+string(REPLACE ";" " " VALID_ARCHS "${VALID_ARCHS}")
 configure_file("${LINPHONESDK_DIR}/cmake/macos/linphone-sdk.podspec.cmake" "${LINPHONESDK_BUILD_DIR}/linphone-sdk.podspec" @ONLY)
