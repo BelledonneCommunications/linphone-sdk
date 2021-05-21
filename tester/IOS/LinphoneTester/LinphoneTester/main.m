5403abb23a04c61f1824239bc827502ce9ad3aa4
QuentinArguillere <quentin.arguillere@belledone-communications.com>
2021-05-21 16:03:51 +0200

    Test refactoring, and test push timeout of calls

--- tester/IOS/LinphoneTester/LinphoneTester/main.m
+++ tester/IOS/LinphoneTester/LinphoneTester/main.m
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]) {
 			liblinphonetester_show_account_manager_logs=TRUE;
         }
     }
-	flexisip_tester_dns_ip_addresses = bctbx_list_new((void *)"192.168.0.40");
+	flexisip_tester_dns_ip_addresses = bctbx_list_new((void *)"5.135.31.162");
 	
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
