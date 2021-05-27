a7fc0c85fb133b23d6d868b7caff3ea0a782e3a3
François Grisez <francois.grisez@belledonne-communications.com>
2021-05-27 18:13:31 +0200

    Prepare CMakeLists.txt for new Flexisip build and packaging scripts
    * Add DISABLE_BC_PACKAGE_SEARCH around some find_package()
    * Require CMake 3.13 or later
    * Add bc_compute_linux_package_version() macro
    
    (cherry picked from commit d87e92947e1029baa76b06789fd71a6c7460f9cb)
    (cherry picked from commit 252b9872641e0df83caeeb4819315ce2cbfd4bb3)

--- bctoolbox/cmake/BcToolboxCMakeUtils.cmake
+++ bctoolbox/cmake/BcToolboxCMakeUtils.cmake
@@ -1,6 +1,6 @@
 ############################################################################
 # BcToolboxCMakeUtils.cmake
-# Copyright (C) 2010-2019 Belledonne Communications, Grenoble France
+# Copyright (C) 2010-2021 Belledonne Communications, Grenoble France
 #
 ############################################################################
 #
@@ -130,22 +130,6 @@ macro(bc_generate_rpm_specfile SOURCE DEST)
 	endif()
 endmacro()
 
-function(bc_parse_full_version version major minor patch)
-	if ("${version}" MATCHES "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)(-[.0-9A-Za-z-]+)?([+][.0-9A-Za-z-]+)?$")
-	    set(${major}       "${CMAKE_MATCH_1}" PARENT_SCOPE)
-	    set(${minor}       "${CMAKE_MATCH_2}" PARENT_SCOPE)
-	    set(${patch}       "${CMAKE_MATCH_3}" PARENT_SCOPE)
-		if (ARGC GREATER 4)
-			set(${ARGV4} "${CMAKE_MATCH_4}" PARENT_SCOPE)
-		endif()
-		if (ARGC GREATER 5)
-			set(${ARGV5}    "${CMAKE_MATCH_5}" PARENT_SCOPE)
-		endif()
-	else()
-		message(FATAL_ERROR "invalid full version '${version}'")
-	endif()
-endfunction()
-
 function(bc_compute_full_version OUTPUT_VERSION)
 	find_program(GIT_EXECUTABLE git NAMES Git CMAKE_FIND_ROOT_PATH_BOTH)
 	if(GIT_EXECUTABLE)
@@ -224,6 +208,67 @@ function(bc_compute_snapshots_or_releases_state OUTPUT_VERSION)
 		endif()
 
 		set(${OUTPUT_VERSION} "${snapshots_or_releases_state}" PARENT_SCOPE)
+	endif()
+endfunction()
+
+# Allows to get the distinct parts of a full version number.
+# Parameters:
+# * version [input]           : the full version number as string.
+# * major   [output]          : the major version.
+# * minor   [output]          : the minor version.
+# * patch   [output]          : the patch version.
+# * branch  [output,optional] : aggregation of the branch name (alpha, beta, etc.) and the commit increment since the last tag.
+#                               The whole is prefixed by '-' and the result is empty for releases. 
+# * hash    [output,optional] : the commit hash prefixed by '+' or empty for releases.
+#
+# Example:
+# '5.2.1-alpha.145+e62e126' -> major : 5
+#                              minor : 2
+#                              patch : 1
+#                              branch : '-alpha.145'
+#                              hash : '+e62e126'
+#
+# '5.2.1' -> major : 5
+#            minor : 2
+#            patch : 1
+#            branch : <empty>
+#            hash : <empty>
+function(bc_parse_full_version version major minor patch)
+	if ("${version}" MATCHES "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)(-[.0-9A-Za-z-]+)?([+][.0-9A-Za-z-]+)?$")
+	    set(${major}       "${CMAKE_MATCH_1}" PARENT_SCOPE)
+	    set(${minor}       "${CMAKE_MATCH_2}" PARENT_SCOPE)
+	    set(${patch}       "${CMAKE_MATCH_3}" PARENT_SCOPE)
+		if (ARGC GREATER 4)
+			set(${ARGV4} "${CMAKE_MATCH_4}" PARENT_SCOPE)
+		endif()
+		if (ARGC GREATER 5)
+			set(${ARGV5}    "${CMAKE_MATCH_5}" PARENT_SCOPE)
+		endif()
+	else()
+		message(FATAL_ERROR "invalid full version '${version}'")
+	endif()
+endfunction()
+
+# Translate the full semantic version into version numbers suitable for GNU/Linux packages (RPM/DEB)
+# Parameters:
+# * full_version_in     [input]  : the full version which has been computed by bc_compute_full_version().
+# * package_version_out [output] : the name of a variable where to store the resulting package version number.
+# * package_release_out [output] : the name of a variable where to store the resulting package release number.
+#
+# Exemple:
+# '5.2.1-alpha.145+e62e126' -> version : '5.2.1'
+#                              release : '0.alpha.145+e62e126'
+#
+# '5.2.1' -> version : '5.2.1'
+#            release : '1'
+function(bc_compute_linux_package_version full_version_in package_version_out package_release_out)
+	bc_parse_full_version("${full_version_in}" version_major version_minor version_patch identifiers metadata)
+	set(${package_version_out} "${version_major}.${version_minor}.${version_patch}" PARENT_SCOPE)
+	if (NOT identifiers)
+	    set(${package_release_out} 1 PARENT_SCOPE)
+	else()
+	    string(SUBSTRING "${identifiers}" 1 -1 identifiers)
+	    set(${package_release_out} "0.${identifiers}${metadata}" PARENT_SCOPE)
 	endif()
 endfunction()
 
