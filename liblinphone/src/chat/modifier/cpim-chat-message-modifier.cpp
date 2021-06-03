BelledonneCommunications/liblinphone

Add reply to chat message feature

src/chat/modifier/cpim-chat-message-modifier.cpp
@@ -45,6 +45,8 @@ const string imdnNamespace = "imdn";
 const string imdnMessageIdHeader = "Message-ID";
 const string imdnForwardInfoHeader = "Forward-Info";
 const string imdnDispositionNotificationHeader = "Disposition-Notification";
+const string imdnReplyingToMessageIdHeader = "Replying-To-Message-ID";
+const string imdnReplyingToMessageSenderHeader = "Replying-To-Sender";
 
 ChatMessageModifier::Result CpimChatMessageModifier::encode (const shared_ptr<ChatMessage> &message, int &errorCode) {
 	Cpim::Message cpimMessage;
@@ -90,6 +92,18 @@ ChatMessageModifier::Result CpimChatMessageModifier::encode (const shared_ptr<Ch
 		if (!forwardInfo.empty()) {
 			cpimMessage.addMessageHeader(
 				Cpim::GenericHeader(imdnNamespace + "." + imdnForwardInfoHeader, forwardInfo)
+			);
+		}
+
+		const string &replyToMessageId = message->getReplyToMessageId();
+		if (!replyToMessageId.empty()) {
+			cpimMessage.addMessageHeader(
+				Cpim::GenericHeader(imdnNamespace + "." + imdnReplyingToMessageIdHeader, replyToMessageId)
+			);
+			const IdentityAddress& senderAddress = message->getReplyToSenderAddress();
+			string address = senderAddress.asString();
+			cpimMessage.addMessageHeader(
+				Cpim::GenericHeader(imdnNamespace + "." + imdnReplyingToMessageSenderHeader, address)
 			);
 		}
 
@@ -224,6 +238,12 @@ ChatMessageModifier::Result CpimChatMessageModifier::decode (const shared_ptr<Ch
 		auto forwardInfoHeader = cpimMessage->getMessageHeader(imdnForwardInfoHeader, imdnNsName);
 		if (forwardInfoHeader) {
 			message->getPrivate()->setForwardInfo(forwardInfoHeader->getValue());
+		}
+
+		auto replyToMessageIdHeader = cpimMessage->getMessageHeader(imdnReplyingToMessageIdHeader, imdnNsName);
+		auto replyToSenderHeader = cpimMessage->getMessageHeader(imdnReplyingToMessageSenderHeader, imdnNsName);
+		if (replyToMessageIdHeader && replyToSenderHeader) {
+			message->getPrivate()->setReplyToMessageIdAndSenderAddress(replyToMessageIdHeader->getValue(), IdentityAddress(replyToSenderHeader->getValue()));
 		}
 	}
 

BelledonneCommunications/linphone-sdk

liblinphone/src/chat/modifier/cpim-chat-message-modifier.cpp
@@ -39,6 +39,8 @@ LINPHONE_BEGIN_NAMESPACE
 const string linphoneNamespaceTag = "tag:linphone.org,2020:params:groupchat";
 const string linphoneNamespace = "linphone";
 const string linphoneEphemeralHeader = "Ephemeral-Time";
+const string linphoneReplyingToMessageIdHeader = "Replying-To-Message-ID";
+const string linphoneReplyingToMessageSenderHeader = "Replying-To-Sender";
 
 const string imdnNamespaceUrn = "urn:ietf:params:imdn";
 const string imdnNamespace = "imdn";
@@ -90,6 +92,21 @@ ChatMessageModifier::Result CpimChatMessageModifier::encode (const shared_ptr<Ch
 		if (!forwardInfo.empty()) {
 			cpimMessage.addMessageHeader(
 				Cpim::GenericHeader(imdnNamespace + "." + imdnForwardInfoHeader, forwardInfo)
+			);
+		}
+
+		const string &replyToMessageId = message->getReplyToMessageId();
+		if (!replyToMessageId.empty()) {
+			if (!message->isEphemeral()) { // If message is ephemeral linphone namespace has already been set
+				cpimMessage.addMessageHeader(Cpim::NsHeader(linphoneNamespaceTag, linphoneNamespace));
+			}
+			cpimMessage.addMessageHeader(
+				Cpim::GenericHeader(linphoneNamespace + "." + linphoneReplyingToMessageIdHeader, replyToMessageId)
+			);
+			const IdentityAddress& senderAddress = message->getReplyToSenderAddress();
+			string address = senderAddress.asString();
+			cpimMessage.addMessageHeader(
+				Cpim::GenericHeader(linphoneNamespace + "." + linphoneReplyingToMessageSenderHeader, address)
 			);
 		}
 
@@ -229,8 +246,16 @@ ChatMessageModifier::Result CpimChatMessageModifier::decode (const shared_ptr<Ch
 
 	if (!linphoneNsName.empty()) {
 		auto timeHeader = cpimMessage->getMessageHeader(linphoneEphemeralHeader, linphoneNsName);
-		long time = (long)Utils::stod(timeHeader->getValue());
-		message->getPrivate()->enableEphemeralWithTime(time);
+		if (timeHeader) {
+			long time = (long)Utils::stod(timeHeader->getValue());
+			message->getPrivate()->enableEphemeralWithTime(time);
+		}
+
+		auto replyToMessageIdHeader = cpimMessage->getMessageHeader(linphoneReplyingToMessageIdHeader, linphoneNsName);
+		auto replyToSenderHeader = cpimMessage->getMessageHeader(linphoneReplyingToMessageSenderHeader, linphoneNsName);
+		if (replyToMessageIdHeader && replyToSenderHeader) {
+			message->getPrivate()->setReplyToMessageIdAndSenderAddress(replyToMessageIdHeader->getValue(), IdentityAddress(replyToSenderHeader->getValue()));
+		}
 	}
 
 	if (messageIdHeader)