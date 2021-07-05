BelledonneCommunications/liblinphone

Added separated setting to allow voice recordings to be auto downloaded

coreapi/linphonecore.c
@@ -1677,6 +1677,9 @@ static void sip_config_read(LinphoneCore *lc) {
 	tmp=linphone_config_get_int(lc->config,"app","auto_download_incoming_files_max_size",-1);
 	linphone_core_set_max_size_for_auto_download_incoming_files(lc, tmp);
 
+	bool_t auto_download_voice_recordings = linphone_config_get_bool(lc->config, "app", "auto_download_incoming_voice_recordings", FALSE);
+	linphone_core_enable_auto_download_voice_recordings(lc, auto_download_voice_recordings);
+
 	tmp=linphone_config_get_int(lc->config,"app","sender_name_hidden_in_forward_message",0);
 	linphone_core_enable_sender_name_hidden_in_forward_message(lc, !!tmp);
 
@@ -4789,6 +4792,15 @@ int linphone_core_get_max_size_for_auto_download_incoming_files(LinphoneCore *lc
 void linphone_core_set_max_size_for_auto_download_incoming_files(LinphoneCore *lc, int size) {
 	lc->auto_download_incoming_files_max_size = size;
 	linphone_config_set_int(lc->config, "app", "auto_download_incoming_files_max_size", size);
+}
+
+bool_t linphone_core_auto_download_voice_recordings_enabled(LinphoneCore *lc) {
+	return lc->auto_download_incoming_voice_recordings;
+}
+
+void linphone_core_enable_auto_download_voice_recordings(LinphoneCore *lc, bool_t auto_download_voice_recordings) {
+	lc->auto_download_incoming_voice_recordings = auto_download_voice_recordings;
+	linphone_config_set_bool(lc->config, "app", "auto_download_incoming_voice_recordings", auto_download_voice_recordings);
 }
 
 bool_t linphone_core_is_sender_name_hidden_in_forward_message(LinphoneCore *lc) {

include/linphone/core.h
@@ -5512,6 +5512,22 @@ LINPHONE_PUBLIC void linphone_core_set_max_size_for_auto_download_incoming_files
 LINPHONE_PUBLIC int linphone_core_get_max_size_for_auto_download_incoming_files(LinphoneCore *core);
 
 /**
+ * Auto download files attach to a chat message if it's content type matches the one we use for voice recordings.
+ * @param core #LinphoneCore object @notnil
+ * @param auto_download_voice_recordings TRUE to automatically download incoming voice recordings, FALSE to disable it.
+ * @ingroup chat
+**/
+LINPHONE_PUBLIC void linphone_core_enable_auto_download_voice_recordings(LinphoneCore *core, bool_t auto_download_voice_recordings);
+
+/**
+ * Gets if the auto download for incoming voice recordings is enabled or not.
+ * @param core #LinphoneCore object @notnil
+ * @return TRUE if voice recordings will be automatically downloaded, FALSE otherwise.
+ * @ingroup chat
+**/
+LINPHONE_PUBLIC bool_t linphone_core_auto_download_voice_recordings_enabled(LinphoneCore *core);
+
+/**
  * Returns whether or not sender name is hidden in forward message.
  * @param core The #LinphoneCore @notnil
  * @return whether or not the feature

tester/message_tester.c
@@ -1216,7 +1216,7 @@ static void transfer_message_4(void) {
 	transfer_message_base(FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, -1, FALSE, FALSE);
 }
 
-static void message_with_voice_recording_base(bool_t create_message_from_recorder) {
+static void message_with_voice_recording_base(bool_t create_message_from_recorder, bool_t auto_download_only_voice_recordings) {
 	LinphoneCoreManager* marie = linphone_core_manager_new( "marie_rc");
 	LinphoneCoreManager* pauline = linphone_core_manager_new( "pauline_tcp_rc");
 
@@ -1224,8 +1224,13 @@ static void message_with_voice_recording_base(bool_t create_message_from_recorde
 
 	LinphoneChatRoom *room = linphone_core_get_chat_room(pauline->lc, marie->identity);
 	BC_ASSERT_TRUE(linphone_chat_room_is_empty(room));
+
 	// Force auto download
-	linphone_core_set_max_size_for_auto_download_incoming_files(marie->lc, 0);
+	if (auto_download_only_voice_recordings) {
+		linphone_core_enable_auto_download_voice_recordings(marie->lc, TRUE);
+	} else {
+		linphone_core_set_max_size_for_auto_download_incoming_files(marie->lc, 0);
+	}
 
 	LinphoneRecorderParams *params = linphone_core_create_recorder_params(pauline->lc);
 	linphone_recorder_params_set_file_format(params, LinphoneRecorderFileFormatWav);
@@ -1276,11 +1281,15 @@ static void message_with_voice_recording_base(bool_t create_message_from_recorde
 }
 
 static void message_with_voice_recording(void) {
-	message_with_voice_recording_base(TRUE);
+	message_with_voice_recording_base(TRUE, FALSE);
 }
 
 static void message_with_voice_recording_2(void) {
-	message_with_voice_recording_base(FALSE);
+	message_with_voice_recording_base(FALSE, FALSE);
+}
+
+static void message_with_voice_recording_3(void) {
+	message_with_voice_recording_base(FALSE, TRUE);
 }
 
 static void transfer_message_legacy(void) {
@@ -3715,6 +3724,7 @@ test_t message_tests[] = {
 	TEST_NO_TAG("Transfer message 4", transfer_message_4),
 	TEST_NO_TAG("Message with voice recording", message_with_voice_recording),
 	TEST_NO_TAG("Message with voice recording 2", message_with_voice_recording_2),
+	TEST_NO_TAG("Message with voice recording 3", message_with_voice_recording_3),
 	TEST_NO_TAG("Transfer message legacy", transfer_message_legacy),
 	TEST_NO_TAG("Transfer message with 2 files", transfer_message_2_files),
 	TEST_NO_TAG("Transfer message auto download", transfer_message_auto_download),

BelledonneCommunications/linphone-sdk

liblinphone/coreapi/linphonecore.c
@@ -1677,6 +1677,9 @@ static void sip_config_read(LinphoneCore *lc) {
 	tmp=linphone_config_get_int(lc->config,"app","auto_download_incoming_files_max_size",-1);
 	linphone_core_set_max_size_for_auto_download_incoming_files(lc, tmp);
 
+	bool_t auto_download_voice_recordings = linphone_config_get_bool(lc->config, "app", "auto_download_incoming_voice_recordings", TRUE);
+	linphone_core_set_auto_download_voice_recordings_enabled(lc, auto_download_voice_recordings);
+
 	tmp=linphone_config_get_int(lc->config,"app","sender_name_hidden_in_forward_message",0);
 	linphone_core_enable_sender_name_hidden_in_forward_message(lc, !!tmp);
 
@@ -4789,6 +4792,15 @@ int linphone_core_get_max_size_for_auto_download_incoming_files(LinphoneCore *lc
 void linphone_core_set_max_size_for_auto_download_incoming_files(LinphoneCore *lc, int size) {
 	lc->auto_download_incoming_files_max_size = size;
 	linphone_config_set_int(lc->config, "app", "auto_download_incoming_files_max_size", size);
+}
+
+bool_t linphone_core_is_auto_download_voice_recordings_enabled(LinphoneCore *lc) {
+	return lc->auto_download_incoming_voice_recordings;
+}
+
+void linphone_core_set_auto_download_voice_recordings_enabled(LinphoneCore *lc, bool_t auto_download_voice_recordings) {
+	lc->auto_download_incoming_voice_recordings = auto_download_voice_recordings;
+	linphone_config_set_bool(lc->config, "app", "auto_download_incoming_voice_recordings", auto_download_voice_recordings);
 }
 
 bool_t linphone_core_is_sender_name_hidden_in_forward_message(LinphoneCore *lc) {

liblinphone/include/linphone/core.h
@@ -5512,6 +5512,22 @@ LINPHONE_PUBLIC void linphone_core_set_max_size_for_auto_download_incoming_files
 LINPHONE_PUBLIC int linphone_core_get_max_size_for_auto_download_incoming_files(LinphoneCore *core);
 
 /**
+ * Auto download files attach to a chat message if it's content type matches the one we use for voice recordings.
+ * @param core #LinphoneCore object @notnil
+ * @param auto_download_voice_recordings TRUE to automatically download incoming voice recordings, FALSE to disable it.
+ * @ingroup chat
+**/
+LINPHONE_PUBLIC void linphone_core_set_auto_download_voice_recordings_enabled(LinphoneCore *core, bool_t auto_download_voice_recordings);
+
+/**
+ * Gets if the auto download for incoming voice recordings is enabled or not.
+ * @param core #LinphoneCore object @notnil
+ * @return TRUE if voice recordings will be automatically downloaded, FALSE otherwise.
+ * @ingroup chat
+**/
+LINPHONE_PUBLIC bool_t linphone_core_is_auto_download_voice_recordings_enabled(LinphoneCore *core);
+
+/**
  * Returns whether or not sender name is hidden in forward message.
  * @param core The #LinphoneCore @notnil
  * @return whether or not the feature

liblinphone/tester/message_tester.c
@@ -1216,7 +1216,7 @@ static void transfer_message_4(void) {
 	transfer_message_base(FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, -1, FALSE, FALSE);
 }
 
-static void message_with_voice_recording_base(bool_t create_message_from_recorder) {
+static void message_with_voice_recording_base(bool_t create_message_from_recorder, bool_t auto_download_only_voice_recordings) {
 	LinphoneCoreManager* marie = linphone_core_manager_new( "marie_rc");
 	LinphoneCoreManager* pauline = linphone_core_manager_new( "pauline_tcp_rc");
 
@@ -1224,8 +1224,14 @@ static void message_with_voice_recording_base(bool_t create_message_from_recorde
 
 	LinphoneChatRoom *room = linphone_core_get_chat_room(pauline->lc, marie->identity);
 	BC_ASSERT_TRUE(linphone_chat_room_is_empty(room));
+
 	// Force auto download
-	linphone_core_set_max_size_for_auto_download_incoming_files(marie->lc, 0);
+	if (auto_download_only_voice_recordings) {
+		linphone_core_set_auto_download_voice_recordings_enabled(marie->lc, TRUE);
+	} else {
+		linphone_core_set_auto_download_voice_recordings_enabled(marie->lc, FALSE);
+		linphone_core_set_max_size_for_auto_download_incoming_files(marie->lc, 0);
+	}
 
 	LinphoneRecorderParams *params = linphone_core_create_recorder_params(pauline->lc);
 	linphone_recorder_params_set_file_format(params, LinphoneRecorderFileFormatWav);
@@ -1276,11 +1282,15 @@ static void message_with_voice_recording_base(bool_t create_message_from_recorde
 }
 
 static void message_with_voice_recording(void) {
-	message_with_voice_recording_base(TRUE);
+	message_with_voice_recording_base(TRUE, FALSE);
 }
 
 static void message_with_voice_recording_2(void) {
-	message_with_voice_recording_base(FALSE);
+	message_with_voice_recording_base(FALSE, FALSE);
+}
+
+static void message_with_voice_recording_3(void) {
+	message_with_voice_recording_base(FALSE, TRUE);
 }
 
 static void transfer_message_legacy(void) {
@@ -3715,6 +3725,7 @@ test_t message_tests[] = {
 	TEST_NO_TAG("Transfer message 4", transfer_message_4),
 	TEST_NO_TAG("Message with voice recording", message_with_voice_recording),
 	TEST_NO_TAG("Message with voice recording 2", message_with_voice_recording_2),
+	TEST_NO_TAG("Message with voice recording 3", message_with_voice_recording_3),
 	TEST_NO_TAG("Transfer message legacy", transfer_message_legacy),
 	TEST_NO_TAG("Transfer message with 2 files", transfer_message_2_files),
 	TEST_NO_TAG("Transfer message auto download", transfer_message_auto_download),