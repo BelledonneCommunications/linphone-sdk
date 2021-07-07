BelledonneCommunications/liblinphone

Add audio level indication support for RFCs 6464 and 6465

src/sal/offeranswer.cpp
@@ -414,6 +414,9 @@ void OfferAnswerEngine::initiateOutgoingStream(MSFactory* factory, const SalStre
 		}
 	}
 
+	result.mixer_to_client_extension_id = remote_answer.mixer_to_client_extension_id;
+	result.client_to_mixer_extension_id = remote_answer.client_to_mixer_extension_id;
+
 	if (!result.payloads.empty() && !OfferAnswerEngine::onlyTelephoneEvent(result.payloads)){
 		result.rtp_addr=remote_answer.rtp_addr;
 		result.rtcp_addr=remote_answer.rtcp_addr;
@@ -511,6 +514,9 @@ void OfferAnswerEngine::initiateIncomingStream(MSFactory *factory, const SalStre
 		}
 		result.rtcp_mux = TRUE; /* RTCP mux must be enabled in bundle mode. */
 	}
+
+	result.mixer_to_client_extension_id = remote_offer.mixer_to_client_extension_id;
+	result.client_to_mixer_extension_id = remote_offer.client_to_mixer_extension_id;
 
 	if (result.hasSrtp() == TRUE) {
 		/* select crypto algo */

BelledonneCommunications/linphone-sdk

liblinphone/src/sal/offeranswer.cpp
@@ -515,6 +515,10 @@ std::pair<SalStreamConfiguration, bool> OfferAnswerEngine::initiateOutgoingConfi
 		}
 	}
 
+	resultCfg.mixer_to_client_extension_id = remoteCfg.mixer_to_client_extension_id;
+	resultCfg.client_to_mixer_extension_id = remoteCfg.client_to_mixer_extension_id;
+	resultCfg.conference_ssrc = remoteCfg.conference_ssrc;
+
 	if (!resultCfg.payloads.empty() && !OfferAnswerEngine::onlyTelephoneEvent(resultCfg.payloads)){
 		resultCfg.ptime=remoteCfg.ptime;
 		resultCfg.maxptime=remoteCfg.maxptime;
@@ -712,6 +716,10 @@ std::pair<SalStreamConfiguration, bool> OfferAnswerEngine::initiateIncomingConfi
 		}
 		resultCfg.rtcp_mux = true; /* RTCP mux must be enabled in bundle mode. */
 	}
+
+	resultCfg.mixer_to_client_extension_id = remoteCfg.mixer_to_client_extension_id;
+	resultCfg.client_to_mixer_extension_id = remoteCfg.client_to_mixer_extension_id;
+	resultCfg.conference_ssrc = localCfg.conference_ssrc;
 
 	if (resultCfg.hasSrtp() == true) {
 		const auto srtpFound = std::find(availableEncs.cbegin(), availableEncs.cend(), LinphoneMediaEncryptionSRTP);