BelledonneCommunications/liblinphone

Add an updat method attribute to choose whether the update method should use sip update or reinvite

src/chat/chat-room/client-group-chat-room.cpp
@@ -731,7 +731,7 @@ void ClientGroupChatRoom::setSubject (const string &subject) {
 
 	shared_ptr<CallSession> session = static_pointer_cast<RemoteConference>(getConference())->focus->getSession();
 	if (session)
-		session->update(nullptr, subject);
+		session->update(nullptr, CallSession::UpdateMethod::Update, subject);
 	else {
 		session = d->createSession();
 		session->startInvite(nullptr, subject, nullptr);
@@ -1159,7 +1159,7 @@ void ClientGroupChatRoom::setEphemeralMode(AbstractChatRoom::EphemeralMode mode,
 			csp->addCustomHeader("Ephemeral-Life-Time", to_string(lifetime));
 		}
 		lInfo() << "Changing ephemeral mode to " << Utils::toString(mode);
-		session->update(csp, getSubject());
+		session->update(csp, CallSession::UpdateMethod::Update, getSubject());
 		delete csp;
 	} else {
 		lError() << "Cannot change the ClientGroupChatRoom ephemeral lifetime in a state other than Created";
@@ -1221,7 +1221,7 @@ void ClientGroupChatRoom::enableEphemeral (bool ephem, bool updateDb) {
 			auto csp = session->getParams()->clone();
 			csp->removeCustomHeader("Ephemeral-Life-Time");
 			csp->addCustomHeader("Ephemeral-Life-Time", (ephem ? to_string(lifetime) : "0"));
-			session->update(csp, getSubject());
+			session->update(csp, CallSession::UpdateMethod::Update, getSubject());
 			delete csp;
 		} else {
 			lError() << "Cannot change the ClientGroupChatRoom ephemeral lifetime in a state other than Created";
@@ -1285,7 +1285,7 @@ void ClientGroupChatRoom::setEphemeralLifetime (long lifetime, bool updateDb) {
 				csp->removeCustomHeader("Ephemeral-Life-Time");
 				csp->addCustomHeader("Ephemeral-Life-Time", to_string(lifetime));
 
-				session->update(csp, getSubject());
+				session->update(csp, CallSession::UpdateMethod::Update, getSubject());
 				delete csp;
 			} else {
 				enableEphemeral(enable, true);

src/conference/session/media-session.cpp
@@ -154,7 +154,7 @@ bool MediaSessionPrivate::tryEnterConference() {
 					// Send update to notify that the call enters conference
 					MediaSessionParams *newParams = q->getMediaParams()->clone();
 					lInfo() << "Media session (local address " << q->getLocalAddress().asString() << " remote address " << q->getRemoteAddress()->asString() << ") was added to conference " << conference->getConferenceAddress() << " while the call was establishing. Sending update to notify remote participant.";
-					q->update(newParams, q->isCapabilityNegotiationEnabled());
+					q->update(newParams, CallSession::UpdateMethod::Update, q->isCapabilityNegotiationEnabled());
 					delete newParams;
 				}
 				return true;
@@ -302,7 +302,7 @@ void MediaSessionPrivate::accepted () {
 						lInfo() << "Sending a reINVITE because the actual configuraton was not chosen in the capability negotiation procedure. Detected differences " << SalMediaDescription::printDifferences(diff);
 						MediaSessionParams newParams(*getParams());
 						newParams.getPrivate()->setInternalCallUpdate(true);
-						q->update(&newParams, true);
+						q->update(&newParams, CallSession::UpdateMethod::Default, true);
 						capabilityNegotiationReInviteSent = true;
 					} else {
 						lInfo() << "Using actual configuration after capability negotiation procedure, hence no need to send a reINVITE";
@@ -1863,7 +1863,7 @@ void MediaSessionPrivate::onIceCompleted(IceService &service){
 			{
 				MediaSessionParams newParams(*getParams());
 				newParams.getPrivate()->setInternalCallUpdate(true);
-				q->update(&newParams, q->isCapabilityNegotiationEnabled());
+				q->update(&newParams, CallSession::UpdateMethod::Default, q->isCapabilityNegotiationEnabled());
 			}
 			break;
 			default:
@@ -1889,7 +1889,7 @@ void MediaSessionPrivate::onIceRestartNeeded(IceService & service){
 	L_Q();
 	getStreamsGroup().getIceService().restartSession(IR_Controlling);
 	MediaSessionParams newParams(*getParams());
-	q->update(&newParams, q->isCapabilityNegotiationEnabled());
+	q->update(&newParams, CallSession::UpdateMethod::Default, q->isCapabilityNegotiationEnabled());
 }
 
 void MediaSessionPrivate::tryEarlyMediaForking (std::shared_ptr<SalMediaDescription> & md) {
@@ -2189,14 +2189,14 @@ LinphoneStatus MediaSessionPrivate::startAcceptUpdate (CallSession::State nextSt
 	return 0;
 }
 
-LinphoneStatus MediaSessionPrivate::startUpdate (const string &subject) {
+LinphoneStatus MediaSessionPrivate::startUpdate (const CallSession::UpdateMethod method, const string &subject) {
 	L_Q();
 
 	const bool doNotAddSdpToInvite = q->getCore()->getCCore()->sip_conf.sdp_200_ack && !getParams()->getPrivate()->getInternalCallUpdate();
 	if (doNotAddSdpToInvite) {
 		op->setLocalMediaDescription(nullptr);
 	}
-	LinphoneStatus result = CallSessionPrivate::startUpdate(subject);
+	LinphoneStatus result = CallSessionPrivate::startUpdate(method, subject);
 	if (doNotAddSdpToInvite) {
 		// We are NOT offering, set local media description after sending the call so that we are ready to
 		// process the remote offer when it will arrive.
@@ -2530,7 +2530,7 @@ void MediaSessionPrivate::reinviteToRecoverFromConnectionLoss () {
 	lInfo() << "MediaSession [" << q << "] is going to be updated (reINVITE) in order to recover from lost connectivity";
 	getStreamsGroup().getIceService().resetSession();
 	MediaSessionParams newParams(*getParams());
-	q->update(&newParams, q->isCapabilityNegotiationEnabled());
+	q->update(&newParams, CallSession::UpdateMethod::Invite, q->isCapabilityNegotiationEnabled());
 }
 
 void MediaSessionPrivate::repairByInviteWithReplaces () {
@@ -3077,10 +3077,10 @@ LinphoneStatus MediaSession::updateFromConference (const MediaSessionParams *msp
 	updateContactAddress (contactAddress);
 	d->op->setContactAddress(contactAddress.getInternalAddress());
 
-	return update(msp, false, subject);
+	return update(msp, CallSession::UpdateMethod::Update, false, subject);
 }
 
-LinphoneStatus MediaSession::update (const MediaSessionParams *msp, const bool isCapabilityNegotiationUpdate, const string &subject) {
+LinphoneStatus MediaSession::update (const MediaSessionParams *msp, const UpdateMethod method, const bool isCapabilityNegotiationUpdate, const string &subject) {
 	L_D();
 	CallSession::State nextState;
 	CallSession::State initialState = d->state;
@@ -3097,13 +3097,12 @@ LinphoneStatus MediaSession::update (const MediaSessionParams *msp, const bool i
 		// Add capability negotiation attributes if caapbility negotiation is enabled and it is not a reINVITE following conclusion of the capability negotiation procedure
 		bool addCapabilityNegotiationAttributesToLocalMd = isCapabilityNegotiationEnabled() && !isCapabilityNegotiationUpdate;
 		bool isCapabilityNegotiationReInvite = isCapabilityNegotiationEnabled() && isCapabilityNegotiationUpdate;
-		//if (!d->getParams()->getPrivate()->getNoUserConsent())
-			d->makeLocalMediaDescription(d->localIsOfferer, addCapabilityNegotiationAttributesToLocalMd, isCapabilityNegotiationReInvite);
+		d->makeLocalMediaDescription(d->localIsOfferer, addCapabilityNegotiationAttributesToLocalMd, isCapabilityNegotiationReInvite);
 
-		auto updateCompletionTask = [this, subject, initialState]() -> LinphoneStatus{
+		auto updateCompletionTask = [this, method, subject, initialState]() -> LinphoneStatus{
 			L_D();
 			d->updateLocalMediaDescriptionFromIce(d->localIsOfferer);
-			LinphoneStatus res = d->startUpdate(subject);
+			LinphoneStatus res = d->startUpdate(method, subject);
 			if (res && (d->state != initialState)) {
 				/* Restore initial state */
 				d->setState(initialState, "Restore initial state");

BelledonneCommunications/linphone-sdk

liblinphone/src/chat/chat-room/client-group-chat-room.cpp
@@ -731,7 +731,7 @@ void ClientGroupChatRoom::setSubject (const string &subject) {
 
 	shared_ptr<CallSession> session = static_pointer_cast<RemoteConference>(getConference())->focus->getSession();
 	if (session)
-		session->update(nullptr, subject);
+		session->update(nullptr, CallSession::UpdateMethod::Default, subject);
 	else {
 		session = d->createSession();
 		session->startInvite(nullptr, subject, nullptr);
@@ -1159,7 +1159,7 @@ void ClientGroupChatRoom::setEphemeralMode(AbstractChatRoom::EphemeralMode mode,
 			csp->addCustomHeader("Ephemeral-Life-Time", to_string(lifetime));
 		}
 		lInfo() << "Changing ephemeral mode to " << Utils::toString(mode);
-		session->update(csp, getSubject());
+		session->update(csp, CallSession::UpdateMethod::Default, getSubject());
 		delete csp;
 	} else {
 		lError() << "Cannot change the ClientGroupChatRoom ephemeral lifetime in a state other than Created";
@@ -1221,7 +1221,7 @@ void ClientGroupChatRoom::enableEphemeral (bool ephem, bool updateDb) {
 			auto csp = session->getParams()->clone();
 			csp->removeCustomHeader("Ephemeral-Life-Time");
 			csp->addCustomHeader("Ephemeral-Life-Time", (ephem ? to_string(lifetime) : "0"));
-			session->update(csp, getSubject());
+			session->update(csp, CallSession::UpdateMethod::Default, getSubject());
 			delete csp;
 		} else {
 			lError() << "Cannot change the ClientGroupChatRoom ephemeral lifetime in a state other than Created";
@@ -1285,7 +1285,7 @@ void ClientGroupChatRoom::setEphemeralLifetime (long lifetime, bool updateDb) {
 				csp->removeCustomHeader("Ephemeral-Life-Time");
 				csp->addCustomHeader("Ephemeral-Life-Time", to_string(lifetime));
 
-				session->update(csp, getSubject());
+				session->update(csp, CallSession::UpdateMethod::Default, getSubject());
 				delete csp;
 			} else {
 				enableEphemeral(enable, true);

liblinphone/src/conference/session/media-session.cpp
@@ -154,7 +154,7 @@ bool MediaSessionPrivate::tryEnterConference() {
 					// Send update to notify that the call enters conference
 					MediaSessionParams *newParams = q->getMediaParams()->clone();
 					lInfo() << "Media session (local address " << q->getLocalAddress().asString() << " remote address " << q->getRemoteAddress()->asString() << ") was added to conference " << conference->getConferenceAddress() << " while the call was establishing. Sending update to notify remote participant.";
-					q->update(newParams, q->isCapabilityNegotiationEnabled());
+					q->update(newParams, CallSession::UpdateMethod::Default, q->isCapabilityNegotiationEnabled());
 					delete newParams;
 				}
 				return true;
@@ -302,7 +302,7 @@ void MediaSessionPrivate::accepted () {
 						lInfo() << "Sending a reINVITE because the actual configuraton was not chosen in the capability negotiation procedure. Detected differences " << SalMediaDescription::printDifferences(diff);
 						MediaSessionParams newParams(*getParams());
 						newParams.getPrivate()->setInternalCallUpdate(true);
-						q->update(&newParams, true);
+						q->update(&newParams, CallSession::UpdateMethod::Default, true);
 						capabilityNegotiationReInviteSent = true;
 					} else {
 						lInfo() << "Using actual configuration after capability negotiation procedure, hence no need to send a reINVITE";
@@ -1863,7 +1863,7 @@ void MediaSessionPrivate::onIceCompleted(IceService &service){
 			{
 				MediaSessionParams newParams(*getParams());
 				newParams.getPrivate()->setInternalCallUpdate(true);
-				q->update(&newParams, q->isCapabilityNegotiationEnabled());
+				q->update(&newParams, CallSession::UpdateMethod::Default, q->isCapabilityNegotiationEnabled());
 			}
 			break;
 			default:
@@ -1889,7 +1889,7 @@ void MediaSessionPrivate::onIceRestartNeeded(IceService & service){
 	L_Q();
 	getStreamsGroup().getIceService().restartSession(IR_Controlling);
 	MediaSessionParams newParams(*getParams());
-	q->update(&newParams, q->isCapabilityNegotiationEnabled());
+	q->update(&newParams, CallSession::UpdateMethod::Default, q->isCapabilityNegotiationEnabled());
 }
 
 void MediaSessionPrivate::tryEarlyMediaForking (std::shared_ptr<SalMediaDescription> & md) {
@@ -2189,14 +2189,14 @@ LinphoneStatus MediaSessionPrivate::startAcceptUpdate (CallSession::State nextSt
 	return 0;
 }
 
-LinphoneStatus MediaSessionPrivate::startUpdate (const string &subject) {
+LinphoneStatus MediaSessionPrivate::startUpdate (const CallSession::UpdateMethod method, const string &subject) {
 	L_Q();
 
 	const bool doNotAddSdpToInvite = q->getCore()->getCCore()->sip_conf.sdp_200_ack && !getParams()->getPrivate()->getInternalCallUpdate();
 	if (doNotAddSdpToInvite) {
 		op->setLocalMediaDescription(nullptr);
 	}
-	LinphoneStatus result = CallSessionPrivate::startUpdate(subject);
+	LinphoneStatus result = CallSessionPrivate::startUpdate(method, subject);
 	if (doNotAddSdpToInvite) {
 		// We are NOT offering, set local media description after sending the call so that we are ready to
 		// process the remote offer when it will arrive.
@@ -2530,7 +2530,7 @@ void MediaSessionPrivate::reinviteToRecoverFromConnectionLoss () {
 	lInfo() << "MediaSession [" << q << "] is going to be updated (reINVITE) in order to recover from lost connectivity";
 	getStreamsGroup().getIceService().resetSession();
 	MediaSessionParams newParams(*getParams());
-	q->update(&newParams, q->isCapabilityNegotiationEnabled());
+	q->update(&newParams, CallSession::UpdateMethod::Invite, q->isCapabilityNegotiationEnabled());
 }
 
 void MediaSessionPrivate::repairByInviteWithReplaces () {
@@ -3077,10 +3077,10 @@ LinphoneStatus MediaSession::updateFromConference (const MediaSessionParams *msp
 	updateContactAddress (contactAddress);
 	d->op->setContactAddress(contactAddress.getInternalAddress());
 
-	return update(msp, false, subject);
+	return update(msp, CallSession::UpdateMethod::Default, false, subject);
 }
 
-LinphoneStatus MediaSession::update (const MediaSessionParams *msp, const bool isCapabilityNegotiationUpdate, const string &subject) {
+LinphoneStatus MediaSession::update (const MediaSessionParams *msp, const UpdateMethod method, const bool isCapabilityNegotiationUpdate, const string &subject) {
 	L_D();
 	CallSession::State nextState;
 	CallSession::State initialState = d->state;
@@ -3097,13 +3097,12 @@ LinphoneStatus MediaSession::update (const MediaSessionParams *msp, const bool i
 		// Add capability negotiation attributes if caapbility negotiation is enabled and it is not a reINVITE following conclusion of the capability negotiation procedure
 		bool addCapabilityNegotiationAttributesToLocalMd = isCapabilityNegotiationEnabled() && !isCapabilityNegotiationUpdate;
 		bool isCapabilityNegotiationReInvite = isCapabilityNegotiationEnabled() && isCapabilityNegotiationUpdate;
-		//if (!d->getParams()->getPrivate()->getNoUserConsent())
-			d->makeLocalMediaDescription(d->localIsOfferer, addCapabilityNegotiationAttributesToLocalMd, isCapabilityNegotiationReInvite);
+		d->makeLocalMediaDescription(d->localIsOfferer, addCapabilityNegotiationAttributesToLocalMd, isCapabilityNegotiationReInvite);
 
-		auto updateCompletionTask = [this, subject, initialState]() -> LinphoneStatus{
+		auto updateCompletionTask = [this, method, subject, initialState]() -> LinphoneStatus{
 			L_D();
 			d->updateLocalMediaDescriptionFromIce(d->localIsOfferer);
-			LinphoneStatus res = d->startUpdate(subject);
+			LinphoneStatus res = d->startUpdate(method, subject);
 			if (res && (d->state != initialState)) {
 				/* Restore initial state */
 				d->setState(initialState, "Restore initial state");