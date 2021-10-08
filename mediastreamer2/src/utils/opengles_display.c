BelledonneCommunications/mediastreamer2

Add video display mode to be able to switch display to black bars, crop or hybrid

src/utils/opengles_display.c
@@ -425,7 +425,8 @@ static void ogl_display_render_type(
 	float vpy,
 	float vpw,
 	float vph,
-	int orientation
+	int orientation,
+	OglDisplayMode mode
 ) {
 	const OpenGlFunctions *f = gldisp->functions;
 
@@ -436,6 +437,7 @@ static void ogl_display_render_type(
 	GLfloat x,y,w,h;
 	GLfloat mat[16];
 	float rad;
+	OglDisplayMode usedMode;
 
 	if (!gldisp) {
 		ms_error("[ogl_display] %s called with null struct opengles_display", __FUNCTION__);
@@ -478,22 +480,46 @@ static void ogl_display_render_type(
 		screenH = (GLfloat)gldisp->backingWidth;
 	}
 
+	usedMode = mode;
+	if (mode == OglDisplayHybrid) {
+		// If the image has the same orientation as the screen then use OccupyAllSpace mode
+		if ((screenW > screenH && gldisp->yuv_size[type].width > gldisp->yuv_size[type].height)
+			|| (screenH > screenW && gldisp->yuv_size[type].height > gldisp->yuv_size[type].width)
+		) {
+			usedMode = OglDisplayOccupyAllSpace;
+		} else {
+			usedMode = OglDisplayBlackBars;
+		}
+	}
+
 	// Fill the smallest dimension, then compute the other one using the image ratio
-	if (screenW <= screenH) {
-		float ratio = gldisp->yuv_size[type].height / (float)(gldisp->yuv_size[type].width);
-		w = screenW * vpw;
-		h = w * ratio;
-		if (h > screenH) {
-			w *= screenH / (float)h;
-			h = screenH;
+	if (usedMode == OglDisplayBlackBars) {
+		if (screenW <= screenH) {
+			float ratio = gldisp->yuv_size[type].height / (float)(gldisp->yuv_size[type].width);
+			w = screenW * vpw;
+			h = w * ratio;
+			if (h > screenH) {
+				w *= screenH / (float)h;
+				h = screenH;
+			}
+		} else {
+			float ratio = gldisp->yuv_size[type].width / (float)(gldisp->yuv_size[type].height);
+			h = screenH * vph;
+			w = h * ratio;
+			if (w > screenW) {
+				h *= screenW / (float)w;
+				w = screenW;
+			}
 		}
 	} else {
-		float ratio = gldisp->yuv_size[type].width / (float)(gldisp->yuv_size[type].height);
-		h = screenH * vph;
-		w = h * ratio;
-		if (w > screenW) {
-			h *= screenW / (float)w;
-			w = screenW;
+		if (screenW > screenH) {
+			float ratio = gldisp->yuv_size[type].height / (float)(gldisp->yuv_size[type].width);
+			w = screenW * vpw;
+			h = w * ratio;
+		} else {
+			float ratio = gldisp->yuv_size[type].width / (float)(gldisp->yuv_size[type].height);
+			h = screenH * vph;
+			w = h * ratio;
 		}
 	}
 
@@ -1284,7 +1310,7 @@ void ogl_display_set_preview_yuv_to_display (struct opengles_display *gldisp, mb
 	ogl_display_set_yuv(gldisp, yuv, PREVIEW_IMAGE);
 }
 
-void ogl_display_render (struct opengles_display *gldisp, int orientation) {
+void ogl_display_render (struct opengles_display *gldisp, int orientation, OglDisplayMode mode) {
 	const OpenGlFunctions *f = gldisp->functions;
 	bool_t render = TRUE;
 
@@ -1310,9 +1336,9 @@ void ogl_display_render (struct opengles_display *gldisp, int orientation) {
 		GL_OPERATION(f, glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 		GL_OPERATION(f, glUseProgram(gldisp->program))
 		check_GL_errors(f, "ogl_display_render");
-		ogl_display_render_type(gldisp, REMOTE_IMAGE, TRUE, 0, 0, 1, 1, orientation);
+		ogl_display_render_type(gldisp, REMOTE_IMAGE, TRUE, 0, 0, 1, 1, orientation, mode);
 		// preview image already have the correct orientation
-		ogl_display_render_type(gldisp, PREVIEW_IMAGE, FALSE, 0.4f, -0.4f, 0.2f, 0.2f, 0);
+		ogl_display_render_type(gldisp, PREVIEW_IMAGE, FALSE, 0.4f, -0.4f, 0.2f, 0.2f, 0, OglDisplayBlackBars);
 		gldisp->texture_index = (gldisp->texture_index + 1) % TEXTURE_BUFFER_SIZE;
 		if(f->eglInitialized && gldisp->mRenderSurface != EGL_NO_SURFACE)
 			f->eglSwapBuffers(gldisp->mEglDisplay, gldisp->mRenderSurface);
@@ -1347,7 +1373,7 @@ JNIEXPORT void JNICALL Java_org_linphone_mediastream_video_display_OpenGLESDispl
 
 JNIEXPORT void JNICALL Java_org_linphone_mediastream_video_display_OpenGLESDisplay_render (JNIEnv * env, jobject obj, jlong ptr) {
 	struct opengles_display* d = (struct opengles_display*) ptr;
-	ogl_display_render(d, 0);
+	ogl_display_render(d, 0, OglDisplayBlackBars);
 }
 #endif
 

src/videofilters/generic_opengl_display.c
@@ -64,6 +64,7 @@ struct _FilterData {
 	bool_t update_context;
 
 	mblk_t * prev_inm;
+	OglDisplayMode mode;
 };
 
 typedef struct _FilterData FilterData;
@@ -89,6 +90,7 @@ static void ogl_init (MSFilter *f) {
 	data->context_info.width=MS_VIDEO_SIZE_CIF_W;
 	data->context_info.height=MS_VIDEO_SIZE_CIF_H;
 	data->context_info.window = NULL;
+	data->mode = OglDisplayBlackBars;
 	f->data = data;
 	if(!gMutexInitialized){
 		gMutexInitialized = TRUE;
@@ -275,6 +277,14 @@ static int ogl_zoom (MSFilter *f, void *arg) {
 	return 0;
 }
 
+static int ogl_set_mode (MSFilter *f, void *arg) {
+	ms_filter_lock(f);
+	((FilterData *)f->data)->mode = *((OglDisplayMode*)arg);
+	ms_filter_unlock(f);
+
+	return 0;
+}
+
 static int ogl_enable_mirroring (MSFilter *f, void *arg) {
 	FilterData *data = (FilterData *)f->data;
 	ms_filter_lock(f);
@@ -311,7 +321,7 @@ static int ogl_call_render (MSFilter *f, void *arg) {
 		data->update_context = UPDATE_CONTEXT_NOTHING;
 	}
 	if (data->show_video && context_info && ( context_info->window || (!context_info->window && context_info->width && context_info->height)) ){
-		ogl_display_render(data->display, 0);
+		ogl_display_render(data->display, 0, data->mode);
 	}
 	ms_mutex_unlock(&gLock);
 	if( f != NULL)
@@ -343,6 +353,7 @@ static MSFilterMethod methods[] = {
 	{ MS_VIDEO_DISPLAY_SHOW_VIDEO, ogl_show_video },
 	{ MS_VIDEO_DISPLAY_ZOOM, ogl_zoom },
 	{ MS_VIDEO_DISPLAY_ENABLE_MIRRORING, ogl_enable_mirroring },
+	{ MS_VIDEO_DISPLAY_SET_MODE, ogl_set_mode },
 	{ MS_OGL_RENDER, ogl_call_render },
 	{ 0, NULL }
 };

src/videofilters/glxvideo.c
@@ -74,6 +74,7 @@ typedef struct GLXVideo
 	bool_t ready;
 	bool_t mirror;
 	bool_t autofit;
+	OglDisplayMode mode;
 } GLXVideo;
 
 
@@ -109,6 +110,7 @@ static void glxvideo_init(MSFilter	*f){
 	obj->wsize=def_size; /* the size of the window*/
 	obj->show=TRUE;
 	obj->autofit=TRUE;
+	obj->mode = OglDisplayBlackBars;
 	f->data=obj;
 
 	XSetErrorHandler(x11error_handler);
@@ -279,7 +281,7 @@ static void glxvideo_process(MSFilter *f){
 	}
 
 	const int orientation=0;
-	ogl_display_render(obj->glhelper, orientation);
+	ogl_display_render(obj->glhelper, orientation, obj->mode);
 	glXSwapBuffers ( obj->display, obj->subwindow );
 
 	end:
@@ -492,6 +494,12 @@ static int glxvideo_set_local_view_mode(MSFilter *f, void *arg){
 	return 0;
 }
 
+static int glxvideo_set_mode(MSFilter *f, void *arg){
+	GLXVideo *s=(GLXVideo*)f->data;
+	s->mode=*((OglDisplayMode*)arg);
+	return 0;
+}
+
 static MSFilterMethod methods[]={
 	{	MS_FILTER_SET_VIDEO_SIZE		, glxvideo_set_vsize },
 	{	MS_VIDEO_DISPLAY_GET_NATIVE_WINDOW_ID	, glxvideo_get_native_window_id },
@@ -501,6 +509,7 @@ static MSFilterMethod methods[]={
 	{	MS_VIDEO_DISPLAY_ENABLE_MIRRORING	, glxvideo_enable_mirroring},
 	{	MS_VIDEO_DISPLAY_ENABLE_AUTOFIT		, glxvideo_enable_autofit	},
 	{	MS_VIDEO_DISPLAY_SET_LOCAL_VIEW_MODE	, glxvideo_set_local_view_mode},
+	{	MS_VIDEO_DISPLAY_SET_MODE			, glxvideo_set_mode },
 	{	0	,NULL}
 };
 

src/videofilters/msqogl.cpp
@@ -103,6 +103,7 @@ static void qogl_init (MSFilter *f) {
 	data->parent = f;
 	data->is_qt_linked = FALSE;
 	data->is_sdk_linked = TRUE;
+	data->mode = OglDisplayBlackBars;
 	memset(&data->functions, 0, sizeof(data->functions));
 	data->functions.getProcAddress = getProcAddress;
 	
@@ -233,6 +234,13 @@ static int qogl_enable_mirroring (MSFilter *f, void *arg) {
 	return 0;
 }
 
+static int qogl_set_mode (MSFilter *f, void *arg) {
+	ms_filter_lock(f);
+	((FilterData *)f->data)->mode = *((OglDisplayMode*)arg);
+	ms_filter_unlock(f);
+	return 0;
+}
+
 static int qogl_call_render (MSFilter *f, void *arg) {
 	
 	FilterData *data;
@@ -245,7 +253,7 @@ static int qogl_call_render (MSFilter *f, void *arg) {
 			ogl_display_init(data->display, &data->functions, data->renderer->mWidth , data->renderer->mHeight);
 			data->update_context = FALSE;
 		}
-		ogl_display_render(data->display, 0);
+		ogl_display_render(data->display, 0, data->mode);
 	}
 	
 	ms_filter_unlock(f);
@@ -264,6 +272,7 @@ static MSFilterMethod methods[] = {
 	{ MS_VIDEO_DISPLAY_SHOW_VIDEO, qogl_show_video },
 	{ MS_VIDEO_DISPLAY_ZOOM, qogl_zoom },
 	{ MS_VIDEO_DISPLAY_ENABLE_MIRRORING, qogl_enable_mirroring },
+	{ MS_VIDEO_DISPLAY_SET_MODE, qogl_set_mode },
 	//{ MS_OGL_RENDER, qogl_call_render }, // qogl_call_render is autocalled by Qt, there is no need to put it in interface
 	{ 0, NULL }
 };

src/videofilters/msqogl.h
@@ -37,6 +37,7 @@ struct _FilterData {
 	OpenGlFunctions functions;
 	
 	struct opengles_display *display;
+	OglDisplayMode mode;
 	
 	MSVideoSize video_size; // Not used at this moment.
 	

BelledonneCommunications/linphone-sdk

mediastreamer2/src/utils/opengles_display.c
@@ -425,7 +425,8 @@ static void ogl_display_render_type(
 	float vpy,
 	float vpw,
 	float vph,
-	int orientation
+	int orientation,
+	MSVideoDisplayMode mode
 ) {
 	const OpenGlFunctions *f = gldisp->functions;
 
@@ -436,6 +437,7 @@ static void ogl_display_render_type(
 	GLfloat x,y,w,h;
 	GLfloat mat[16];
 	float rad;
+	MSVideoDisplayMode usedMode;
 
 	if (!gldisp) {
 		ms_error("[ogl_display] %s called with null struct opengles_display", __FUNCTION__);
@@ -478,22 +480,46 @@ static void ogl_display_render_type(
 		screenH = (GLfloat)gldisp->backingWidth;
 	}
 
+	usedMode = mode;
+	if (mode == MSVideoDisplayHybrid) {
+		// If the image has the same orientation as the screen then use OccupyAllSpace mode
+		if ((screenW > screenH && gldisp->yuv_size[type].width > gldisp->yuv_size[type].height)
+			|| (screenH > screenW && gldisp->yuv_size[type].height > gldisp->yuv_size[type].width)
+		) {
+			usedMode = MSVideoDisplayOccupyAllSpace;
+		} else {
+			usedMode = MSVideoDisplayBlackBars;
+		}
+	}
+
 	// Fill the smallest dimension, then compute the other one using the image ratio
-	if (screenW <= screenH) {
-		float ratio = gldisp->yuv_size[type].height / (float)(gldisp->yuv_size[type].width);
-		w = screenW * vpw;
-		h = w * ratio;
-		if (h > screenH) {
-			w *= screenH / (float)h;
-			h = screenH;
+	if (usedMode == MSVideoDisplayBlackBars) {
+		if (screenW <= screenH) {
+			float ratio = gldisp->yuv_size[type].height / (float)(gldisp->yuv_size[type].width);
+			w = screenW * vpw;
+			h = w * ratio;
+			if (h > screenH) {
+				w *= screenH / (float)h;
+				h = screenH;
+			}
+		} else {
+			float ratio = gldisp->yuv_size[type].width / (float)(gldisp->yuv_size[type].height);
+			h = screenH * vph;
+			w = h * ratio;
+			if (w > screenW) {
+				h *= screenW / (float)w;
+				w = screenW;
+			}
 		}
 	} else {
-		float ratio = gldisp->yuv_size[type].width / (float)(gldisp->yuv_size[type].height);
-		h = screenH * vph;
-		w = h * ratio;
-		if (w > screenW) {
-			h *= screenW / (float)w;
-			w = screenW;
+		if (screenW > screenH) {
+			float ratio = gldisp->yuv_size[type].height / (float)(gldisp->yuv_size[type].width);
+			w = screenW * vpw;
+			h = w * ratio;
+		} else {
+			float ratio = gldisp->yuv_size[type].width / (float)(gldisp->yuv_size[type].height);
+			h = screenH * vph;
+			w = h * ratio;
 		}
 	}
 
@@ -1284,7 +1310,7 @@ void ogl_display_set_preview_yuv_to_display (struct opengles_display *gldisp, mb
 	ogl_display_set_yuv(gldisp, yuv, PREVIEW_IMAGE);
 }
 
-void ogl_display_render (struct opengles_display *gldisp, int orientation) {
+void ogl_display_render (struct opengles_display *gldisp, int orientation, MSVideoDisplayMode mode) {
 	const OpenGlFunctions *f = gldisp->functions;
 	bool_t render = TRUE;
 
@@ -1310,9 +1336,9 @@ void ogl_display_render (struct opengles_display *gldisp, int orientation) {
 		GL_OPERATION(f, glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 		GL_OPERATION(f, glUseProgram(gldisp->program))
 		check_GL_errors(f, "ogl_display_render");
-		ogl_display_render_type(gldisp, REMOTE_IMAGE, TRUE, 0, 0, 1, 1, orientation);
+		ogl_display_render_type(gldisp, REMOTE_IMAGE, TRUE, 0, 0, 1, 1, orientation, mode);
 		// preview image already have the correct orientation
-		ogl_display_render_type(gldisp, PREVIEW_IMAGE, FALSE, 0.4f, -0.4f, 0.2f, 0.2f, 0);
+		ogl_display_render_type(gldisp, PREVIEW_IMAGE, FALSE, 0.4f, -0.4f, 0.2f, 0.2f, 0, MSVideoDisplayBlackBars);
 		gldisp->texture_index = (gldisp->texture_index + 1) % TEXTURE_BUFFER_SIZE;
 		if(f->eglInitialized && gldisp->mRenderSurface != EGL_NO_SURFACE)
 			f->eglSwapBuffers(gldisp->mEglDisplay, gldisp->mRenderSurface);
@@ -1347,7 +1373,7 @@ JNIEXPORT void JNICALL Java_org_linphone_mediastream_video_display_OpenGLESDispl
 
 JNIEXPORT void JNICALL Java_org_linphone_mediastream_video_display_OpenGLESDisplay_render (JNIEnv * env, jobject obj, jlong ptr) {
 	struct opengles_display* d = (struct opengles_display*) ptr;
-	ogl_display_render(d, 0);
+	ogl_display_render(d, 0, MSVideoDisplayBlackBars);
 }
 #endif
 

mediastreamer2/src/videofilters/generic_opengl_display.c
@@ -64,6 +64,7 @@ struct _FilterData {
 	bool_t update_context;
 
 	mblk_t * prev_inm;
+	MSVideoDisplayMode mode;
 };
 
 typedef struct _FilterData FilterData;
@@ -89,6 +90,7 @@ static void ogl_init (MSFilter *f) {
 	data->context_info.width=MS_VIDEO_SIZE_CIF_W;
 	data->context_info.height=MS_VIDEO_SIZE_CIF_H;
 	data->context_info.window = NULL;
+	data->mode = MSVideoDisplayBlackBars;
 	f->data = data;
 	if(!gMutexInitialized){
 		gMutexInitialized = TRUE;
@@ -275,6 +277,14 @@ static int ogl_zoom (MSFilter *f, void *arg) {
 	return 0;
 }
 
+static int ogl_set_mode (MSFilter *f, void *arg) {
+	ms_filter_lock(f);
+	((FilterData *)f->data)->mode = *((MSVideoDisplayMode*)arg);
+	ms_filter_unlock(f);
+
+	return 0;
+}
+
 static int ogl_enable_mirroring (MSFilter *f, void *arg) {
 	FilterData *data = (FilterData *)f->data;
 	ms_filter_lock(f);
@@ -311,7 +321,7 @@ static int ogl_call_render (MSFilter *f, void *arg) {
 		data->update_context = UPDATE_CONTEXT_NOTHING;
 	}
 	if (data->show_video && context_info && ( context_info->window || (!context_info->window && context_info->width && context_info->height)) ){
-		ogl_display_render(data->display, 0);
+		ogl_display_render(data->display, 0, data->mode);
 	}
 	ms_mutex_unlock(&gLock);
 	if( f != NULL)
@@ -343,6 +353,7 @@ static MSFilterMethod methods[] = {
 	{ MS_VIDEO_DISPLAY_SHOW_VIDEO, ogl_show_video },
 	{ MS_VIDEO_DISPLAY_ZOOM, ogl_zoom },
 	{ MS_VIDEO_DISPLAY_ENABLE_MIRRORING, ogl_enable_mirroring },
+	{ MS_VIDEO_DISPLAY_SET_MODE, ogl_set_mode },
 	{ MS_OGL_RENDER, ogl_call_render },
 	{ 0, NULL }
 };

mediastreamer2/src/videofilters/glxvideo.c
@@ -74,6 +74,7 @@ typedef struct GLXVideo
 	bool_t ready;
 	bool_t mirror;
 	bool_t autofit;
+	MSVideoDisplayMode mode;
 } GLXVideo;
 
 
@@ -109,6 +110,7 @@ static void glxvideo_init(MSFilter	*f){
 	obj->wsize=def_size; /* the size of the window*/
 	obj->show=TRUE;
 	obj->autofit=TRUE;
+	obj->mode = MSVideoDisplayBlackBars;
 	f->data=obj;
 
 	XSetErrorHandler(x11error_handler);
@@ -279,7 +281,7 @@ static void glxvideo_process(MSFilter *f){
 	}
 
 	const int orientation=0;
-	ogl_display_render(obj->glhelper, orientation);
+	ogl_display_render(obj->glhelper, orientation, obj->mode);
 	glXSwapBuffers ( obj->display, obj->subwindow );
 
 	end:
@@ -492,6 +494,12 @@ static int glxvideo_set_local_view_mode(MSFilter *f, void *arg){
 	return 0;
 }
 
+static int glxvideo_set_mode(MSFilter *f, void *arg){
+	GLXVideo *s=(GLXVideo*)f->data;
+	s->mode=*((MSVideoDisplayMode*)arg);
+	return 0;
+}
+
 static MSFilterMethod methods[]={
 	{	MS_FILTER_SET_VIDEO_SIZE		, glxvideo_set_vsize },
 	{	MS_VIDEO_DISPLAY_GET_NATIVE_WINDOW_ID	, glxvideo_get_native_window_id },
@@ -501,6 +509,7 @@ static MSFilterMethod methods[]={
 	{	MS_VIDEO_DISPLAY_ENABLE_MIRRORING	, glxvideo_enable_mirroring},
 	{	MS_VIDEO_DISPLAY_ENABLE_AUTOFIT		, glxvideo_enable_autofit	},
 	{	MS_VIDEO_DISPLAY_SET_LOCAL_VIEW_MODE	, glxvideo_set_local_view_mode},
+	{	MS_VIDEO_DISPLAY_SET_MODE			, glxvideo_set_mode },
 	{	0	,NULL}
 };
 

mediastreamer2/src/videofilters/msqogl.cpp
@@ -103,6 +103,7 @@ static void qogl_init (MSFilter *f) {
 	data->parent = f;
 	data->is_qt_linked = FALSE;
 	data->is_sdk_linked = TRUE;
+	data->mode = MSVideoDisplayBlackBars;
 	memset(&data->functions, 0, sizeof(data->functions));
 	data->functions.getProcAddress = getProcAddress;
 	
@@ -233,6 +234,13 @@ static int qogl_enable_mirroring (MSFilter *f, void *arg) {
 	return 0;
 }
 
+static int qogl_set_mode (MSFilter *f, void *arg) {
+	ms_filter_lock(f);
+	((FilterData *)f->data)->mode = *((MSVideoDisplayMode*)arg);
+	ms_filter_unlock(f);
+	return 0;
+}
+
 static int qogl_call_render (MSFilter *f, void *arg) {
 	
 	FilterData *data;
@@ -245,7 +253,7 @@ static int qogl_call_render (MSFilter *f, void *arg) {
 			ogl_display_init(data->display, &data->functions, data->renderer->mWidth , data->renderer->mHeight);
 			data->update_context = FALSE;
 		}
-		ogl_display_render(data->display, 0);
+		ogl_display_render(data->display, 0, data->mode);
 	}
 	
 	ms_filter_unlock(f);
@@ -264,6 +272,7 @@ static MSFilterMethod methods[] = {
 	{ MS_VIDEO_DISPLAY_SHOW_VIDEO, qogl_show_video },
 	{ MS_VIDEO_DISPLAY_ZOOM, qogl_zoom },
 	{ MS_VIDEO_DISPLAY_ENABLE_MIRRORING, qogl_enable_mirroring },
+	{ MS_VIDEO_DISPLAY_SET_MODE, qogl_set_mode },
 	//{ MS_OGL_RENDER, qogl_call_render }, // qogl_call_render is autocalled by Qt, there is no need to put it in interface
 	{ 0, NULL }
 };

mediastreamer2/src/videofilters/msqogl.h
@@ -37,6 +37,7 @@ struct _FilterData {
 	OpenGlFunctions functions;
 	
 	struct opengles_display *display;
+	MSVideoDisplayMode mode;
 	
 	MSVideoSize video_size; // Not used at this moment.
 	