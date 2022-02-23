7307d0bbb6325d0a1b8da1afaa0c5490a5ffe896
Jehan Monnier <jehan.monnier@linphone.org>
2022-02-23 16:25:26 +0100

    fix macOS swift wrapper

--- cmake/macos/linphone-sdk.podspec.cmake
+++ cmake/macos/linphone-sdk.podspec.cmake
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 
-  s.name         = "linphone-sdk"
+  s.name         = "@LINPHONESDK_NAME@"
   s.version      = "@LINPHONESDK_VERSION@"
   s.summary      = "This is the linphone SDK, a free (GPL) video voip library based on the SIP protocol"
   s.description  = <<-PODSPEC_DESC
@@ -11,12 +11,10 @@ Pod::Spec.new do |s|
     }
   s.author       = { 'Belledonne Communications SARL' => 'linphone-desktop@belledonne-communications.com' }
   s.platform     = :osx, "10.9"
-  s.source       = { :http => "@LINPHONESDK_MACOS_BASE_URL@/linphone-sdk-macos-@LINPHONESDK_VERSION@.zip" }
-  s.vendored_frameworks = "linphone-sdk/@INSTALL_FOLDER@/Frameworks/**"
+  s.source       = { :http => "@LINPHONESDK_MACOS_BASE_URL@/@LINPHONESDK_NAME@-macos-@LINPHONESDK_VERSION@.zip" }
+  s.vendored_frameworks = "@LINPHONESDK_NAME@/@INSTALL_FOLDER@/Frameworks/**"
   s.pod_target_xcconfig = { 'VALID_ARCHS' => "@VALID_ARCHS@" }
-
-  s.subspec 'all-frameworks' do |sp|
-    sp.vendored_frameworks = "linphone-sdk/@INSTALL_FOLDER@/Frameworks/**"
-  end
- 
+  s.source_files = "@LINPHONESDK_NAME@/@INSTALL_FOLDER@/share/linphonesw/*.swift"
+  s.module_name   = 'linphonesw' # name of the swift package
+  s.swift_version = '4.0'
 end
