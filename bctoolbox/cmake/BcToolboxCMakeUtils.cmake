8ea5ec324770a526e70bd598205f1c73518b74fb
Mickaël Turnel <mickael.turnel@belledonne-communications.com>
2022-04-15 10:47:06 +0200

    Fix wrong read variable resulting by always being in "snapshots" state

--- bctoolbox/cmake/BcToolboxCMakeUtils.cmake
+++ bctoolbox/cmake/BcToolboxCMakeUtils.cmake
@@ -203,7 +203,7 @@ function(bc_compute_snapshots_or_releases_state OUTPUT_VERSION)
 
 		# Check git describe to see if we are on a release or not
 		set(snapshots_or_releases_state "snapshots")
-		if(NOT GIT_DESC MATCHES ".*(alpha|beta).*")
+		if(NOT GIT_DESCRIBE_VERSION MATCHES ".*(alpha|beta).*")
 			set(snapshots_or_release_state "releases")
 		endif()
 
