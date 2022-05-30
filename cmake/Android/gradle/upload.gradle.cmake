6934f6c784cab962930c53ecc3ad0de2a48c9666
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2022-05-30 10:07:24 +0200

    Added Android SDK no-gpl nightly build

--- cmake/Android/gradle/upload.gradle.cmake
+++ cmake/Android/gradle/upload.gradle.cmake
@@ -37,6 +37,9 @@ if (project.hasProperty("no-video")) {
 if (project.hasProperty("minimal-size")) {
     artefactGroupId = artefactGroupId + '.minimal'
 }
+if (project.hasProperty("no-gpl")) {
+    artefactGroupId = artefactGroupId + '.no-gpl'
+}
 println("AAR artefact group is: " + artefactGroupId + ", SDK version @LINPHONESDK_VERSION@")
 
 publishing {
