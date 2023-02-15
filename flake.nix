86d679ebf3fb1556959f2fa07cd86e426a76b560
Thibault Lemaire <thibault.lemaire@belledonne-communications.com>
2023-02-15 15:17:09 +0100

    Add headleass mediastreamer nix build
    Add ccached nix builds
    
    Add headless dev shell

--- flake.nix
+++ flake.nix
@@ -75,7 +75,7 @@
     , ...
     }:
     let
-      version = "sdk-${self.shortRev}";
+      version = "sdk-${if builtins.hasAttr "shortRev" self then self.shortRev else "dev"}";
     in
     {
 
@@ -97,6 +97,20 @@
             libpulseaudio
             glew
             nixpkgs-fmt
+          ];
+        };
+
+      # CC=gcc CXX=g++ BUILD_DIR_NAME="build-headless" cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -S . -B ./$BUILD_DIR_NAME -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Sanitizer -DCMAKE_INSTALL_PREFIX="$PWD/$BUILD_DIR_NAME/install" -DENABLE_UNIT_TESTS=ON -DENABLE_STRICT=OFF -DENABLE_V4L=0
+      devShells.x86_64-linux.headless = with nixpkgs.legacyPackages.x86_64-linux;
+        mkShell {
+          buildInputs = [
+            cmake
+            clang_13
+            (python3.withPackages (ps: with ps; [ pystache six ]))
+            pkg-config
+            doxygen
+            yasm
+            libpulseaudio # deleteme
           ];
         };
 
@@ -132,6 +146,9 @@
         bzrtp = prev.bzrtp.overrideAttrs (attrs: {
           inherit version;
           src = bzrtp-src;
+          cmakeFlags = attrs.cmakeFlags ++ [
+            "-DCMAKE_C_FLAGS=-Wno-error=unused-parameter"
+          ];
         });
         lime = prev.lime.overrideAttrs (attrs: {
           inherit version;
@@ -157,13 +174,132 @@
           inherit version;
           src = ortp-src;
         });
+
+        mediastreamer-headless = final.mediastreamer.overrideAttrs (attrs:
+          let
+            notIn = list: element: ! builtins.elem element list;
+            nameDoesNotStartWith = str: package: with builtins; substring 0 (stringLength str) package.pname != str;
+          in
+          {
+            propagatedBuildInputs = builtins.filter
+              (with final; notIn [
+                libpulseaudio
+                glew
+                xorg.libX11
+                xorg.libXext
+                libv4l
+              ])
+              attrs.propagatedBuildInputs;
+
+            nativeBuildInputs = builtins.filter (nameDoesNotStartWith "qt") attrs.nativeBuildInputs;
+
+            cmakeFlags = attrs.cmakeFlags ++ [
+              "-DENABLE_SOUND=NO"
+              "-DENABLE_QT_GL=OFF"
+              "-DENABLE_V4L=0"
+            ];
+            NIX_LDFLAGS = null;
+          });
+
+        # WIP/Broken
+        linphone-sdk =
+          let
+            combined-src = prev.runCommand "linphone-sdk-src" { }
+              ''
+                mkdir $out
+                ln -s ${./.}/* $out
+                ln -s ${bcunit-src} $out/bcunit
+                ln -s ${bctoolbox-src} $out/bctoolbox
+              '';
+          in
+          {
+            inherit combined-src;
+            all-in-one = {
+              sanitizer =
+                prev.stdenv.mkDerivation {
+                  name = "linphone-sdk-all-in-one";
+                  src = combined-src;
+                  cmakeFlags = [
+                    "-DCMAKE_BUILD_TYPE=Sanitizer"
+                    "-DENABLE_UNIT_TESTS=ON"
+                    "-DENABLE_STRICT=OFF"
+                    "-DENABLE_SANITIZER=ON"
+                  ];
+                  nativeBuildInputs = with prev; [
+                    cmake
+                  ];
+                };
+            };
+          };
       };
+
+      overlays.ccached = final: prev:
+        let
+          withCCache = packages: with builtins; listToAttrs (map
+            (name: {
+              inherit name; value = (getAttr name prev).override { stdenv = prev.ccacheStdenv; };
+            })
+            packages);
+        in
+        {
+          ccacheWrapper = prev.ccacheWrapper.override {
+            extraConfig = ''
+              export CCACHE_COMPRESS=1
+              export CCACHE_DIR="/var/cache/ccache"
+              export CCACHE_UMASK=007
+              if [ ! -d "$CCACHE_DIR" ]; then
+                echo "====="
+                echo "Directory '$CCACHE_DIR' does not exist"
+                echo "Please create it with:"
+                echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+                echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+                echo "====="
+                exit 1
+              fi
+              if [ ! -w "$CCACHE_DIR" ]; then
+                echo "====="
+                echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+                echo "Please verify its access permissions"
+                echo "====="
+                exit 1
+              fi
+            '';
+          };
+        } // withCCache [
+          "bctoolbox"
+          "bcunit"
+          "bc-decaf"
+          "bc-soci"
+          "belcard"
+          "belle-sip"
+          "belr"
+          "bzrtp"
+          "lime"
+          "liblinphone"
+          "mediastreamer"
+          "mediastreamer-openh264"
+          "ortp"
+
+          "mediastreamer-headless"
+        ];
 
 
       packages.x86_64-linux = import nixpkgs {
         system = "x86_64-linux";
         overlays = [
           self.overlays.default
+        ];
+      };
+
+      # ⚠️ Requires ccache configured on the system
+      # https://nixos.wiki/wiki/CCache
+      # Build packages with ccache like:
+      # nix build .#ccachedPackages.x86_64-linux.liblinphone
+      ccachedPackages.x86_64-linux = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [
+          self.overlays.default
+          self.overlays.ccached
         ];
       };
 
