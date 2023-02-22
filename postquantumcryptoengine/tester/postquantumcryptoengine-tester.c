BelledonneCommunications/postquantumcryptoengine

Enable compiler option '-Wunused-parameter'

tester/postquantumcryptoengine-tester.c
@@ -18,6 +18,9 @@
  */
 
 #include <stdio.h>
+
+#include <bctoolbox/defs.h>
+
 #include "postquantumcryptoengine-tester.h"
 
 static const char *log_domain = "bctoobox-pq-tester";
@@ -57,13 +60,13 @@ end:
 	return res;
 }
 
-int silent_arg_func(const char *arg) {
+int silent_arg_func(UNUSED(const char *arg)) {
 	bctbx_set_log_level(log_domain, BCTBX_LOG_FATAL);
 	bctbx_set_log_level(BCTBX_LOG_DOMAIN, BCTBX_LOG_FATAL);
 	return 0;
 }
 
-int verbose_arg_func(const char *arg) {
+int verbose_arg_func(UNUSED(const char *arg)) {
 	bctbx_set_log_level(log_domain, BCTBX_LOG_DEBUG);
 	bctbx_set_log_level(BCTBX_LOG_DOMAIN,BCTBX_LOG_DEBUG);
 	return 0;

BelledonneCommunications/linphone-sdk

postquantumcryptoengine/tester/postquantumcryptoengine-tester.c
@@ -18,6 +18,9 @@
  */
 
 #include <stdio.h>
+
+#include <bctoolbox/defs.h>
+
 #include "postquantumcryptoengine-tester.h"
 
 static const char *log_domain = "bctoobox-pq-tester";
@@ -57,13 +60,13 @@ end:
 	return res;
 }
 
-int silent_arg_func(const char *arg) {
+int silent_arg_func(BCTBX_UNUSED(const char *arg)) {
 	bctbx_set_log_level(log_domain, BCTBX_LOG_FATAL);
 	bctbx_set_log_level(BCTBX_LOG_DOMAIN, BCTBX_LOG_FATAL);
 	return 0;
 }
 
-int verbose_arg_func(const char *arg) {
+int verbose_arg_func(BCTBX_UNUSED(const char *arg)) {
 	bctbx_set_log_level(log_domain, BCTBX_LOG_DEBUG);
 	bctbx_set_log_level(BCTBX_LOG_DOMAIN,BCTBX_LOG_DEBUG);
 	return 0;