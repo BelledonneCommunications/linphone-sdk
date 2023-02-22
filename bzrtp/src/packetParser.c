BelledonneCommunications/bzrtp

Enable compiler option '-Wunused-parameter'

src/packetParser.c
@@ -22,6 +22,7 @@
 #include <string.h>
 #include "typedef.h"
 #include "packetParser.h"
+#include <bctoolbox/defs.h>
 #include <bctoolbox/crypto.h>
 #include "cryptoUtils.h"
 
@@ -293,7 +294,7 @@ bzrtpPacket_t *bzrtp_packetCheck(uint8_t **inputPtr, uint16_t *inputLength, bzrt
 
 
 /* Call this function after the packetCheck one, to actually parse the packet : create and fill the messageData structure */
-int bzrtp_packetParser(bzrtpContext_t *zrtpContext, bzrtpChannelContext_t *zrtpChannelContext, const uint8_t * input, uint16_t inputLength, bzrtpPacket_t *zrtpPacket) {
+int bzrtp_packetParser(UNUSED(bzrtpContext_t *zrtpContext), bzrtpChannelContext_t *zrtpChannelContext, const uint8_t * input, uint16_t inputLength, bzrtpPacket_t *zrtpPacket) {
 
 	int i;
 

src/stateMachine.c
@@ -24,6 +24,7 @@
 #include "cryptoUtils.h"
 #include "zidCache.h"
 #include <bctoolbox/crypto.h>
+#include <bctoolbox/defs.h>
 #include "stateMachine.h"
 
 
@@ -2584,7 +2585,7 @@ static int bzrtp_computeS0MultiStreamMode(bzrtpContext_t *zrtpContext, bzrtpChan
  * return 0 on success, error code otherwise
  *
  */
-static int bzrtp_deriveKeysFromS0(bzrtpContext_t *zrtpContext, bzrtpChannelContext_t *zrtpChannelContext) {
+static int bzrtp_deriveKeysFromS0(UNUSED(bzrtpContext_t *zrtpContext), bzrtpChannelContext_t *zrtpChannelContext) {
 	int retval = 0;
 	/* allocate memory for mackeyi, mackeyr, zrtpkeyi, zrtpkeyr */
 	zrtpChannelContext->mackeyi = (uint8_t *)malloc(zrtpChannelContext->hashLength*(sizeof(uint8_t)));

src/zidCache.c
@@ -20,6 +20,7 @@
 #include <string.h>
 #include "typedef.h"
 #include <bctoolbox/crypto.h>
+#include <bctoolbox/defs.h>
 #include "cryptoUtils.h"
 #include "zidCache.h"
 
@@ -44,7 +45,7 @@
  */
 #define ZIDCACHE_DBSCHEMA_VERSION_NUMBER 0x000002
 
-static int callback_getSelfZID(void *data, int argc, char **argv, char **colName){
+static int callback_getSelfZID(void *data, UNUSED(int argc), char **argv, UNUSED(char **colName)){
 	uint8_t **selfZID = (uint8_t **)data;
 
 	/* we selected zid only, it must then be in argv[0] */
@@ -56,7 +57,7 @@ static int callback_getSelfZID(void *data, int argc, char **argv, char **colName
 	return 0;
 }
 
-static int callback_getUserVersion(void *data, int argc, char **argv, char **colName){
+static int callback_getUserVersion(void *data, UNUSED(int argc), char **argv, UNUSED(char **colName)){
 	int *userVersion = (int *)data;
 
 	if (argv[0]) {

test/bzrtpConfigsTest.c
@@ -20,6 +20,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include <bctoolbox/defs.h>
+
 #include "bzrtp/bzrtp.h"
 #include "zidCache.h"
 #include "bzrtpTest.h"
@@ -198,7 +200,7 @@ int getSAS(void *clientData, bzrtpSrtpSecrets_t *secrets, int32_t pvs) {
 	return 0;
 }
 
-int getMessage(void *clientData, const uint8_t level, const uint8_t message, const char *messageString) {
+int getMessage(void *clientData, const uint8_t level, const uint8_t message, UNUSED(const char *messageString)) {
 	/* get the client context */
 	clientContext_t *clientContext = (clientContext_t *)clientData;
 	if (level == BZRTP_MESSAGE_ERROR && message == BZRTP_MESSAGE_CACHEMISMATCH) {
@@ -213,7 +215,7 @@ int getMessage(void *clientData, const uint8_t level, const uint8_t message, con
 	return 0;
 }
 
-int computeExportedKeys(void *clientData, int zuid, uint8_t role) {
+int computeExportedKeys(void *clientData, UNUSED(int zuid), uint8_t role) {
 	size_t keyLength = 16;
 	/* get the client context */
 	clientContext_t *clientContext = (clientContext_t *)clientData;

test/bzrtpTest.c
@@ -17,6 +17,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <bctoolbox/defs.h>
+
 #include <stdio.h>
 #include "bzrtpTest.h"
 #include "typedef.h"
@@ -59,14 +61,14 @@ end:
 	return res;
 }
 
-int silent_arg_func(const char *arg) {
+int silent_arg_func(UNUSED(const char *arg)) {
 	bctbx_set_log_level(log_domain, BCTBX_LOG_FATAL);
 	bctbx_set_log_level(BCTBX_LOG_DOMAIN, BCTBX_LOG_FATAL);
 	verbose = 0;
 	return 0;
 }
 
-int verbose_arg_func(const char *arg) {
+int verbose_arg_func(UNUSED(const char *arg)) {
 	bctbx_set_log_level(log_domain, BCTBX_LOG_DEBUG);
 	bctbx_set_log_level(BCTBX_LOG_DOMAIN,BCTBX_LOG_DEBUG);
 	verbose = 1;

BelledonneCommunications/linphone-sdk

bzrtp/src/packetParser.c
@@ -22,6 +22,7 @@
 #include <string.h>
 #include "typedef.h"
 #include "packetParser.h"
+#include <bctoolbox/defs.h>
 #include <bctoolbox/crypto.h>
 #include "cryptoUtils.h"
 
@@ -293,7 +294,7 @@ bzrtpPacket_t *bzrtp_packetCheck(uint8_t **inputPtr, uint16_t *inputLength, bzrt
 
 
 /* Call this function after the packetCheck one, to actually parse the packet : create and fill the messageData structure */
-int bzrtp_packetParser(bzrtpContext_t *zrtpContext, bzrtpChannelContext_t *zrtpChannelContext, const uint8_t * input, uint16_t inputLength, bzrtpPacket_t *zrtpPacket) {
+int bzrtp_packetParser(BCTBX_UNUSED(bzrtpContext_t *zrtpContext), bzrtpChannelContext_t *zrtpChannelContext, const uint8_t * input, uint16_t inputLength, bzrtpPacket_t *zrtpPacket) {
 
 	int i;
 

bzrtp/src/stateMachine.c
@@ -24,6 +24,7 @@
 #include "cryptoUtils.h"
 #include "zidCache.h"
 #include <bctoolbox/crypto.h>
+#include <bctoolbox/defs.h>
 #include "stateMachine.h"
 
 
@@ -2584,7 +2585,7 @@ static int bzrtp_computeS0MultiStreamMode(bzrtpContext_t *zrtpContext, bzrtpChan
  * return 0 on success, error code otherwise
  *
  */
-static int bzrtp_deriveKeysFromS0(bzrtpContext_t *zrtpContext, bzrtpChannelContext_t *zrtpChannelContext) {
+static int bzrtp_deriveKeysFromS0(BCTBX_UNUSED(bzrtpContext_t *zrtpContext), bzrtpChannelContext_t *zrtpChannelContext) {
 	int retval = 0;
 	/* allocate memory for mackeyi, mackeyr, zrtpkeyi, zrtpkeyr */
 	zrtpChannelContext->mackeyi = (uint8_t *)malloc(zrtpChannelContext->hashLength*(sizeof(uint8_t)));

bzrtp/src/zidCache.c
@@ -20,6 +20,7 @@
 #include <string.h>
 #include "typedef.h"
 #include <bctoolbox/crypto.h>
+#include <bctoolbox/defs.h>
 #include "cryptoUtils.h"
 #include "zidCache.h"
 
@@ -44,7 +45,7 @@
  */
 #define ZIDCACHE_DBSCHEMA_VERSION_NUMBER 0x000002
 
-static int callback_getSelfZID(void *data, int argc, char **argv, char **colName){
+static int callback_getSelfZID(void *data, BCTBX_UNUSED(int argc), char **argv, BCTBX_UNUSED(char **colName)){
 	uint8_t **selfZID = (uint8_t **)data;
 
 	/* we selected zid only, it must then be in argv[0] */
@@ -56,7 +57,7 @@ static int callback_getSelfZID(void *data, int argc, char **argv, char **colName
 	return 0;
 }
 
-static int callback_getUserVersion(void *data, int argc, char **argv, char **colName){
+static int callback_getUserVersion(void *data, BCTBX_UNUSED(int argc), char **argv, BCTBX_UNUSED(char **colName)){
 	int *userVersion = (int *)data;
 
 	if (argv[0]) {

bzrtp/test/bzrtpConfigsTest.c
@@ -20,6 +20,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include <bctoolbox/defs.h>
+
 #include "bzrtp/bzrtp.h"
 #include "zidCache.h"
 #include "bzrtpTest.h"
@@ -198,7 +200,7 @@ int getSAS(void *clientData, bzrtpSrtpSecrets_t *secrets, int32_t pvs) {
 	return 0;
 }
 
-int getMessage(void *clientData, const uint8_t level, const uint8_t message, const char *messageString) {
+int getMessage(void *clientData, const uint8_t level, const uint8_t message, BCTBX_UNUSED(const char *messageString)) {
 	/* get the client context */
 	clientContext_t *clientContext = (clientContext_t *)clientData;
 	if (level == BZRTP_MESSAGE_ERROR && message == BZRTP_MESSAGE_CACHEMISMATCH) {
@@ -213,7 +215,7 @@ int getMessage(void *clientData, const uint8_t level, const uint8_t message, con
 	return 0;
 }
 
-int computeExportedKeys(void *clientData, int zuid, uint8_t role) {
+int computeExportedKeys(void *clientData, BCTBX_UNUSED(int zuid), uint8_t role) {
 	size_t keyLength = 16;
 	/* get the client context */
 	clientContext_t *clientContext = (clientContext_t *)clientData;

bzrtp/test/bzrtpTest.c
@@ -17,6 +17,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <bctoolbox/defs.h>
+
 #include <stdio.h>
 #include "bzrtpTest.h"
 #include "typedef.h"
@@ -59,14 +61,14 @@ end:
 	return res;
 }
 
-int silent_arg_func(const char *arg) {
+int silent_arg_func(BCTBX_UNUSED(const char *arg)) {
 	bctbx_set_log_level(log_domain, BCTBX_LOG_FATAL);
 	bctbx_set_log_level(BCTBX_LOG_DOMAIN, BCTBX_LOG_FATAL);
 	verbose = 0;
 	return 0;
 }
 
-int verbose_arg_func(const char *arg) {
+int verbose_arg_func(BCTBX_UNUSED(const char *arg)) {
 	bctbx_set_log_level(log_domain, BCTBX_LOG_DEBUG);
 	bctbx_set_log_level(BCTBX_LOG_DOMAIN,BCTBX_LOG_DEBUG);
 	verbose = 1;