3f22c35a861d0da1c1dfbd5c4fecfb624e89d523
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2023-03-13 14:21:31 +0100

    Update CMakeLists.txt so that the project can be added as a subdirectory of linphone-sdk.
    Rename CMake targets for uniform naming.

--- msopenh264/cmake/FindOpenH264.cmake
+++ msopenh264/cmake/FindOpenH264.cmake
@@ -1,6 +1,6 @@
 ############################################################################
-# FindOpenH264.txt
-# Copyright (C) 2014  Belledonne Communications, Grenoble France
+# FindOpenH264.cmake
+# Copyright (C) 2014-2023  Belledonne Communications, Grenoble France
 #
 ############################################################################
 #
@@ -26,48 +26,59 @@
 #  OPENH264_INCLUDE_DIRS - the openh264 include directory
 #  OPENH264_LIBRARIES - The libraries needed to use openh264
 
-include(CMakePushCheckState)
-include(CheckCXXSymbolExists)
+if(TARGET openh264)
 
-set(_OPENH264_ROOT_PATHS
-	${WITH_OPENH264}
-	${CMAKE_INSTALL_PREFIX}
-)
-
-find_path(OPENH264_INCLUDE_DIRS
-	NAMES wels/codec_api.h
-	HINTS _OPENH264_ROOT_PATHS
-	PATH_SUFFIXES include
-)
-if(OPENH264_INCLUDE_DIRS)
+	set(OPENH264_LIBRARIES libopenh264)
+	get_target_property(OPENH264_INCLUDE_DIRS libopenh264 INTERFACE_INCLUDE_DIRECTORIES)
 	set(HAVE_WELS_CODEC_API_H 1)
-endif()
+	set(HAVE_WELS_CREATE_DECODER TRUE)
 
-find_library(OPENH264_LIBRARIES
-	NAMES openh264_dll openh264
-	HINTS _OPENH264_ROOT_PATHS
-	PATH_SUFFIXES bin lib
-)
+else()
 
-if(OPENH264_LIBRARIES)
-	if( WIN32 OR ANDROID )#issue with check_cxx_symbol_exists on Windows (TODO: missing libraries in OPENH264_LIBRARIES?). If OPENH264_INCLUDE_DIRS, then decoder exists.
-		set(OPENH264_FOUND TRUE)
-		set(HAVE_WELS_CREATE_DECODER TRUE)
-	else()
-		# need pthread lib for check_cxx_symbol_exists
-		find_library(PTHREAD_LIBRARY
-			NAMES pthread
-		)
-		list(APPEND OPENH264_LIBRARIES ${PTHREAD_LIBRARY})
-		cmake_push_check_state(RESET)
-		list(APPEND CMAKE_REQUIRED_INCLUDES ${OPENH264_INCLUDE_DIRS})
-		list(APPEND CMAKE_REQUIRED_LIBRARIES ${OPENH264_LIBRARIES})
-		check_cxx_symbol_exists("WelsCreateDecoder" "wels/codec_api.h" HAVE_WELS_CREATE_DECODER)
-		cmake_pop_check_state()
-		if(HAVE_WELS_CREATE_DECODER)
+	include(CMakePushCheckState)
+	include(CheckCXXSymbolExists)
+
+	set(_OPENH264_ROOT_PATHS
+		${WITH_OPENH264}
+		${CMAKE_INSTALL_PREFIX}
+	)
+
+	find_path(OPENH264_INCLUDE_DIRS
+		NAMES wels/codec_api.h
+		HINTS _OPENH264_ROOT_PATHS
+		PATH_SUFFIXES include
+	)
+	if(OPENH264_INCLUDE_DIRS)
+		set(HAVE_WELS_CODEC_API_H 1)
+	endif()
+
+	find_library(OPENH264_LIBRARIES
+		NAMES openh264_dll openh264
+		HINTS _OPENH264_ROOT_PATHS
+		PATH_SUFFIXES bin lib
+	)
+
+	if(OPENH264_LIBRARIES)
+		if( WIN32 OR ANDROID )#issue with check_cxx_symbol_exists on Windows (TODO: missing libraries in OPENH264_LIBRARIES?). If OPENH264_INCLUDE_DIRS, then decoder exists.
 			set(OPENH264_FOUND TRUE)
+			set(HAVE_WELS_CREATE_DECODER TRUE)
+		else()
+			# need pthread lib for check_cxx_symbol_exists
+			find_library(PTHREAD_LIBRARY
+				NAMES pthread
+			)
+			list(APPEND OPENH264_LIBRARIES ${PTHREAD_LIBRARY})
+			cmake_push_check_state(RESET)
+			list(APPEND CMAKE_REQUIRED_INCLUDES ${OPENH264_INCLUDE_DIRS})
+			list(APPEND CMAKE_REQUIRED_LIBRARIES ${OPENH264_LIBRARIES})
+			check_cxx_symbol_exists("WelsCreateDecoder" "wels/codec_api.h" HAVE_WELS_CREATE_DECODER)
+			cmake_pop_check_state()
+			if(HAVE_WELS_CREATE_DECODER)
+				set(OPENH264_FOUND TRUE)
+			endif()
 		endif()
 	endif()
+
 endif()
 
 include(FindPackageHandleStandardArgs)
