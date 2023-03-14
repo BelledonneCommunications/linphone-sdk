BelledonneCommunications/liblinphone

Set send master key only during the first invite or the key changes during a call

coreapi/private_functions.h
@@ -48,6 +48,8 @@ void linphone_call_notify_state_changed(LinphoneCall *call, LinphoneCallState cs
 void linphone_call_notify_dtmf_received(LinphoneCall *call, int dtmf);
 void linphone_call_notify_goclear_ack_sent (LinphoneCall *call);
 void linphone_call_notify_encryption_changed(LinphoneCall *call, bool_t on, const char *authentication_token);
+void linphone_call_notify_send_master_key_changed (LinphoneCall *call, const char *master_key);
+void linphone_call_notify_receive_master_key_changed (LinphoneCall *call, const char *master_key);
 void linphone_call_notify_transfer_state_changed(LinphoneCall *call, LinphoneCallState cstate);
 void linphone_call_notify_stats_updated(LinphoneCall *call, const LinphoneCallStats *stats);
 void linphone_call_notify_info_message_received(LinphoneCall *call, const LinphoneInfoMessage *msg);
@@ -614,6 +616,8 @@ void linphone_core_notify_global_state_changed(LinphoneCore *lc, LinphoneGlobalS
 void linphone_core_notify_call_state_changed(LinphoneCore *lc, LinphoneCall *call, LinphoneCallState cstate, const char *message);
 void linphone_core_notify_call_goclear_ack_sent(LinphoneCore *lc, LinphoneCall *call);
 void linphone_core_notify_call_encryption_changed(LinphoneCore *lc, LinphoneCall *call, bool_t on, const char *authentication_token);
+void linphone_core_notify_call_send_master_key_changed(LinphoneCore *lc, LinphoneCall *call, const char *master_key);
+void linphone_core_notify_call_receive_master_key_changed(LinphoneCore *lc, LinphoneCall *call, const char *master_key);
 void linphone_core_notify_registration_state_changed(LinphoneCore *lc, LinphoneProxyConfig *cfg, LinphoneRegistrationState cstate, const char *message);
 void linphone_core_notify_account_registration_state_changed(LinphoneCore *core, LinphoneAccount *account, LinphoneRegistrationState state, const char *message);
 void linphone_core_notify_new_subscription_requested(LinphoneCore *lc, LinphoneFriend *lf, const char *url);

src/call/call.h
@@ -60,6 +60,8 @@ public:
 	LinphoneCallCbsDtmfReceivedCb dtmfReceivedCb;
 	LinphoneCallCbsGoClearAckSentCb goClearAckSentCb;
 	LinphoneCallCbsEncryptionChangedCb encryptionChangedCb;
+	LinphoneCallCbsSendMasterKeyChangedCb sendMasterKeyChangedCb;
+	LinphoneCallCbsReceiveMasterKeyChangedCb receiveMasterKeyChangedCb;
 	LinphoneCallCbsInfoMessageReceivedCb infoMessageReceivedCb;
 	LinphoneCallCbsStateChangedCb stateChangedCb;
 	LinphoneCallCbsStatsUpdatedCb statsUpdatedCb;
@@ -284,6 +286,8 @@ public:
 	void onInfoReceived (const std::shared_ptr<CallSession> &session, const LinphoneInfoMessage *im) override;
 	void onLossOfMediaDetected (const std::shared_ptr<CallSession> &session) override;
 	void onEncryptionChanged (const std::shared_ptr<CallSession> &session, bool activated, const std::string &authToken) override;
+	void onSendMasterKeyChanged (const std::shared_ptr<CallSession> &session, const std::string &masterKey) override;
+	void onReceiveMasterKeyChanged (const std::shared_ptr<CallSession> &session, const std::string &masterKey) override;
 	void onGoClearAckSent() override;
 	void onCallSessionStateChangedForReporting (const std::shared_ptr<CallSession> &session) override;
 	void onRtcpUpdateForReporting (const std::shared_ptr<CallSession> &session, SalStreamType type) override;

BelledonneCommunications/linphone-sdk

liblinphone/coreapi/private_functions.h
@@ -50,6 +50,8 @@ void linphone_call_notify_state_changed(LinphoneCall *call, LinphoneCallState cs
 void linphone_call_notify_dtmf_received(LinphoneCall *call, int dtmf);
 void linphone_call_notify_goclear_ack_sent(LinphoneCall *call);
 void linphone_call_notify_encryption_changed(LinphoneCall *call, bool_t on, const char *authentication_token);
+void linphone_call_notify_send_master_key_changed(LinphoneCall *call, const char *master_key);
+void linphone_call_notify_receive_master_key_changed(LinphoneCall *call, const char *master_key);
 void linphone_call_notify_transfer_state_changed(LinphoneCall *call, LinphoneCallState cstate);
 void linphone_call_notify_stats_updated(LinphoneCall *call, const LinphoneCallStats *stats);
 void linphone_call_notify_info_message_received(LinphoneCall *call, const LinphoneInfoMessage *msg);
@@ -783,6 +785,8 @@ void linphone_core_notify_call_encryption_changed(LinphoneCore *lc,
                                                   LinphoneCall *call,
                                                   bool_t on,
                                                   const char *authentication_token);
+void linphone_core_notify_call_send_master_key_changed(LinphoneCore *lc, LinphoneCall *call, const char *master_key);
+void linphone_core_notify_call_receive_master_key_changed(LinphoneCore *lc, LinphoneCall *call, const char *master_key);
 void linphone_core_notify_registration_state_changed(LinphoneCore *lc,
                                                      LinphoneProxyConfig *cfg,
                                                      LinphoneRegistrationState cstate,

liblinphone/src/call/call.h
@@ -60,6 +60,8 @@ public:
 	LinphoneCallCbsDtmfReceivedCb dtmfReceivedCb;
 	LinphoneCallCbsGoClearAckSentCb goClearAckSentCb;
 	LinphoneCallCbsEncryptionChangedCb encryptionChangedCb;
+	LinphoneCallCbsSendMasterKeyChangedCb sendMasterKeyChangedCb;
+	LinphoneCallCbsReceiveMasterKeyChangedCb receiveMasterKeyChangedCb;
 	LinphoneCallCbsInfoMessageReceivedCb infoMessageReceivedCb;
 	LinphoneCallCbsStateChangedCb stateChangedCb;
 	LinphoneCallCbsStatsUpdatedCb statsUpdatedCb;
@@ -285,6 +287,8 @@ public:
 	void onEncryptionChanged(const std::shared_ptr<CallSession> &session,
 	                         bool activated,
 	                         const std::string &authToken) override;
+	void onSendMasterKeyChanged(const std::shared_ptr<CallSession> &session, const std::string &masterKey) override;
+	void onReceiveMasterKeyChanged(const std::shared_ptr<CallSession> &session, const std::string &masterKey) override;
 	void onGoClearAckSent() override;
 	void onCallSessionStateChangedForReporting(const std::shared_ptr<CallSession> &session) override;
 	void onRtcpUpdateForReporting(const std::shared_ptr<CallSession> &session, SalStreamType type) override;