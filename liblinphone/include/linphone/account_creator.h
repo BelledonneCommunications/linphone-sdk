BelledonneCommunications/liblinphone

Added missing account creator getters + improved doc

include/linphone/account_creator.h
@@ -201,32 +201,60 @@ LINPHONE_PUBLIC LinphoneAccountCreatorUsernameStatus linphone_account_creator_se
 LINPHONE_PUBLIC const char * linphone_account_creator_get_username(const LinphoneAccountCreator *creator);
 
 /**
- * Set the pn_provider.
+ * Set the provider to be used by the backend to send the push notification to the device asking for an auth token.
  * @param creator #LinphoneAccountCreator object @notnil
- * @param pn_provider The pn_provider to set @notnil
+ * @param pn_provider The pn_provider to set @maybenil
 **/
 LINPHONE_PUBLIC void linphone_account_creator_set_pn_provider(LinphoneAccountCreator *creator, const char* pn_provider);
 
 /**
- * Set the pn_param.
+ * Get the provider to be used by the backend to send the push notification to the device asking for an auth token.
  * @param creator #LinphoneAccountCreator object @notnil
- * @param pn_param The pn_param to set @notnil
+ * @return The pn_provider set, if any @maybenil
+**/
+LINPHONE_PUBLIC const char* linphone_account_creator_get_pn_provider(const LinphoneAccountCreator *creator);
+
+/**
+ * Set the param to be used by the backend to send the push notification to the device asking for an auth token.
+ * @param creator #LinphoneAccountCreator object @notnil
+ * @param pn_param The pn_param to set @maybenil
 **/
 LINPHONE_PUBLIC void linphone_account_creator_set_pn_param(LinphoneAccountCreator *creator, const char* pn_param);
 
 /**
- * Set the pn_prid.
+ * Get the param to be used by the backend to send the push notification to the device asking for an auth token.
  * @param creator #LinphoneAccountCreator object @notnil
- * @param pn_prid The pn_prid to set @notnil
+ * @return The pn_param set, if any @maybenil
+**/
+LINPHONE_PUBLIC const char* linphone_account_creator_get_pn_param(const LinphoneAccountCreator *creator);
+
+/**
+ * Set the prid to be used by the backend to send the push notification to the device asking for an auth token.
+ * @param creator #LinphoneAccountCreator object @notnil
+ * @param pn_prid The pn_prid to set @maybenil
 **/
 LINPHONE_PUBLIC void linphone_account_creator_set_pn_prid(LinphoneAccountCreator *creator, const char* pn_prid);
 
 /**
- * Set the token.
+ * Get the prid to be used by the backend to send the push notification to the device asking for an auth token.
  * @param creator #LinphoneAccountCreator object @notnil
- * @param token The pn_prid to set @notnil
+ * @return The pn_prid set, if any @maybenil
+**/
+LINPHONE_PUBLIC const char* linphone_account_creator_get_pn_prid(const LinphoneAccountCreator *creator);
+
+/**
+ * Set the authentication token received by push notification to be used to authenticate next queries, if required.
+ * @param creator #LinphoneAccountCreator object @notnil
+ * @param token The token to set @maybenil
 **/
 LINPHONE_PUBLIC void linphone_account_creator_set_token(LinphoneAccountCreator *creator, const char* token);
+
+/**
+ * Get the authentication token set (if any) to be used to authenticate next queries, if required.
+ * @param creator #LinphoneAccountCreator object @notnil
+ * @return The token set, if any @maybenil
+**/
+LINPHONE_PUBLIC const char* linphone_account_creator_get_token(const LinphoneAccountCreator *creator);
 
 /**
  * Set the phone number normalized.

src/account_creator/main.cpp
@@ -124,16 +124,32 @@ void linphone_account_creator_set_pn_provider(LinphoneAccountCreator *creator, c
 	set_string(&creator->pn_provider, pn_provider, FALSE);
 }
 
+const char* linphone_account_creator_get_pn_provider(const LinphoneAccountCreator *creator) {
+	return creator->pn_provider;
+}
+
 void linphone_account_creator_set_pn_param(LinphoneAccountCreator *creator, const char* pn_param) {
 	set_string(&creator->pn_param, pn_param, FALSE);
+}
+
+const char* linphone_account_creator_get_pn_param(const LinphoneAccountCreator *creator) {
+	return creator->pn_param;
 }
 
 void linphone_account_creator_set_pn_prid(LinphoneAccountCreator *creator, const char* pn_prid) {
 	set_string(&creator->pn_prid, pn_prid, FALSE);
 }
 
+const char* linphone_account_creator_get_pn_prid(const LinphoneAccountCreator *creator) {
+	return creator->pn_prid;
+}
+
 void linphone_account_creator_set_token(LinphoneAccountCreator *creator, const char* token) {
 	set_string(&creator->token, token, FALSE);
+}
+
+const char* linphone_account_creator_get_token(const LinphoneAccountCreator *creator) {
+	return creator->token;
 }
 
 LinphoneAccountCreatorPhoneNumberStatusMask linphone_account_creator_set_phone_number(LinphoneAccountCreator *creator, const char *phone_number, const char *country_code) {

BelledonneCommunications/linphone-sdk

liblinphone/include/linphone/account_creator.h
@@ -218,32 +218,55 @@ linphone_account_creator_set_username(LinphoneAccountCreator *creator, const cha
 LINPHONE_PUBLIC const char *linphone_account_creator_get_username(const LinphoneAccountCreator *creator);
 
 /**
- * Set the pn_provider.
+ * Set the provider to be used by the backend to send the push notification to the device asking for an auth token.
  * @param creator #LinphoneAccountCreator object @notnil
- * @param pn_provider The pn_provider to set @notnil
+ * @param pn_provider The pn_provider to set @maybenil
  **/
 LINPHONE_PUBLIC void linphone_account_creator_set_pn_provider(LinphoneAccountCreator *creator, const char *pn_provider);
 
 /**
- * Set the pn_param.
+ * Get the provider to be used by the backend to send the push notification to the device asking for an auth token.
  * @param creator #LinphoneAccountCreator object @notnil
- * @param pn_param The pn_param to set @notnil
+ * @return The pn_provider set, if any @maybenil
+ **/
+LINPHONE_PUBLIC const char *linphone_account_creator_get_pn_provider(const LinphoneAccountCreator *creator);
+
+/**
+ * Set the param to be used by the backend to send the push notification to the device asking for an auth token.
+ * @param creator #LinphoneAccountCreator object @notnil
+ * @param pn_param The pn_param to set @maybenil
  **/
 LINPHONE_PUBLIC void linphone_account_creator_set_pn_param(LinphoneAccountCreator *creator, const char *pn_param);
 
 /**
- * Set the pn_prid.
+ * Get the param to be used by the backend to send the push notification to the device asking for an auth token.
  * @param creator #LinphoneAccountCreator object @notnil
- * @param pn_prid The pn_prid to set @notnil
+ * @return The pn_param set, if any @maybenil
+ **/
+LINPHONE_PUBLIC const char *linphone_account_creator_get_pn_param(const LinphoneAccountCreator *creator);
+
+/**
+ * Set the prid to be used by the backend to send the push notification to the device asking for an auth token.
+ * @param creator #LinphoneAccountCreator object @notnil
+ * @param pn_prid The pn_prid to set @maybenil
  **/
 LINPHONE_PUBLIC void linphone_account_creator_set_pn_prid(LinphoneAccountCreator *creator, const char *pn_prid);
 
 /**
- * Set the token.
+ * Get the prid to be used by the backend to send the push notification to the device asking for an auth token.
  * @param creator #LinphoneAccountCreator object @notnil
- * @param token The pn_prid to set @notnil
+ * @return The pn_prid set, if any @maybenil
+ * @param creator #LinphoneAccountCreator object @notnil
+ * @param token The token to set @maybenil
  **/
 LINPHONE_PUBLIC void linphone_account_creator_set_token(LinphoneAccountCreator *creator, const char *token);
+
+/**
+ * Get the authentication token set (if any) to be used to authenticate next queries, if required.
+ * @param creator #LinphoneAccountCreator object @notnil
+ * @return The token set, if any @maybenil
+ **/
+LINPHONE_PUBLIC const char *linphone_account_creator_get_token(const LinphoneAccountCreator *creator);
 
 /**
  * Set the phone number normalized.

liblinphone/src/account_creator/main.cpp
@@ -132,16 +132,32 @@ void linphone_account_creator_set_pn_provider(LinphoneAccountCreator *creator, c
 	set_string(&creator->pn_provider, pn_provider, FALSE);
 }
 
+const char *linphone_account_creator_get_pn_provider(const LinphoneAccountCreator *creator) {
+	return creator->pn_provider;
+}
+
 void linphone_account_creator_set_pn_param(LinphoneAccountCreator *creator, const char *pn_param) {
 	set_string(&creator->pn_param, pn_param, FALSE);
+}
+
+const char *linphone_account_creator_get_pn_param(const LinphoneAccountCreator *creator) {
+	return creator->pn_param;
 }
 
 void linphone_account_creator_set_pn_prid(LinphoneAccountCreator *creator, const char *pn_prid) {
 	set_string(&creator->pn_prid, pn_prid, FALSE);
 }
 
+const char *linphone_account_creator_get_pn_prid(const LinphoneAccountCreator *creator) {
+	return creator->pn_prid;
+}
+
 void linphone_account_creator_set_token(LinphoneAccountCreator *creator, const char *token) {
 	set_string(&creator->token, token, FALSE);
+}
+
+const char *linphone_account_creator_get_token(const LinphoneAccountCreator *creator) {
+	return creator->token;
 }
 
 LinphoneAccountCreatorPhoneNumberStatusMask linphone_account_creator_set_phone_number(LinphoneAccountCreator *creator,