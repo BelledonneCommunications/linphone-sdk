BelledonneCommunications/liblinphone

Fixed echo canceller calibration not using speaker audio device on some Android devices when using AAudio due to MODE_IN_COMMUNICATION being done too early

src/core/platform-helpers/android-platform-helpers.cpp
@@ -75,6 +75,8 @@ public:
 
 	void startAudioForEchoTestOrCalibration () override;
 	void stopAudioForEchoTestOrCalibration () override;
+	void routeAudioToSpeaker () override;
+	void restorePreviousAudioRoute () override;
 
 	void enableAutoIterate (bool autoIterateEnabled) override;
 
@@ -133,6 +135,8 @@ private:
 	jmethodID mCoreManagerOnLinphoneCoreStopId = nullptr;
 	jmethodID mStartAudioForEchoTestOrCalibrationId = nullptr;
 	jmethodID mStopAudioForEchoTestOrCalibrationId = nullptr;
+	jmethodID mRouteAudioToSpeakerId = nullptr;
+	jmethodID mRestorePreviousAudioRouteId = nullptr;
 	jmethodID mStartAutoIterateId = nullptr;
 	jmethodID mStopAutoIterateId = nullptr;
 	jmethodID mSetAudioManagerCommunicationMode = nullptr;
@@ -187,6 +191,8 @@ void AndroidPlatformHelpers::createCoreManager (std::shared_ptr<LinphonePrivate:
 	
 	mStartAudioForEchoTestOrCalibrationId = getMethodId(env, klass, "startAudioForEchoTestOrCalibration", "()V");
 	mStopAudioForEchoTestOrCalibrationId = getMethodId(env, klass, "stopAudioForEchoTestOrCalibration", "()V");
+	mRouteAudioToSpeakerId = getMethodId(env, klass, "routeAudioToSpeaker", "()V");
+	mRestorePreviousAudioRouteId = getMethodId(env, klass, "restorePreviousAudioRoute", "()V");
 	mStartAutoIterateId = getMethodId(env, klass, "startAutoIterate", "()V");
 	mStopAutoIterateId = getMethodId(env, klass, "stopAutoIterate", "()V");
 	mSetAudioManagerCommunicationMode = getMethodId(env, klass, "setAudioManagerInCommunicationMode", "()V");
@@ -520,6 +526,24 @@ void AndroidPlatformHelpers::stopAudioForEchoTestOrCalibration () {
 	if (env) {
 		if (mJavaCoreManager) {
 			env->CallVoidMethod(mJavaCoreManager, mStopAudioForEchoTestOrCalibrationId);
+		}
+	}
+}
+
+void AndroidPlatformHelpers::routeAudioToSpeaker () {
+	JNIEnv *env = ms_get_jni_env();
+	if (env) {
+		if (mJavaCoreManager) {
+			env->CallVoidMethod(mJavaCoreManager, mRouteAudioToSpeakerId);
+		}
+	}
+ }
+
+void AndroidPlatformHelpers::restorePreviousAudioRoute () {
+	JNIEnv *env = ms_get_jni_env();
+	if (env) {
+		if (mJavaCoreManager) {
+			env->CallVoidMethod(mJavaCoreManager, mRestorePreviousAudioRouteId);
 		}
 	}
 }

src/core/platform-helpers/platform-helpers.h
@@ -98,6 +98,9 @@ public:
 	virtual std::shared_ptr<SharedCoreHelpers> getSharedCoreHelpers() = 0;
 	virtual void startAudioForEchoTestOrCalibration () = 0;
 	virtual void stopAudioForEchoTestOrCalibration () = 0;
+	virtual void routeAudioToSpeaker () = 0;
+	virtual void restorePreviousAudioRoute () = 0;
+
 	virtual void start (std::shared_ptr<LinphonePrivate::Core> core) = 0;
 	virtual void stop (void) = 0;
 
@@ -167,6 +170,8 @@ public:
 	std::shared_ptr<SharedCoreHelpers> getSharedCoreHelpers() override;
 	void startAudioForEchoTestOrCalibration () override;
 	void stopAudioForEchoTestOrCalibration () override;
+	void routeAudioToSpeaker () override;
+	void restorePreviousAudioRoute () override;
 	
 	void start (BCTBX_UNUSED(std::shared_ptr<LinphonePrivate::Core> core)) override {};
 	void stop (void) override {};

BelledonneCommunications/linphone-sdk

liblinphone/src/core/platform-helpers/android-platform-helpers.cpp
@@ -75,6 +75,8 @@ public:
 
 	void startAudioForEchoTestOrCalibration() override;
 	void stopAudioForEchoTestOrCalibration() override;
+	void routeAudioToSpeaker() override;
+	void restorePreviousAudioRoute() override;
 
 	void enableAutoIterate(bool autoIterateEnabled) override;
 
@@ -133,6 +135,8 @@ private:
 	jmethodID mCoreManagerOnLinphoneCoreStopId = nullptr;
 	jmethodID mStartAudioForEchoTestOrCalibrationId = nullptr;
 	jmethodID mStopAudioForEchoTestOrCalibrationId = nullptr;
+	jmethodID mRouteAudioToSpeakerId = nullptr;
+	jmethodID mRestorePreviousAudioRouteId = nullptr;
 	jmethodID mStartAutoIterateId = nullptr;
 	jmethodID mStopAutoIterateId = nullptr;
 	jmethodID mSetAudioManagerCommunicationMode = nullptr;
@@ -187,6 +191,8 @@ void AndroidPlatformHelpers::createCoreManager(std::shared_ptr<LinphonePrivate::
 
 	mStartAudioForEchoTestOrCalibrationId = getMethodId(env, klass, "startAudioForEchoTestOrCalibration", "()V");
 	mStopAudioForEchoTestOrCalibrationId = getMethodId(env, klass, "stopAudioForEchoTestOrCalibration", "()V");
+	mRouteAudioToSpeakerId = getMethodId(env, klass, "routeAudioToSpeaker", "()V");
+	mRestorePreviousAudioRouteId = getMethodId(env, klass, "restorePreviousAudioRoute", "()V");
 	mStartAutoIterateId = getMethodId(env, klass, "startAutoIterate", "()V");
 	mStopAutoIterateId = getMethodId(env, klass, "stopAutoIterate", "()V");
 	mSetAudioManagerCommunicationMode = getMethodId(env, klass, "setAudioManagerInCommunicationMode", "()V");
@@ -515,6 +521,24 @@ void AndroidPlatformHelpers::stopAudioForEchoTestOrCalibration() {
 	if (env) {
 		if (mJavaCoreManager) {
 			env->CallVoidMethod(mJavaCoreManager, mStopAudioForEchoTestOrCalibrationId);
+		}
+	}
+}
+
+void AndroidPlatformHelpers::routeAudioToSpeaker() {
+	JNIEnv *env = ms_get_jni_env();
+	if (env) {
+		if (mJavaCoreManager) {
+			env->CallVoidMethod(mJavaCoreManager, mRouteAudioToSpeakerId);
+		}
+	}
+}
+
+void AndroidPlatformHelpers::restorePreviousAudioRoute() {
+	JNIEnv *env = ms_get_jni_env();
+	if (env) {
+		if (mJavaCoreManager) {
+			env->CallVoidMethod(mJavaCoreManager, mRestorePreviousAudioRouteId);
 		}
 	}
 }

liblinphone/src/core/platform-helpers/platform-helpers.h
@@ -95,8 +95,12 @@ public:
 	virtual void onLinphoneCoreStop() = 0;
 
 	virtual std::shared_ptr<SharedCoreHelpers> getSharedCoreHelpers() = 0;
+
 	virtual void startAudioForEchoTestOrCalibration() = 0;
 	virtual void stopAudioForEchoTestOrCalibration() = 0;
+	virtual void routeAudioToSpeaker() = 0;
+	virtual void restorePreviousAudioRoute() = 0;
+
 	virtual void start(std::shared_ptr<LinphonePrivate::Core> core) = 0;
 	virtual void stop(void) = 0;
 
@@ -165,8 +169,11 @@ public:
 	void onLinphoneCoreStop() override;
 
 	std::shared_ptr<SharedCoreHelpers> getSharedCoreHelpers() override;
+
 	void startAudioForEchoTestOrCalibration() override;
 	void stopAudioForEchoTestOrCalibration() override;
+	void routeAudioToSpeaker() override;
+	void restorePreviousAudioRoute() override;
 
 	void start(BCTBX_UNUSED(std::shared_ptr<LinphonePrivate::Core> core)) override{};
 	void stop(void) override{};