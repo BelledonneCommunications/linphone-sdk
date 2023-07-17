c6f9cc8f9fafd99b63e4ac3096eb296e562d95d9
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2023-07-17 14:27:03 +0200

    Various fixes from release/5.2 branch

--- cmake/Android/gradle/LinphoneSdkManifest.xml.cmake
+++ cmake/Android/gradle/LinphoneSdkManifest.xml.cmake
@@ -7,7 +7,6 @@
     <uses-permission android:name="android.permission.INTERNET" />
     <uses-permission android:name="android.permission.RECORD_AUDIO" />
     <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
-	<uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
     <uses-permission android:name="android.permission.WAKE_LOCK" />
     <!-- Needed to be able to use WifiManager.MulticastLock -->
     <uses-permission android:name="android.permission.CHANGE_WIFI_MULTICAST_STATE" />
@@ -24,6 +23,13 @@
     <uses-permission android:name="android.permission.ACCESS_NOTIFICATION_POLICY" />
     <!-- Required to be able to monitor WiFi signal strength -->
     <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+    <!-- Needed for Android 14
+    https://developer.android.com/about/versions/14/behavior-changes-14#fgs-types -->
+    <uses-permission android:name="android.permission.FOREGROUND_SERVICE"/>
+    <uses-permission android:name="android.permission.FOREGROUND_SERVICE_CAMERA" />
+    <uses-permission android:name="android.permission.FOREGROUND_SERVICE_MICROPHONE" />
+    <uses-permission android:name="android.permission.FOREGROUND_SERVICE_PHONE_CALL" />
+    <uses-permission android:name="android.permission.FOREGROUND_SERVICE_DATA_SYNC" />
     <!-- End of SDK required permissions -->
 	
 </manifest>
