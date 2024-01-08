f6aef60c334c0b704be69127bb786a72a9f42465
QuentinArguillere <quentin.arguillere@belledone-communications.com>
2024-01-08 10:01:28 +0100

    ios begin/end background task : remove the dispatch_sync call to force execution on main thread, as beginBackgroundTask and endBackgroundTask functions can safely be called on non-main thread

--- bctoolbox/src/utils/ios_utils_app.mm
+++ bctoolbox/src/utils/ios_utils_app.mm
@@ -31,68 +31,50 @@ namespace bctoolbox {
 unsigned long IOSUtilsApp::beginBackgroundTask(const char *name, std::function<void()> cb) {
     __block UIBackgroundTaskIdentifier bgid = UIBackgroundTaskInvalid;
 
-    dispatch_block_t block = ^{
-        UIApplication *app=[UIApplication sharedApplication];
-
-        @try {
-            if (cb==nullptr){
-                bctbx_error("belle_sip_begin_background_task(): the callback must not be NULL. Application must be aware that the background task needs to be terminated.");
-                bgid = UIBackgroundTaskInvalid;
-                @throw([NSException exceptionWithName:@"LinphoneCoreException" reason:@"Background task has no callback" userInfo:nil]);            
-            }
-
-            void (^handler)() = ^{
-                cb();
-            };
-
-            if([app respondsToSelector:@selector(beginBackgroundTaskWithName:expirationHandler:)]){
-                bgid = [app beginBackgroundTaskWithName:[NSString stringWithUTF8String:name] expirationHandler:handler];
-            } else {
-                bgid = [app beginBackgroundTaskWithExpirationHandler:handler];
-            }
-
-            if (bgid==UIBackgroundTaskInvalid){
-                bctbx_error("Could not start background task %s.", name);
-                bgid = 0;
-		@throw([NSException exceptionWithName:@"LinphoneCoreException" reason:@"Could not start background task" userInfo:nil]);
-            }
-
-            // backgroundTimeRemaining is properly set only when running background... but not immediately!
-            if (app.applicationState != UIApplicationStateBackground || (app.backgroundTimeRemaining == DBL_MAX)) {
-                bctbx_message("Background task %s started. Unknown remaining time since application is not fully in background.", name);
-            } else {
-                bctbx_message("Background task %s started. Remaining time %.1f secs", name, app.backgroundTimeRemaining);
-            }
-        }
-        @catch (NSException*) {
-            // do nothing
-        }
-    };
-
-    if( [NSThread isMainThread] ) {
-        block();
-    }
-    else {
-        dispatch_sync(dispatch_get_main_queue(), block);
-    }
+	UIApplication *app=[UIApplication sharedApplication];
+	
+	@try {
+		if (cb==nullptr){
+			bctbx_error("belle_sip_begin_background_task(): the callback must not be NULL. Application must be aware that the background task needs to be terminated.");
+			bgid = UIBackgroundTaskInvalid;
+			@throw([NSException exceptionWithName:@"LinphoneCoreException" reason:@"Background task has no callback" userInfo:nil]);
+		}
+		
+		void (^handler)() = ^{
+			cb();
+		};
+		
+		if([app respondsToSelector:@selector(beginBackgroundTaskWithName:expirationHandler:)]){
+			bgid = [app beginBackgroundTaskWithName:[NSString stringWithUTF8String:name] expirationHandler:handler];
+		} else {
+			bgid = [app beginBackgroundTaskWithExpirationHandler:handler];
+		}
+		
+		if (bgid==UIBackgroundTaskInvalid){
+			bctbx_error("Could not start background task %s.", name);
+			bgid = 0;
+			@throw([NSException exceptionWithName:@"LinphoneCoreException" reason:@"Could not start background task" userInfo:nil]);
+		}
+		
+		// backgroundTimeRemaining is properly set only when running background... but not immediately!
+		if (app.applicationState != UIApplicationStateBackground || (app.backgroundTimeRemaining == DBL_MAX)) {
+			bctbx_message("Background task %s started. Unknown remaining time since application is not fully in background.", name);
+		} else {
+			bctbx_message("Background task %s started. Remaining time %.1f secs", name, app.backgroundTimeRemaining);
+		}
+	}
+	@catch (NSException*) {
+		// do nothing
+	}
 
     return (unsigned long)bgid;
 }
 
 void IOSUtilsApp::endBackgroundTask(unsigned long id) {
-    dispatch_block_t block = ^{
-        UIApplication *app=[UIApplication sharedApplication];
-        if (id != UIBackgroundTaskInvalid){
-            [app endBackgroundTask:(UIBackgroundTaskIdentifier)id];
-        }
-    };
-
-    if( [NSThread isMainThread] ) {
-        block();
-    }
-    else {
-        dispatch_sync(dispatch_get_main_queue(), block);
-    }
+	UIApplication *app=[UIApplication sharedApplication];
+	if (id != UIBackgroundTaskInvalid){
+		[app endBackgroundTask:(UIBackgroundTaskIdentifier)id];
+	}
 }
 
 bool IOSUtilsApp::isApplicationStateActive() {
