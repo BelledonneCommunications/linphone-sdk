af941e04146a3c5c860c4689b17cbd932ff16de0
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2024-02-12 08:51:47 +0100

    Fix naming of windows sdk zip files.

--- cmake/Windows/GenerateSDK.cmake
+++ cmake/Windows/GenerateSDK.cmake
@@ -1,8 +1,8 @@
-############################################################################
+# ###########################################################################
 # GenerateSDK.cmake
 # Copyright (C) 2010-2023 Belledonne Communications, Grenoble France
 #
-############################################################################
+# ###########################################################################
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -18,20 +18,21 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-############################################################################
+# ###########################################################################
 
 include("${LINPHONESDK_DIR}/cmake/LinphoneSdkUtils.cmake")
 
-
 find_program(7Z_PROGRAM 7z.exe)
+
 if(7Z_PROGRAM)
 	if(NOT ENABLE_EMBEDDED_OPENH264)
 		set(7Z_MORE_CONFIG "-xr!openh264.dll")
 	else()
 		set(7Z_MORE_CONFIG "")
 	endif()
+
 	execute_process(
-		COMMAND ${7Z_PROGRAM} "a" "-r" "linphone-sdk-${LINPHONESDK_PLATFORM}-${LINPHONESDK_VERSION}.zip" "${LINPHONESDK_FOLDER}" ${7Z_MORE_CONFIG}
+		COMMAND ${7Z_PROGRAM} "a" "-r" "linphone-sdk-${LINPHONESDK_WINDOWS_ARCH}-${LINPHONESDK_VERSION}.zip" "${LINPHONESDK_FOLDER}" ${7Z_MORE_CONFIG}
 		WORKING_DIRECTORY "${LINPHONESDK_BUILD_DIR}"
 	)
 else()
