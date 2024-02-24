7798ab5e6beeea3b941dd4e39c1b788b1670fd29
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2024-02-24 11:28:17 +0100

    Updated liblinphone to improve generated python doc + added comments to module __init__.py

--- cmake/python/__init__.py
+++ cmake/python/__init__.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
-# Get path where wheel was installed
+# Get path where wheel was installed (where this file is located).
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
+# This allows loading the shared library containing the python wrapper module.
 sys.path.append(base_dir)
-
 from pylinphone import *
 
+# This is required so the Factory knows where the grammar libraries are available.
+# It will prevent the Core from aborting when created due to grammar not being found.
 grammars_dir = os.path.join(base_dir, 'share/')
 Factory.get().top_resources_dir = grammars_dir
-
-print(Core.get_version())
