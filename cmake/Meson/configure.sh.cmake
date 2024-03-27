4d32973942a2919a57183cd929dd8aa47f63cdb7
jehan <jehan.monnier@linphone.org>
2024-03-27 10:20:56 +0000

    add support for http basic auth

--- cmake/Meson/configure.sh.cmake
+++ cmake/Meson/configure.sh.cmake
@@ -5,4 +5,4 @@ export PATH="@EP_PROGRAM_PATH@"
 cd @EP_BUILD_DIR@
 
 # Setting only prefix is not enough for older versions of Meson, libdir MUST be set to.
-meson setup @EP_SOURCE_DIR_RELATIVE_TO_BUILD_DIR@ --buildtype @EP_BUILD_TYPE@ --prefix "@CMAKE_INSTALL_PREFIX@" --libdir "@CMAKE_INSTALL_LIBDIR@" @EP_ADDITIONAL_OPTIONS@
+@LINPHONESDK_MESON_PROGRAM@ setup @EP_SOURCE_DIR_RELATIVE_TO_BUILD_DIR@ --buildtype @EP_BUILD_TYPE@ --prefix "@CMAKE_INSTALL_PREFIX@" --libdir "@CMAKE_INSTALL_LIBDIR@" @EP_ADDITIONAL_OPTIONS@
