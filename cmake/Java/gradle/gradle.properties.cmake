8787e6cb9eb5b88e65fdbef9b59dd50c10065016
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2024-10-24 12:06:53 +0200

    Do not start gradle daemon for Java SDK to prevent hanging build.

--- cmake/Java/gradle/gradle.properties.cmake
+++ cmake/Java/gradle/gradle.properties.cmake
@@ -1,2 +1,3 @@
 #source:https://docs.gradle.org/current/userguide/build_environment.html#sec:configuring_jvm_memory
 org.gradle.jvmargs=-XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8
+org.gradle.daemon=false
