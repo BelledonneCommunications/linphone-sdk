d3c7b66cc4ab74b2627d8f0d678a66372af735d8
Clemence Him <clemence.him@belledonne-communications.com>
2025-02-05 10:47:58 +0100

    Refactor Core::createXmlFromEktInfo to accept an account parameter

--- cmake/EktServerClone.cmake
+++ cmake/EktServerClone.cmake
@@ -1,6 +1,6 @@
 ############################################################################
 # EktServer.cmake
-# Copyright (C) 2010-2023 Belledonne Communications, Grenoble France
+# Copyright (C) 2010-2025 Belledonne Communications, Grenoble France
 #
 ############################################################################
 #
@@ -24,7 +24,7 @@ include("${PROJECT_SOURCE_DIR}/cmake/LinphoneSdkUtils.cmake")
 
 linphone_sdk_check_git()
 
-set(EKT_SERVER_REVISION "44a5d89c75b0fe61a5d070a70027d814be73b94b")
+set(EKT_SERVER_REVISION "392201d0fab343426359c382c4f694c3e43231b7")
 
 if(IS_DIRECTORY "${PROJECT_SOURCE_DIR}/ekt-server")
 	execute_process(
