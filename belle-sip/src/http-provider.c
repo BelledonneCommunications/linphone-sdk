d1a98177951688a3cba2cc727203ec7f2d01ce1c
Simon Morlat <simon.morlat@linphone.org>
2026-01-19 16:51:37 +0100

    Fix crash because of null pointer, during http authentication.

--- belle-sip/src/http-provider.c
+++ belle-sip/src/http-provider.c
@@ -198,7 +198,9 @@ static int http_channel_context_handle_authentication(belle_http_channel_context
 			if (header) {
 				belle_sip_header_address_t *from_address =
 				    belle_sip_header_address_parse(belle_sip_header_get_unparsed_value(header));
-				from_uri = belle_sip_header_address_get_uri(from_address);
+				if (from_address) {
+					from_uri = belle_sip_header_address_get_uri(from_address);
+				}
 			} else if (username && !passwd) {
 				from_uri = belle_sip_uri_create(username, realm);
 			}
