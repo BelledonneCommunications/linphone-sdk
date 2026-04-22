f922f998f5dfb816353e34d7c57e9c808fc38cee
Julien Wadel <julien.wadel@belledonne-communications.com>
2026-04-22 09:50:23 +0200

    CMAKE_BUILD_TYPE was not propagated, then some submodules were build in their default build configuration which could be in conflict with the main build type. The issue appeared on Windows platforms with a conflict in debug/release runtimes with the type Release.

--- cmake/LinphoneSdkUtils.cmake
+++ cmake/LinphoneSdkUtils.cmake
@@ -38,7 +38,8 @@ function(linphone_sdk_get_inherited_cmake_args OUTPUT_CONFIGURE_ARGS_VAR OUTPUT_
 	endif()
 	if(CMAKE_GENERATOR STREQUAL "Ninja Multi-Config" OR CMAKE_GENERATOR STREQUAL "Xcode" OR CMAKE_GENERATOR MATCHES "^Visual Studio")
 		list(APPEND _CMAKE_BUILD_ARGS "--config $<CONFIG>")
-	elseif(NOT "${CMAKE_BUILD_TYPE}" STREQUAL "")
+	endif()
+	if(NOT "${CMAKE_BUILD_TYPE}" STREQUAL "")
 		list(APPEND _CMAKE_CONFIGURE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}")
 	endif()
 
