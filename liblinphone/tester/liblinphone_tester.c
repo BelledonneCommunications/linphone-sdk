68f9802f71fbb529621bde6a44964f116e3fb6ee
QuentinA <quentin.arguillere@belledone-communications.com>
2026-06-30 10:21:05 +0200

    Increase liblinphone tester timeout limit from 36min to 38min

--- liblinphone/tester/liblinphone_tester.c
+++ liblinphone/tester/liblinphone_tester.c
@@ -673,7 +673,7 @@ void liblinphone_tester_init(void (*ftester_printf)(int level, const char *fmt, 
 	liblinphone_tester_add_suites();
 	bc_tester_set_max_parallel_suites(
 	    liblinphone_tester_max_parallel_suites); /* empiricaly defined as sustainable for our lab 12 threads machine.*/
-	bc_tester_set_global_timeout(36 * 60 * 32 /
+	bc_tester_set_global_timeout(38 * 60 * 32 /
 	                             liblinphone_tester_max_parallel_suites); /* 36 mn max for 32 processes */
 }
 
